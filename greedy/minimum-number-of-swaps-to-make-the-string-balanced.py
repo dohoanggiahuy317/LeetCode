151e2b25d6199678e71051bf50f8bb163f1abc6d
JaWeee <dohoanggiahuy310703@gmail.com>
2024-05-16 06:58:09 -0400

    :ballot_box_with_check: minimum-number-of-swaps-to-make-the-string-balanced [Runtime: 284 ms; Memory: 33.9 MB]

---
+++ greedy/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        li = list(s)
+        close_brack = 0
+        ite = len(s) - 1
+        ans = 0
+
+
+        for i in range(len(s)):
+            #print(li, close_brack)
+            
+            if li[i] == "[":
+                close_brack -= 1
+            elif li[i] == "]":
+                close_brack += 1
+            
+                if close_brack > 0:
+                    while li[ite] != "[":
+                        ite -= 1
+                    
+                    li[i], li[ite] = li[ite], li[i]
+                    ans += 1
+                
+                    close_brack -= 2
+            
+            
+        
+        return ans
+        
