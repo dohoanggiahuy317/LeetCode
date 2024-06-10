8668f0f360a5f0e61c8169777b4e99c9973d05c3
JaWeee <dohoanggiahuy310703@gmail.com>
2024-06-10 15:14:33 +0700

    :ballot_box_with_check: largest-divisible-subset [Runtime: 357 ms; Memory: 16.8 MB]

---
+++ dynamic-programming/largest-divisible-subset.py
@@ -0,0 +1,20 @@
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        dp = [1] * len(nums)
+        nums.sort()
+        ans = [[i] for i in nums]
+
+
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 or nums[j] % nums[i] == 0:
+                    if dp[j] + 1 >= dp[i]:
+                        ans[i] = ans[j] + [nums[i]]
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+                # else:
+                #     dp[i] = max(dp[i], dp[j])
+        # print(dp, ans)
+        ans.sort(key = len)
+
+        return ans[-1]
