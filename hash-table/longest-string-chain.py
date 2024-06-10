800f28ed147b26c1f07556f683861d20ac3425c1
JaWeee <dohoanggiahuy310703@gmail.com>
2024-06-10 10:36:53 +0700

    :ballot_box_with_check: longest-string-chain [Runtime: 714 ms; Memory: 16.9 MB]

---
+++ hash-table/longest-string-chain.py
@@ -0,0 +1,41 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        def pred(s1, s2):
+            if len(s1) + 1 != len(s2):
+                return False
+            f = 0
+            it = 0
+
+            for i in range(len(s2)):
+                if it == len(s1):
+                    return True
+
+                if s2[i] == s1[it]:
+                    it += 1
+                else:
+                    f += 1
+
+                if f == 2:
+                    return False
+
+            return it == len(s1)
+                
+
+            
+        dp = [1] * len(words)
+        words.sort(key = lambda x: len(x))
+        # print(words)
+
+        for i in range(len(words)):
+            # print(i)
+            for j in range(i):
+                # print(words[j], words[i])
+                if pred(words[j], words[i]):
+                    dp[i] = max(dp[i], dp[j] + 1)
+        #             print(dp)
+
+        #     print()
+
+        # print(dp)
+
+        return max(dp)
