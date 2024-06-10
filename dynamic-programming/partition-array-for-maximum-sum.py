e99dcad8ae4b72b5ab41008d3f0399fd2c8e34d6
JaWeee <dohoanggiahuy310703@gmail.com>
2024-06-10 15:01:11 +0700

    :ballot_box_with_check: partition-array-for-maximum-sum [Runtime: 2727 ms; Memory: 16.6 MB]

---
+++ dynamic-programming/partition-array-for-maximum-sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxSumAfterPartitioning(self, arr, k):
+
+        arr = [0] + arr
+        dp = [0] * (len(arr))
+        curr_max = max(arr[:k])
+        dp[1] = arr[1]
+
+        for i in range(2, len(arr)):
+            curr = 0  
+            for x in range(k):
+                # print(arr[i-x:i+1])
+                curr = max(
+                    curr, 
+                    dp[max(0, i-1 - x)] + (x+1 if i-x > 0 else i) * max(
+                                        arr[ 
+                                            max(0, i-x): i+1
+                                            ]))
+
+                # print(i, x, dp[max(0, i-1 - x)], (x+1 if i-x > 0 else i) * max(
+                #                         arr[ 
+                #                             max(0, i-x): i+1
+                #                             ]))
+            dp[i] = max(dp[i], curr)
+
+            # print(dp)
+
+        return dp[-1]
+
+
