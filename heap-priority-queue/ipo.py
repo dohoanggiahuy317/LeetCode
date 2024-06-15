987859a6ea82d81f8677d5a0b7446bc40466041d
JaWeee <dohoanggiahuy310703@gmail.com>
2024-06-15 16:01:06 +0700

    :ballot_box_with_check: ipo [Runtime: 2262 ms; Memory: 48.3 MB]

--- heap-priority-queue/ipo.py
+++ heap-priority-queue/ipo.py
@@ -5,12 +5,8 @@ class Solution:
         projects = [[capital[i], -profits[i]] for i in range(len(profits))]
         projects.sort(key = lambda x: [x[0], x[1]])
         
-        heap = []
-
-        while projects and projects[0][0] <= w:
-            heapq.heappush(heap, projects.pop(0)[1])
-
-        i = 0
+        heap = [0]
+        i = -1
         while heap and i < k:
             w -= heapq.heappop(heap)
 
@@ -18,25 +14,5 @@ class Solution:
                 heapq.heappush(heap, projects.pop(0)[1]) 
 
             i += 1
-
-        # for t in range(len(profits)):
-        #     heapq.heappush(heap, [-profits[t], capital[t]])
-
-        # i = 0
-        # while len(heap) > 0 and i < k:
-        #     temp = []
-
-        #     while heap:
-        #         f = heapq.heappop(heap)
-        #         if f[1] <= w:
-        #             w += -f[0]
-        #             break
-        #         else:
-        #             temp.append(f)
-            
-        #     i += 1
-        #     for x in temp:
-        #         heapq.heappush(heap, x)
-
 
         return w
