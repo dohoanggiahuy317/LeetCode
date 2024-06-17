049f3db246cee8802cbe0f8264d8b02356426a43
Jeremy <dohoanggiahuy310703@gmail.com>
2024-06-17 17:27:26 +0700

    :ballot_box_with_check: sum-of-square-numbers [Runtime: 709 ms; Memory: 18.3 MB]

---
+++ two-pointers/sum-of-square-numbers.py
@@ -0,0 +1,26 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+
+        if c % 8 not in [0, 1, 2, 4, 5]:
+            return False
+        if c % 4 == 3:
+            return False
+
+        temp = []
+        i = 0
+        holder = 2 ** 31 - 1
+        while i ** 2 <= holder:
+            temp.append(i ** 2)
+            i += 1
+
+        s = 0
+        e = len(temp) - 1
+
+        while s <= e:
+            if temp[s] + temp[e] > c:
+                e -= 1
+            elif temp[s] + temp[e] < c:
+                s += 1
+            else:
+                return True
+        return False
