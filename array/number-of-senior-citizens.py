1adb9aee271828655d3f31251284c4c7bbda9994
Jeremy <dohoanggiahuy310703@gmail.com>
2024-08-01 14:26:05 +0700

    :ballot_box_with_check: number-of-senior-citizens [Runtime: 50 ms; Memory: 16.5 MB]

--- array/number-of-senior-citizens.py
+++ array/number-of-senior-citizens.py
@@ -2,12 +2,8 @@ class Solution:
     def countSeniors(self, details: List[str]) -> int:
         return len(
                     list(filter(
-                        lambda x: x > 60,
-                        list(
-                            map(
-                                lambda x: int(x[11:13]), 
-                                details
-                                )
-                            ))
-                    ))
+                        lambda x: int(x[11:13]) > 60, 
+                        details
+                        ))
+                    )
                     
