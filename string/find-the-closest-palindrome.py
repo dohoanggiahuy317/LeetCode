7664bf522d84da084b1aff38dc3a6d44ca39071f
Jeremy <dohoanggiahuy310703@gmail.com>
2024-08-24 00:01:49 -0400

    LC-find-the-closest-palindrome [Runtime: N/A; Memory: N/A]

--- string/find-the-closest-palindrome.py
+++ string/find-the-closest-palindrome.py
@@ -10,6 +10,16 @@ class Solution:
         n1 = n[:l//2] + n[:l//2][::-1] if l % 2 == 0 else n[:l//2] + n[l//2] + n[:l//2][::-1]
         n2 = "9" * (l-1)
 
+        if n == n1:
+
+            if n == "11":
+                n1 = "22"
+            else:
+                x = int(n1[l//2])-1
+                if n1[l//2] == "0":
+                    x = 1
+                n1 = n1[:l//2-1] + str(x) * 2 + n1[:l//2-1][::-1] if l % 2 == 0 else n1[:l//2] + str(x) + n1[:l//2][::-1]
+
         print(n1, n2)
 
         if abs(int(n1)-int(n)) == abs(int(n2)-int(n)):
