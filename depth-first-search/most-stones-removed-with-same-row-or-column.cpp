b6bb69f7bc0364791a2fd3f1d4e987d2ae69f0a2
Jeremy <dohoanggiahuy310703@gmail.com>
2024-08-30 05:56:42 -0400

    LC-most-stones-removed-with-same-row-or-column [Runtime: 13 ms; Memory: 24.4 MB]

--- depth-first-search/most-stones-removed-with-same-row-or-column.cpp
+++ depth-first-search/most-stones-removed-with-same-row-or-column.cpp
@@ -1,31 +1,32 @@
 class Solution {
+private:
+    int connectedComponentCount = 0;
+
+    int findRepresentative(int element, vector<int>& setRepresentatives) {
+        if (setRepresentatives[element] == 0) {
+            setRepresentatives[element] = element;
+            connectedComponentCount++;
+        }
+        return setRepresentatives[element] == element ? element : 
+               (setRepresentatives[element] = findRepresentative(setRepresentatives[element], setRepresentatives));
+    }
+
+    void mergeComponents(int elementA, int elementB, vector<int>& setRepresentatives) {
+        int repA = findRepresentative(elementA, setRepresentatives);
+        int repB = findRepresentative(elementB, setRepresentatives);
+        if (repA != repB) {
+            setRepresentatives[repB] = repA;
+            connectedComponentCount--;
+        }
+    }
+
 public:
-int f(int *p, int i) {
-while (p[i] != -1) {
-  i = p[i];
-}
-return i;
-}
-
-void u(int *p, int i, int j) {
-int gi = f(p, i);
-int gj = f(p, j);
-if (gi != gj) p[gi] = gj;
-}
-
-int removeStones(vector<vector<int>> &stones) {
-const int len = stones.size();
-if (len <= 1) return 0;
-
-int grps[10000 * 2];
-for (int i = 0; i < 10000 * 2; i++) grps[i] = -1;
-for (const auto &st : stones) {
-  u(grps, st[0], 10000 + st[1]);
- }
-set<int> s;
-for (const auto &st : stones) {
-  s.insert(f(grps, st[0]));
-}
-return len - s.size();
-}
+    int removeStones(vector<vector<int>>& stonePositions) {
+        vector<int> setRepresentatives(20003, 0);
+        for (const auto& stonePosition : stonePositions) {
+            mergeComponents(stonePosition[0] + 1, stonePosition[1] + 10002, setRepresentatives);
+        }
+        return stonePositions.size() - connectedComponentCount;
+    }
 };
+
