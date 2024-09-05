95f63a6937b37f43d56a4f47bc2b665addc7849f
Jeremy <dohoanggiahuy310703@gmail.com>
2024-09-05 08:32:58 -0400

    LC-summary-ranges [Runtime: N/A; Memory: N/A]

---
+++ array/summary-ranges.py
@@ -0,0 +1,21 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        
+        s = 0
+
+        ans = []
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1] + 1:
+                if i > s + 1:
+                    ans.append(str(nums[s]) + "->" + str(nums[i-1]) )
+                elif i == s + 1:
+                    ans.append(str(nums[s]))
+
+                s = i
+        if s < len(nums) - 1:
+            ans.append(str(nums[s]) + "->" + str(nums[-1]) )
+        else:
+            ans.append(str(nums[s]))
+
+        return ans
