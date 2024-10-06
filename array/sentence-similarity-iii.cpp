0497d557c47d4de3ea8f1051a0526ec45f7d4969
Jeremy <dohoanggiahuy310703@gmail.com>
2024-10-05 23:13:48 -0400

    LC-sentence-similarity-iii [Runtime: 0 ms; Memory: 8.7 MB]

--- array/sentence-similarity-iii.cpp
+++ array/sentence-similarity-iii.cpp
@@ -12,7 +12,7 @@ public:
     static bool areSentencesSimilar(string sentence1, string sentence2) {
         auto s1=toStringVec(sentence1), s2=toStringVec(sentence2);
         int n1=s1.size(), n2=s2.size();
-        if (n1>n2) swap(n1, n2), swap(s1, s1);
+        if (n1>n2) swap(n1, n2), swap(s1, s2);
         int l=0, r2=n2-1, r1=n1-1;
         for( ;l<n1 && s1[l]==s2[l]; l++);
         for( ;r1>=0 && s1[r1]==s2[r2]; r2--, r1--);
