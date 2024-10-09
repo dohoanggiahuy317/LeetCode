51dd4d5e0f75aeb144c09ad82a0574ab3d1e9354
Jeremy <dohoanggiahuy310703@gmail.com>
2024-10-08 23:46:13 -0400

    LC-minimum-add-to-make-parentheses-valid [Runtime: 35 ms; Memory: 16.6 MB]

--- stack/minimum-add-to-make-parentheses-valid.py
+++ stack/minimum-add-to-make-parentheses-valid.py
@@ -1,11 +1,16 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         l = 0
+        ans = 0
 
         for char in list(s):
             if char == "(":
                 l += 1
             else:
                 l -= 1
+            
+            if l < 0:
+                ans += 1
+                l = 0
         
-        return abs(l)
+        return ans + abs(l)
