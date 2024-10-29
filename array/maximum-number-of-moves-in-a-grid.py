d9acc57a027d19291dd198ce88d670569ba4b308
Jeremy <dohoanggiahuy310703@gmail.com>
2024-10-28 21:33:04 -0400

    LC-maximum-number-of-moves-in-a-grid [Runtime: 317 ms; Memory: 27 MB]

--- array/maximum-number-of-moves-in-a-grid.py
+++ array/maximum-number-of-moves-in-a-grid.py
@@ -2,39 +2,55 @@ class Solution:
     def maxMoves(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
 
-        def backtrack(i, j, curr):
-            nonlocal m, n, temp
 
-            temp = max(temp, curr)
+        dp = [[0] * n for _ in range(m)]
 
-            if j >= n-1:
-                return
+
+        for j in range(n-2, -1, -1):
+            for i in range(m-1, -1, -1):
             
-            if j<n-1 and i>0 and grid[i][j] < grid[i-1][j+1]:
-                backtrack(i-1, j+1, curr+1)
-            if j<n-1 and grid[i][j] < grid[i][j+1]:
-                backtrack(i, j+1, curr+1)
-            if j<n-1 and i<m-1 and grid[i][j] < grid[i+1][j+1]:
-                backtrack(i+1, j+1, curr+1)
+                temp = 0
+                if i > 0 and grid[i][j] < grid[i-1][j+1]:
+                    temp = max(temp, dp[i-1][j+1]+1)
+                
+                if i < m-1 and grid[i][j] < grid[i+1][j+1]:
+                    temp = max(temp, dp[i+1][j+1]+1)
+                
+                if grid[i][j] < grid[i][j+1]:
+                    temp = max(temp, dp[i][j+1]+1)
+
+                dp[i][j] = temp
 
 
-        ans = 0
-        for i in range(m):
-            temp = 0
-            backtrack(i, 0, 0)
-            ans = max(ans, temp)
-
-        return ans
-
-
-
-        # dp = [[0] * (n+1) for _ in range(m + 1)]
-
-        # for i in range(m-1, -1, -1):
-        #     for j in range(n-1, -1, -1):
-        #         temp = 0
-
-        #         if j < n-1 and i > 0 and grid[i][j] < grid[i-1][j+1]:
-        #             temp = max(temp, dp grid[i-1][j+1])
+        return max(list(map(lambda x: x[0], dp)))
 
         
+
+
+
+        # def backtrack(i, j, curr):
+        #     nonlocal m, n, temp
+
+        #     temp = max(temp, curr)
+
+        #     if j >= n-1:
+        #         return
+            
+        #     if j<n-1 and i>0 and grid[i][j] < grid[i-1][j+1]:
+        #         backtrack(i-1, j+1, curr+1)
+        #     if j<n-1 and grid[i][j] < grid[i][j+1]:
+        #         backtrack(i, j+1, curr+1)
+        #     if j<n-1 and i<m-1 and grid[i][j] < grid[i+1][j+1]:
+        #         backtrack(i+1, j+1, curr+1)
+
+
+        # ans = 0
+        # for i in range(m):
+        #     temp = 0
+        #     backtrack(i, 0, 0)
+        #     ans = max(ans, temp)
+
+        # return ans
+
+
+
