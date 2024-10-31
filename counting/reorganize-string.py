c72e1a20983660e70fdaa5462914757e44672030
Jeremy <dohoanggiahuy310703@gmail.com>
2024-10-30 23:11:41 -0400

    LC-reorganize-string [Runtime: N/A; Memory: N/A]

---
+++ counting/reorganize-string.py
@@ -0,0 +1,30 @@
+from collections import Counter
+
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        d = Counter(list(s))
+        h = []
+        ans = ""
+        prev = None
+        for u, v in d.items():
+            heappush(h, (-v, u))
+
+        while h or prev:
+            if not h and prev:
+                return ""
+            
+            v, u = heappop(h)
+
+            ans += u
+            v = -v
+            v -= 1
+
+            if prev:
+                heappush(h, prev)
+                prev = None
+            if v != 0:
+                prev = [-v, u]
+                
+
+
+        return ans
