c69f002d387ce536ba82a4aa725f08505ab83c58
Jeremy <dohoanggiahuy310703@gmail.com>
2024-10-30 22:13:55 -0400

    LC-partition-equal-subset-sum [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/partition-equal-subset-sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        s = sum(nums)
+
+        if s % 2 == 1:
+            return False
+
+        target = s // 2
+
+        nums.sort()
+
+        def addup(curr, i):
+            nonlocal ans
+
+            if curr == target:
+                ans = True
+                return
+
+            if curr > target:
+                return
+
+            if i >= len(nums):
+                return
+
+            addup(curr + nums[i], i + 1)
+            addup(curr, i + 1)
+        
+        ans = False
+        addup(0, 0)
+        return ans
