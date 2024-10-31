8e0d7d53f91e7e4ade132d05cae0da93a574b9c1
Jeremy <dohoanggiahuy310703@gmail.com>
2024-10-30 22:43:09 -0400

    LC-partition-equal-subset-sum [Runtime: 479 ms; Memory: 16.7 MB]

--- dynamic-programming/partition-equal-subset-sum.py
+++ dynamic-programming/partition-equal-subset-sum.py
@@ -3,37 +3,19 @@ from collections import Counter
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         s = sum(nums)
-
-        if s % 2 == 1:
+        if s % 2 == 1 or len(nums) == 1:
             return False
 
         target = s // 2
-        nums.sort()
+        dp = [False] * (target + 1)
+        dp[0] = True
+        curr_sum = 0
 
-
-        def addup(curr, tu, i):
-            nonlocal ans, nums, visited
-
-            # print(curr, tu, i)
-
-            if curr == target:
-                ans = True
-                return
-
-            if curr > target:
-                return
-
-            if i >= len(nums):
-                return
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
             
-            if tu in visited:
-                return
 
-            addup(curr + nums[i], tuple(list(tu) + [nums[i]]), i + 1)
-            addup(curr, tu, i + 1)
-            visited.add(tu)
-        
-        ans = False
-        visited = set()
-        addup(0, tuple(), 0)
-        return ans
+        # print(dp)
+
+        return dp[-1]
