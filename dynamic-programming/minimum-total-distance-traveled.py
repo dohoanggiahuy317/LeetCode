a07890c036fb1a5fa5306dfdaa10abee4275145e
Jeremy <dohoanggiahuy310703@gmail.com>
2024-10-30 21:07:31 -0400

    LC-minimum-total-distance-traveled [Runtime: 782 ms; Memory: 18.6 MB]

--- dynamic-programming/minimum-total-distance-traveled.py
+++ dynamic-programming/minimum-total-distance-traveled.py
@@ -3,37 +3,35 @@ class Solution:
         robot.sort()
         factory.sort()
 
-        dp = [[0 for w in range(len(robot))] for _ in range(len(factory))]
-        fact_stat = [[0 for w in range(len(robot))] for _ in range(len(factory))]
-
-        for i in range(len(factory)):
-            for j in range(len(robot)):
-                curr_dist = abs(robot[j] - factory[i][0])
-                fact_id = factory[i][0]
-
-                if i == 0:
-                    if j == 0:
-                        dp[i][j] = curr_dist
-                        fact_stat[0][0] = 1
-                    else:
-                        if fact_stat[i][j-1] < factory[0][1]:
-                            dp[i][j] = dp[i][j-1] + curr_dist
-                            fact_stat[0][j] = fact_stat[0][j-1] + 1
-                        else:
-                            dp[0][j] = float("INF")
-                
+        # calculate the total limit for first k factories and store them in the array
+        fac_prefix_sum = [0]
+        for _, limit in factory:
+            fac_prefix_sum.append(fac_prefix_sum[-1] + limit)
+            
+        # dp[i][j] is the minimumTotalDistance for assignning the first i robots to the first j factories
+        # when num_robots == 0, total distance should be 0 anyway
+        # when num_factories == 0, if there is any robot, we have no way to assign it, set value to -1.
+        dp = [[0] * (len(factory) + 1)] + [[-1] + [None] * len(factory) for _ in range(len(robot))]
+        def helper(num_robots, num_factories):
+            if dp[num_robots][num_factories] is None:
+                # when do not have enough limits to repair robot, set value to -1
+                if fac_prefix_sum[num_factories] < num_robots:
+                    dp[num_robots][num_factories] = -1
                 else:
-                    if j == 0:
-                        dp[i][j] = dp[i-1][j]
-                        fact_stat[0][0] = 1
-                    else:
-                        if fact_stat[i][j-1] < factory[0][1]:
-                            if dp[i-1][j] > dp[i][j-1] + curr_dist:
-                                dp[i][j] = dp[i][j-1] + curr_dist
-                                fact_stat[i][j] = fact_stat[i][j-1] + 1
-                            else:
-                                dp[i][j] = dp[i-1][j]
-                        else:
-                            dp[0][j] = float("INF")
-    
-        return dp[-1][-1]
+                    # get the position and limit of the right most factory
+                    idx, limit = factory[num_factories - 1]
+                    best = -1
+                    # total distance for all robot going to the right most factory
+                    cur_distance = 0
+
+                    # loop through number of robots assginning to the right most factory, max number is min(limit, num_robots)
+                    for i in range(min(num_robots, limit) + 1):
+                        cur = helper(num_robots - i, num_factories - 1)
+                        if cur >= 0:
+                            cur += cur_distance
+                            if best < 0 or cur < best:
+                                best = cur
+                        cur_distance += abs(robot[num_robots - 1 - i] - idx)
+                    dp[num_robots][num_factories] = best
+            return dp[num_robots][num_factories]
+        return helper(len(robot), len(factory))
