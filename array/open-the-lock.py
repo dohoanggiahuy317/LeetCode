ccb9fe1242fb1d9f03b7137a2022aa1c16412fdd
Jeremy <dohoanggiahuy310703@gmail.com>
2024-10-30 21:56:22 -0400

    LC-open-the-lock [Runtime: N/A; Memory: N/A]

--- array/open-the-lock.py
+++ array/open-the-lock.py
@@ -1,51 +1,44 @@
+from collections import Counter
+
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-
-        def bfs(source):
-            nonlocal d, visited, que
-
-            curr = source
-
-            while len(que) > 0:
-                curr = que.pop(0)
-
-                num11 = str((int(curr[0]) + 1) % 10) + curr[1:]
-                num12 = str((int(curr[0]) - 1) % 10) + curr[1:]
-                num21 = curr[0] + str((int(curr[1]) + 1) % 10) + curr[2:]
-                num22 = curr[0] + str((int(curr[1]) - 1) % 10) + curr[2:]
-                num31 = curr[:2] + str((int(curr[2]) + 1) % 10) + curr[3:]
-                num32 = curr[:2] + str((int(curr[2]) - 1) % 10) + curr[3:]
-                num41 = curr[:3] + str((int(curr[3]) + 1) % 10)
-                num42 = curr[:3] + str((int(curr[3]) - 1) % 10)
-                li = [num11, num12, num21, num22, num31, num32, num41, num42]
-
-                for adj in li:
-                    if visited[adj] == False:
-                        que.append(adj)
-                        d[adj] = d[curr] + 1
-                        visited[adj] = True
-
-                if curr == target:
-                    print("stop")
-                    return
-                
-        d = {}
-        visited = {}
-        que = ["0000"]
-        for i in range(0, 10000):
-            si = "0" * (4 - len(str(i))) + str(i)
-            visited[si] = False
-            d[si] = -1
-
-        visited["0000"] = True
+        
+        d = defaultdict(lambda: -1)
         d["0000"] = 0
-
+        visited = set(["0000"])
         for x in deadends:
-            visited[x] = True
+            visited.add(x)
+        q = ["0000"]
 
-        if "0000" in deadends:
-            return -1
+        def bfs(curr):
+            nonlocal d, visited, q
+
+            while len(q) > 0:
+                temp = q.pop(0)
+
+                num11 = str( (int(temp[0]) + 1) % 10 ) + temp[1:]
+                num12 = str( (int(temp[0]) + 9) % 10 ) + temp[1:]
+                num21 = temp[:1] + str( (int(temp[1]) + 1) % 10 ) + temp[2:]
+                num22 = temp[:1] + str( (int(temp[1]) + 9) % 10 ) + temp[2:]
+                num31 = temp[:2] + str( (int(temp[2]) + 1) % 10 ) + temp[3:]
+                num32 = temp[:2] + str( (int(temp[2]) + 9) % 10 ) + temp[3:]
+                num41 = temp[:3] + str( (int(temp[3]) + 1) % 10 )
+                num42 = temp[:3] + str( (int(temp[3]) + 9) % 10 )
+
+                li = [num11, num12, num21, num22, num31, num32, num41, num42]
+                for neigh in li:
+                    if neigh not in visited:
+                        visited.add(neigh)
+                        q.append(neigh)
+                        d[neigh] = d[temp] + 1
+
+                if temp == target:
+                    return
 
 
         bfs("0000")
         return d[target]
+
+        
+
+            
