774ffc3ac48d42a9929589d269fccb8cf3ad29ae
Jeremy <dohoanggiahuy310703@gmail.com>
2024-11-01 09:51:09 -0400

    LC-number-of-islands [Runtime: 219 ms; Memory: 18.8 MB]

--- matrix/number-of-islands.py
+++ matrix/number-of-islands.py
@@ -1,30 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
-        ans = 0
         visited = [[False] * len(grid[0]) for _ in range(len(grid))]
+        ans = 0
 
-        def backtrack(i, j):
-            nonlocal grid, visited
+        def dfs(i, j):
+            nonlocal visited
 
-            if visited[i][j] or grid[i][j] == "0":
+            if visited[i][j]:
                 return
-
+            
             visited[i][j] = True
-            if j > 0 and grid[i][j-1] == "1":
-                backtrack(i, j-1)
-            if j < len(grid[0]) - 1 and grid[i][j+1] == "1":
-                backtrack(i, j+1)
+
             if i > 0 and grid[i-1][j] == "1":
-                backtrack(i-1, j)
+                dfs(i - 1, j)
+            if j > 0 and grid[i][j-1] == "1":
+                dfs(i, j - 1)
             if i < len(grid) - 1 and grid[i+1][j] == "1":
-                backtrack(i+1, j)
+                dfs(i + 1, j)
+            if j < len(grid[0]) - 1 and grid[i][j+1] == "1":
+                dfs(i, j + 1)
 
-
+        ans = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and visited[i][j] == False:
+                if grid[i][j] == "1" and not visited[i][j]:
                     ans += 1
-                backtrack(i, j)
+                    dfs(i, j)
+
 
         return ans
