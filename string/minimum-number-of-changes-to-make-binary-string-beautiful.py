c04ab498b3248d148548a80f6aa0c06d0f486cea
Jeremy <dohoanggiahuy310703@gmail.com>
2024-11-05 15:21:40 -0500

    LC-minimum-number-of-changes-to-make-binary-string-beautiful [Runtime: N/A; Memory: N/A]

--- string/minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ string/minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -1,8 +1,10 @@
-def min_changes(s):
-    count = 0
-    i = 0
-    while i < len(s) - 1:
-        if s[i] != s[i + 1]:
-            count += 1
-        i += 2
-    return count
+class Solution:
+    def minChanges(self, s: str) -> int:
+        def min_changes(s):
+            count = 0
+            i = 0
+            while i < len(s) - 1:
+                if s[i] != s[i + 1]:
+                    count += 1
+                i += 2
+            return count
