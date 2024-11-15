001e11227c90c6bd21f937fa2efd116f9e1e12b6
Jeremy <dohoanggiahuy310703@gmail.com>
2024-11-14 23:13:45 -0500

    LC-shortest-subarray-to-be-removed-to-make-array-sorted [Runtime: N/A; Memory: N/A]

---
+++ array/shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+        dp = [1] * len(arr)
+        ans = float("INF")
+
+        for i in range(1, len(arr)):
+            for j in range(i-1, -1, -1):
+                if arr[j] <= arr[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+            
+            ans = min(ans, (len(arr) - 1 - i) + (i + 1 - dp[i]) )
+
+        # print(dp)
+        return ans
