45639fdcd0143aa6a2284ecc3aff82df496c008a
Jeremy <dohoanggiahuy310703@gmail.com>
2024-11-20 19:36:36 -0500

    LC-count-unguarded-cells-in-the-grid [Runtime: N/A; Memory: N/A]

--- matrix/count-unguarded-cells-in-the-grid.py
+++ matrix/count-unguarded-cells-in-the-grid.py
@@ -1,39 +1,42 @@
-from collections import namedtuple
-from typing import List
+class Solution:
+    from collections import namedtuple
+    from typing import List
 
-Directions = namedtuple('Directions', ['x', 'y'])
-up = Directions(-1, 0)
-down = Directions(1, 0)
-left = Directions(0, -1)
-right = Directions(0, 1)
+    Directions = namedtuple('Directions', ['x', 'y'])
+    up = Directions(-1, 0)
+    down = Directions(1, 0)
+    left = Directions(0, -1)
+    right = Directions(0, 1)
 
 
-def countUnguarded(m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-    matrix = [[0 for _ in range(n)] for _ in range(m)]
-    for x, y in guards:
-        matrix[x][y] = 'G'
-    for x, y in walls:
-        matrix[x][y] = 'W'
+    def countUnguarded(m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        matrix = [[0 for _ in range(n)] for _ in range(m)]
+        for x, y in guards:
+            matrix[x][y] = 'G'
+        for x, y in walls:
+            matrix[x][y] = 'W'
 
-    def dfs(row, col, direction):
-        if row < 0 or row >= m or col < 0 or col >= n or matrix[row][col] == 'W' or matrix[row][col] == 'G':
-            return
+        def dfs(row, col, direction):
+            if row < 0 or row >= m or col < 0 or col >= n or matrix[row][col] == 'W' or matrix[row][col] == 'G':
+                return
 
-        matrix[row][col] = 'X'
-        dfs(row + direction.x, col + direction.y, direction)
+            matrix[row][col] = 'X'
+            dfs(row + direction.x, col + direction.y, direction)
 
-    for x in range(m):
-        for y in range(n):
-            if matrix[x][y] == 'G':
-                dfs(x + 1, y, down)
-                dfs(x - 1, y, up)
-                dfs(x, y + 1, right)
-                dfs(x, y - 1, left)
+        for x in range(m):
+            for y in range(n):
+                if matrix[x][y] == 'G':
+                    dfs(x + 1, y, down)
+                    dfs(x - 1, y, up)
+                    dfs(x, y + 1, right)
+                    dfs(x, y - 1, left)
 
-    counter = 0
-    for x in range(m):
-        for y in range(n):
-            if matrix[x][y] == 0:
-                counter += 1
+        counter = 0
+        for x in range(m):
+            for y in range(n):
+                if matrix[x][y] == 0:
+                    counter += 1
 
-    return counter
+        return counter
+
+            
