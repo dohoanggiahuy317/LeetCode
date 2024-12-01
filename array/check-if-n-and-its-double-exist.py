4f4b91a2388b58a90825ba97453b233a38c783fb
Jeremy <dohoanggiahuy310703@gmail.com>
2024-11-30 23:16:22 -0500

    LC-check-if-n-and-its-double-exist [Runtime: N/A; Memory: N/A]

--- array/check-if-n-and-its-double-exist.py
+++ array/check-if-n-and-its-double-exist.py
@@ -1,5 +1,7 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
+
+        arr.sort(reverse = True)
         
         for i in range(len(arr)):
             if arr[i] % 2 == 0 and arr[i] // 2 in set(arr[i+1:]):
