440996798e5d6643851187088f1a5f4a0b09c4be
Jeremy <dohoanggiahuy310703@gmail.com>
2024-12-12 00:03:45 -0500

    LC-course-schedule-ii [Runtime: N/A; Memory: N/A]

--- breadth-first-search/course-schedule-ii.py
+++ breadth-first-search/course-schedule-ii.py
@@ -6,9 +6,26 @@ class Solution:
         m = defaultdict(list)
         for a, b in prerequisites:
             m[b].append(a)
+
+        def cyc(i):
+            nonlocal rec_visit
+            rec_visit.add(i)
+            for ne in m[i]:
+                if ne in rec_visit:
+                    return False
+                return cyc(ne)
+                
+            rec_visit.remove(i)
+            return True
+
+        for i in range(numCourses):
+            rec_visit = set()
+            if cyc(i) == False:
+                return []
+
         
         def dfs(i):
-            nonlocal visited, stack, m
+            nonlocal visited, stack, m, b
             visited.add(i)
 
             for ne in m[i]:
