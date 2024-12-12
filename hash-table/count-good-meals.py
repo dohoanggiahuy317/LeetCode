d92f21e847188eaf5ad0d004fed70bb9bdeb34b1
Jeremy <dohoanggiahuy310703@gmail.com>
2024-12-11 21:01:18 -0500

    LC-count-good-meals [Runtime: N/A; Memory: N/A]

--- hash-table/count-good-meals.py
+++ hash-table/count-good-meals.py
@@ -4,16 +4,30 @@ import math
 class Solution:
     def countPairs(self, deliciousness: List[int]) -> int:
         if len(deliciousness) == 1:
-            if math.log(deliciousness[-1]).isdigit():
+            if math.log(deliciousness[-1]) - int(math.log(deliciousness[-1])) == 0:
                 return 1
             return 0
 
-        return 0
+        freq = Counter()
+        for deli in deliciousness:
+            freq[deli] += 1
 
-        # freq = Counter()
-        # for deli in deliciousness:
-        #     freq[deli] += 1
+        deliciousness.sort()
+        max_base = int(math.log(deliciousness[-1] + deliciousness[-2], 2))
+        ans = 0
 
-        # deliciousness.sort()
-        # max_base = int(Math.log(deliciousness[-1] + ))
+        # print(max_base, freq)
+
+        for i in range(max_base + 1):
+            curr_sum = 2 ** i
+            
+            for k, v in freq.items():
+                target = curr_sum - k
+                if target in freq:
+                    if target == k:
+                        ans += v * (v-1) //2
+                    else:
+                        ans += v * freq[target] / 2
+
+        return int(ans)
         
