29a26a496cae35bacbfb64103f94489de887a0e5
Jeremy <dohoanggiahuy310703@gmail.com>
2024-12-12 00:17:48 -0500

    LC-take-gifts-from-the-richest-pile [Runtime: 3 ms; Memory: 17.3 MB]

--- heap-priority-queue/take-gifts-from-the-richest-pile.py
+++ heap-priority-queue/take-gifts-from-the-richest-pile.py
@@ -5,9 +5,11 @@ class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
         
         gifts = list(map(lambda x: -x, gifts))
+        heapify(gifts)
 
         for i in range(k):
-            curr_gift = -heapq.heappop(gifts)
-            heapq.heappush(gifts, -int(math.sqrt(curr_gift)) )
+            curr_gift = -heappop(gifts)
+            
+            heappush(gifts, -int(math.sqrt(curr_gift)) )
         
         return -sum(gifts)
