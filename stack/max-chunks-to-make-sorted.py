98d237a199f89c4225cfd805364872cfacc3a6d4
Jeremy <dohoanggiahuy310703@gmail.com>
2024-12-19 14:37:49 -0500

    LC-max-chunks-to-make-sorted [Runtime: 0 ms; Memory: 18 MB]

--- stack/max-chunks-to-make-sorted.py
+++ stack/max-chunks-to-make-sorted.py
@@ -1,25 +1,19 @@
 class Solution:
-    def maxChunksToSorted(self, arr: List[int]) -> int:
+    def maxChunksToSorted(self, arr: list[int]) -> int:
+        n = len(arr)
+        chunks = 0
+        prefix_sum = 0
+        sorted_prefix_sum = 0
 
-        if len(arr) == 1:
-            return 1
-        
-        ans = 0
-        i = 0
+        # Iterate over the array
+        for i in range(n):
+            # Update prefix sum of `arr`
+            prefix_sum += arr[i]
+            # Update prefix sum of the sorted array
+            sorted_prefix_sum += i
 
-        while i < len(arr)-1:
-            if arr[i] < arr[i + 1]:
-                ans += 1
-                i += 1
-                continue
-            
-            j = i + 1
-            while j < len(arr) and arr[j] < arr[i]:
-                j += 1
+            # If the two sums are equal, the two prefixes contain the same elements; a chunk can be formed
+            if prefix_sum == sorted_prefix_sum:
+                chunks += 1
 
-            ans += 1
-            i = j
-
-        # print(i)
-
-        return ans + 1 if i == len(arr) - 1 else ans
+        return chunks
