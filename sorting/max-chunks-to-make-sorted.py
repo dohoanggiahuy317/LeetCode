263d5c0105fc5a07e57285acfedee8f04b9c8611
Jeremy <dohoanggiahuy310703@gmail.com>
2024-12-19 09:27:41 -0500

    LC-max-chunks-to-make-sorted [Runtime: N/A; Memory: N/A]

---
+++ sorting/max-chunks-to-make-sorted.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+
+        if len(arr) == 1:
+            return 1
+        
+        ans = 0
+        i = 0
+
+        while i < len(arr)-1:
+            if arr[i] < arr[i + 1]:
+                ans += 1
+                i += 1
+                continue
+            
+            j = i + 1
+            while j < len(arr) and arr[j] < arr[i]:
+                j += 1
+
+            ans += 1
+            i = j
+
+        return ans + 1 if arr[-2] < arr[-1] else ans
