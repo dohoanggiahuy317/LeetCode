467a1b9bf2e9420361b6a81e58cfc46e3dc60958
Jeremy <dohoanggiahuy310703@gmail.com>
2025-01-03 21:29:33 -0500

    LC-unique-length-3-palindromic-subsequences [Runtime: 125 ms; Memory: 18.9 MB]

--- hash-table/unique-length-3-palindromic-subsequences.py
+++ hash-table/unique-length-3-palindromic-subsequences.py
@@ -1,16 +1,14 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
         ans = 0
+
         for i in range(26):
             char = chr(i + ord("a"))
+
             if char in s:
-                fi = s.index(char)
-                la = s.rindex(char)
+                l, r = s.index(char), s.rindex(char)
 
-                if fi != la:
-                    rest = set()
-                    for x in list(s[fi + 1:la]):
-                        rest.add(x)
-                    ans += len(rest)
-
+                if r != l:
+                    ans += len(set(list(s[l+1: r])))
+        
         return ans
