6b660173a652a6345707bb5a400cbe29ef3844dd
Jeremy <dohoanggiahuy310703@gmail.com>
2025-01-10 21:49:35 -0500

    LC-construct-k-palindrome-strings [Runtime: 119 ms; Memory: 18 MB]

--- string/construct-k-palindrome-strings.py
+++ string/construct-k-palindrome-strings.py
@@ -10,7 +10,7 @@ class Solution:
             if v % 2 == 1:
                 ans += 1
 
-        if len(d) < k:
+        if sum(list(d.values())) < k:
             return False
 
         return True if ans <= k else False
