2c941f96b89a43f5a7e708160c6ca56858d1fd11
Jeremy <dohoanggiahuy310703@gmail.com>
2025-01-10 21:48:19 -0500

    LC-construct-k-palindrome-strings [Runtime: N/A; Memory: N/A]

---
+++ hash-table/construct-k-palindrome-strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        d = Counter()
+
+        for i in range(len(s)):
+            d[s[i]] += 1
+
+        ans = 0
+        for u, v in d.items():
+            if v % 2 == 1:
+                ans += 1
+
+        return True if ans <= k else False
