b6ce1f9cbcb8f7301c3ac592be4e1185ecb9ab66
Jeremy <dohoanggiahuy310703@gmail.com>
2025-01-19 21:34:13 -0500

    LC-first-completely-painted-row-or-column [Runtime: 128 ms; Memory: 53.4 MB]

--- array/first-completely-painted-row-or-column.py
+++ array/first-completely-painted-row-or-column.py
@@ -1,35 +1,25 @@
 class Solution:
-    def firstCompleteIndex(self, arr, mat):
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
         num_rows, num_cols = len(mat), len(mat[0])
+        row_count, col_count = [0] * num_rows, [0] * num_cols
         num_to_pos = {}
-        # Populate the num_to_pos map by iterating over the matrix
+
+        # Create a map to store the position of each number in the matrix
         for row in range(num_rows):
             for col in range(num_cols):
-                value = mat[row][col]
-                num_to_pos[value] = (row, col)
+                num_to_pos[mat[row][col]] = [row, col]
 
         for i in range(len(arr)):
             num = arr[i]
             row, col = num_to_pos[num]
-            mat[row][col] = -mat[row][col]  # mark as seen
+
+            # Increment the count for the corresponding row and column
+            row_count[row] += 1
+            col_count[col] += 1
 
             # Check if the row or column is completely painted
-            if self._check_row(row, mat) or self._check_column(col, mat):
+            if row_count[row] == num_cols or col_count[col] == num_rows:
                 return i
 
         # This line will never be reached as per the problem statement
         return -1
-
-    def _check_row(self, row, mat):
-        # Return true if row is completely seen
-        for col in range(len(mat[0])):
-            if mat[row][col] > 0:
-                return False
-        return True
-
-    def _check_column(self, col, mat):
-        # Return true if col is completely seen
-        for row in range(len(mat)):
-            if mat[row][col] > 0:
-                return False
-        return True
