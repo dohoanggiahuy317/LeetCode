73905ca7026382f431cf42d0601861bfb2a7f0e2
Jeremy <dohoanggiahuy310703@gmail.com>
2025-01-24 23:29:30 -0500

    LC-make-lexicographically-smallest-array-by-swapping-elements [Runtime: 456 ms; Memory: 54.3 MB]

--- union-find/make-lexicographically-smallest-array-by-swapping-elements.py
+++ union-find/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,13 +1,36 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        
-        ans = []
 
-        for i in range(len(nums)):
-            ans.append(nums[i])
-            for j in range(i):
-                if 0 < nums[j] - nums[i] <= limit:
-                    ans[i], ans[j] = ans[j], ans[i]
-                    break
+        indx_nums = list(zip([i for i in range(len(nums))], nums))
+        snums = sorted(indx_nums, key=lambda x: x[1])
+
+
+        num_gr = 0
+        groups = {0: [snums[0]]}
+
+
+        for i in range(1, len(snums)):
+            if snums[i][1] - snums[i-1][1] > limit:
+                num_gr += 1
+
+            if num_gr not in groups:
+                groups[num_gr] = []
+            groups[num_gr].append( snums[i] )
+
+        for _, v in groups.items():
+            indx_li = list(map(lambda x: x[0], v))
+            val_li = list(map(lambda x: x[1], v))
+
+            val_li.sort()
+            indx_li.sort()
+
+            # print(indx_li, val_li)
+
+            for i in range(len(indx_li)):
+                nums[ indx_li[i] ] = val_li[i]
+
+        return nums
+  
+
+                
         
-        return ans
