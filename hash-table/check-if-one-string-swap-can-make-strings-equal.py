0301e97300c1b9335cb76e4b0b1b290d85c66ef7
Jeremy <dohoanggiahuy310703@gmail.com>
2025-02-04 22:18:03 -0500

    LC-check-if-one-string-swap-can-make-strings-equal [Runtime: N/A; Memory: N/A]

---
+++ hash-table/check-if-one-string-swap-can-make-strings-equal.py
@@ -0,0 +1,21 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        c = 0
+        prev = -1
+
+        for i in range(len(s1)):
+            if c > 2:
+                return False
+
+            if s1[i] != s2[i]:
+                c += 1
+
+                if c == 1:
+                    prev = i
+                elif c == 2:
+                    if s1[prev] != s2[i] or s1[i] != s2[prev]:
+                        return False
+
+        return True
+
+        
