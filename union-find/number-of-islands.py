cceb2a9babcf600f8d20dadbe278ab1c49dd422f
Jeremy <dohoanggiahuy310703@gmail.com>
2025-02-09 22:43:47 -0500

    LC-number-of-islands [Runtime: 259 ms; Memory: 20.3 MB]

--- union-find/number-of-islands.py
+++ union-find/number-of-islands.py
@@ -4,28 +4,27 @@ class Solution:
         ans = 0
 
         def dfs(i, j):
-            nonlocal visited
+            nonlocal grid, visited
 
-            if visited[i][j]:
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+                return
+
+            if grid[i][j] == "0" or visited[i][j]:
                 return
             
             visited[i][j] = True
 
-            if i > 0 and grid[i-1][j] == "1":
-                dfs(i - 1, j)
-            if j > 0 and grid[i][j-1] == "1":
-                dfs(i, j - 1)
-            if i < len(grid) - 1 and grid[i+1][j] == "1":
-                dfs(i + 1, j)
-            if j < len(grid[0]) - 1 and grid[i][j+1] == "1":
-                dfs(i, j + 1)
+            dfs(i-1, j)
+            dfs(i+1, j)
+            dfs(i, j-1)
+            dfs(i, j+1)
 
-        ans = 0
+            
+
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and not visited[i][j]:
+                if not visited[i][j] and grid[i][j] == "1":
                     ans += 1
                     dfs(i, j)
-
 
         return ans
