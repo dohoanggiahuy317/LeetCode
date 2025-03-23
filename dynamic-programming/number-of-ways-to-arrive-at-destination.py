966e5caa47e61187c02dab9d8891a8f978537845
Jeremy <giahuy.jeremy.do@gmail.com>
2025-03-23 01:01:09 -0400

    LC-number-of-ways-to-arrive-at-destination [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/number-of-ways-to-arrive-at-destination.py
@@ -0,0 +1,34 @@
+import heapq
+from collections import defaultdict
+
+class Solution:
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        MOD = 10**9 + 7
+        m = defaultdict(list)
+
+        for u, v, val in roads:
+            m[u].append((val, v))
+            m[v].append((val, u))
+        
+        ans = 0
+        nodes = [float("INF")] * n
+        tonodes = [1] * n
+        tonodes[0] = 0
+        nodes[0] = 1
+        q = [(0, 0)]
+
+        while q:
+            dist, curr = heapq.heappop(q)
+
+            for val, neigh in m[curr]:
+                if nodes[neigh] == nodes[curr] + val:
+                    tonodes[neigh] = (tonodes[neigh] + tonodes[curr]) % MOD
+
+                if nodes[neigh] > nodes[curr] + val:
+                    tonodes[neigh] = 1
+                    nodes[neigh] = nodes[curr] + val
+                    heapq.heappush(q, (nodes[neigh], neigh))
+
+        # print(tonodes)
+                
+        return tonodes[n-1] % MOD
