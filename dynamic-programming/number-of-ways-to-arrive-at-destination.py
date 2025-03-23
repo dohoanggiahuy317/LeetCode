d3f35f6e6904bc1151084d555e20ebcc07bf3fed
Jeremy <giahuy.jeremy.do@gmail.com>
2025-03-23 01:18:08 -0400

    LC-number-of-ways-to-arrive-at-destination [Runtime: 20 ms; Memory: 25 MB]

--- dynamic-programming/number-of-ways-to-arrive-at-destination.py
+++ dynamic-programming/number-of-ways-to-arrive-at-destination.py
@@ -12,23 +12,34 @@ class Solution:
         
         ans = 0
         nodes = [float("INF")] * n
-        tonodes = [1] * n
+        visited = [False] * n
+        tonodes = [100] * n
         tonodes[0] = 1
-        nodes[0] = 1
+        nodes[0] = 0
         q = [(0, 0)]
 
         while q:
             dist, curr = heapq.heappop(q)
-
+            # print(curr)
+            # print(nodes)
+            if visited[curr]:
+                continue
+            visited[curr] = True
             for val, neigh in m[curr]:
-                if nodes[neigh] == nodes[curr] + val:
-                    tonodes[neigh] = (tonodes[neigh] + tonodes[curr]) % MOD
+                if not visited[neigh]:
+                    if nodes[neigh] == nodes[curr] + val:
+                        tonodes[neigh] = (tonodes[neigh] + tonodes[curr]) % MOD
 
-                if nodes[neigh] > nodes[curr] + val:
-                    tonodes[neigh] = tonodes[curr] % MOD
-                    nodes[neigh] = nodes[curr] + val
-                    heapq.heappush(q, (nodes[neigh], neigh))
-
-        # print(tonodes)
+                    if nodes[neigh] > nodes[curr] + val:
+                        tonodes[neigh] = tonodes[curr] % MOD
+                        nodes[neigh] = nodes[curr] + val
+                        heapq.heappush(q, (nodes[neigh], neigh))
+            # print(nodes)
+            # print(tonodes)
                 
         return tonodes[n-1] % MOD
+
+
+    # 0 --- 4
+    # |       \ 
+    # 1 --- 2--3
