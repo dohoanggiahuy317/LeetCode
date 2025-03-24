22edf2aaefbf263c75c803153b054c8f9ccfcbef
Jeremy <giahuy.jeremy.do@gmail.com>
2025-03-23 22:26:21 -0400

    LC-count-days-without-meetings [Runtime: 215 ms; Memory: 52.7 MB]

--- array/count-days-without-meetings.py
+++ array/count-days-without-meetings.py
@@ -1,26 +1,20 @@
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
-        if days == 1000000000:
-            if meetings == [[1,1]]:
-                return 999999999
-            if meetings == [[1,1000000000]]:
-                return 0
-            if meetings[0] == [42,498560067]:
-                return 2088
-            return 4017
-        
-        nm = sorted(meetings, key = lambda x: (x[0], x[1]))
-        # print(nm)
-        i = 1
-        ans = 0
-        while i < days+1:
-            # print(i)
-            # print(nm)
-            # print(ans)
-            while nm and (nm[0][0] <= i <= nm[0][1] or nm[0][1] <= i) :
-                i = max(i, nm[0][1] + 1)
-                nm.pop(0)
-            if i < days+1:
-                ans += 1
-            i+=1
-        return ans
+        free_days = 0
+        latest_end = 0
+
+        # Sort meetings based on starting times
+        meetings.sort()
+
+        for start, end in meetings:
+            # Add current range of days without a meeting
+            if start > latest_end + 1:
+                free_days += start - latest_end - 1
+
+            # Update latest meeting end
+            latest_end = max(latest_end, end)
+
+        # Add all days after the last day of meetings
+        free_days += days - latest_end
+
+        return free_days
