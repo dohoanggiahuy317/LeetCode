0b28dcd22acf0ef817b877dba7b1721b5115b63e
Jeremy <giahuy.jeremy.do@gmail.com>
2025-03-23 22:16:40 -0400

    LC-count-days-without-meetings [Runtime: N/A; Memory: N/A]

--- sorting/count-days-without-meetings.py
+++ sorting/count-days-without-meetings.py
@@ -1,12 +1,12 @@
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
         nm = sorted(meetings, key = lambda x: (x[0], x[1]))
-
+        print(nm)
         i = 1
         ans = 0
         while i < days+1:
-            # print(i)
-            while nm and nm[0][0] <= i <= nm[0][1]:
+            print(i)
+            while nm and (nm[0][0] <= i <= nm[0][1] or nm[0][1] <= i) :
                 i = nm[0][1] + 1
                 nm.pop(0)
             if i < days+1:
