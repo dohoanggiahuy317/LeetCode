78a6b6d36a3c37bfe0e6d54e1d3b5662eebdc80d
Jeremy <giahuy.jeremy.do@gmail.com>
2025-03-24 21:31:55 -0400

    LC-check-if-grid-can-be-cut-into-sections [Runtime: 3315 ms; Memory: 85 MB]

--- sorting/check-if-grid-can-be-cut-into-sections.py
+++ sorting/check-if-grid-can-be-cut-into-sections.py
@@ -2,24 +2,21 @@ class Solution:
     def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
         
         def checkcuts(arr):
-            sarr = sorted(arr, key = lambda x: (x[0], x[1]))
+            sarr = sorted(arr, key = lambda x: (x[0], -x[1]))
             ans = 0
 
-            curr_end = sarr.pop(0)[1]
-            last_start = sarr.pop(-1)[0]
+            curr_start, curr_end = sarr.pop(0)
 
-            while arr:
+            while sarr:
+
                 if ans >= 2:
                     return True
 
-                start, end = arr.pop(0)
+                start, end = sarr.pop(0)
                 if start >= curr_end:
                     ans += 1
                 
                 curr_end = max(end, curr_end)
-
-                if not arr and end <= last_start:
-                    ans += 1
 
             return True if ans >= 2 else False
 
