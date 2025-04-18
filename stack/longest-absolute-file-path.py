5a90e03499e1bea5871480654bc8a98111530a4e
Jeremy <giahuy.jeremy.do@gmail.com>
2025-04-17 22:52:00 -0400

    LC-longest-absolute-file-path [Runtime: 0 ms; Memory: 18 MB]

--- stack/longest-absolute-file-path.py
+++ stack/longest-absolute-file-path.py
@@ -1,33 +1,27 @@
 class Solution:
     def lengthLongestPath(self, input: str) -> int:
-        inp_li = input.split("\n")
-        depth = [0] * len(inp_li)
+        whole = input.split("\n")
+        temp = list(map(lambda x: len(x.split("\t")), whole))
+        val = list(map(lambda x: x.split("\t")[-1], whole))
+ 
+        ans = []
+        hold = []
+        res = 0
 
-        def count_depth(s):
-            count = 0
-            for i in range(len(s)):
-                if s[i:].startswith("\t"):
-                    count += 1
-                else:
-                    break
+        for i in range(len(temp)):
+            while ans and ans[-1] >= temp[i]:
+                ans.pop(-1)
+                hold.pop(-1)
+            
+            ans.append(temp[i])
+            hold.append(val[i])
 
-            return count
+            if "." in val[i]:
+                res = max(res, len("/".join(hold)))
+                continue
 
-        for i in range(len(depth)):
-            depth[i] = count_depth(inp_li[i])
+        return res
 
-        temp = []
-        curr = []
-        ans = 0
-        for i in range(len(depth)):
-            while curr and curr[-1] >= depth[i]:
-                curr.pop()
-                temp.pop()
+        
 
-
-            temp.append(inp_li[i][depth[i]:])
-            curr.append(depth[i])
-            if "." in temp[-1]:
-                ans = max(ans, len("/".join(temp)))
-
-        return ans
+        
