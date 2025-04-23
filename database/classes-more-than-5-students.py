c0f8ea94092118932940a5b527b59053cf8ba868
Jeremy <giahuy.jeremy.do@gmail.com>
2025-04-23 12:53:16 -0400

    LC-classes-more-than-5-students [Runtime: 369 ms; Memory: 68.3 MB]

---
+++ database/classes-more-than-5-students.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    courses = courses.groupby(by='class')['student'].size().reset_index()
+    print(courses)
+    courses = courses[courses["student"] >= 5]
+    return courses[['class']]
