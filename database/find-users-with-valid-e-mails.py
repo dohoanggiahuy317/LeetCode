e879ef52cc3d6e93f92851e0c39e81d6f7b0d185
Jeremy <giahuy.jeremy.do@gmail.com>
2025-04-23 00:54:06 -0400

    LC-find-users-with-valid-e-mails [Runtime: 296 ms; Memory: 67.2 MB]

--- database/find-users-with-valid-e-mails.py
+++ database/find-users-with-valid-e-mails.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
     def check(mail):
-        if mail[0].isalpha() and mail.endswith("@leetcode.com"):
+        if mail[0].isalpha() and "@" in mail and mail[mail.index("@"):] == "@leetcode.com":
             for i in mail[:mail.index("@")]:
                 if not (i.isalnum() or i in "_.-"):
                     return False
