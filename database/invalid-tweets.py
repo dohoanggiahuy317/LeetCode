781e44b9d1e5ce44761d6162e28f393c7b406520
Jeremy <giahuy.jeremy.do@gmail.com>
2025-04-24 02:13:47 -0400

    LC-invalid-tweets [Runtime: N/A; Memory: N/A]

--- database/invalid-tweets.py
+++ database/invalid-tweets.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
-    return tweets[tweets["content"].str.size() >= 15][["tweet_id"]]
+    return tweets[tweets["content"].str.len() >= 15][["tweet_id"]]
