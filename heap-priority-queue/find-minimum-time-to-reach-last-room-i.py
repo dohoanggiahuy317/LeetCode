932f361adc0894e2541baf7982f5efb64d3e9140
Jeremy <giahuy.jeremy.do@gmail.com>
2025-05-07 00:39:31 -0400

    LC-find-minimum-time-to-reach-last-room-i [Runtime: 144 ms; Memory: 18.3 MB]

--- heap-priority-queue/find-minimum-time-to-reach-last-room-i.py
+++ heap-priority-queue/find-minimum-time-to-reach-last-room-i.py
@@ -1,16 +1,39 @@
+class State:
+    def __init__(self, x, y, dis):
+        self.x = x
+        self.y = y
+        self.dis = dis
+
+    def __lt__(self, other):
+        return self.dis < other.dis
+
+
 class Solution:
-    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        dp = [[-float("INF")] * len(moveTime[0]) for _ in range(len(moveTime))]
+    def minTimeToReach(self, moveTime):
+        n = len(moveTime)
+        m = len(moveTime[0])
+        inf = float("inf")
+        d = [[inf] * m for _ in range(n)]
+        v = [[0] * m for _ in range(n)]
 
-        dp[0][0] = 0
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-        for i in range(1, len(moveTime)):
-            dp[i][0] = max(dp[i-1][0] + 1, moveTime[i][0] + 1)
-        for j in range(1, len(moveTime[0])):
-            dp[0][j] = max(dp[0][j-1] + 1, moveTime[0][j] + 1)
+        d[0][0] = 0
+        q = []
+        heapq.heappush(q, State(0, 0, 0))
 
-        for i in range(1, len(moveTime)):
-            for j in range(1, len(moveTime[0])):
-                dp[i][j] = max( moveTime[i][j] + 1, min(dp[i][j-1] + 1, dp[i-1][j] + 1) )
+        while q:
+            s = heapq.heappop(q)
+            if v[s.x][s.y]:
+                continue
+            v[s.x][s.y] = 1
+            for dx, dy in dirs:
+                nx, ny = s.x + dx, s.y + dy
+                if not (0 <= nx < n and 0 <= ny < m):
+                    continue
+                dist = max(d[s.x][s.y], moveTime[nx][ny]) + 1
+                if d[nx][ny] > dist:
+                    d[nx][ny] = dist
+                    heapq.heappush(q, State(nx, ny, dist))
 
-        return dp[-1][-1]
+        return d[n - 1][m - 1]
