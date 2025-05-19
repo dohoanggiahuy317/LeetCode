88263d318976b1a36d3cbcceb05630423a33d7cf
Jeremy <giahuy.jeremy.do@gmail.com>
2025-05-18 23:34:57 -0400

    LC-max-consecutive-ones-iii [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/max-consecutive-ones-iii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        num0 = k
+        ans = 0
+        curr = 0
+        trail = 0
+
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                curr += 1
+                ans = max(ans, curr)
+                # print(trail, i, curr)
+                continue
+            
+            else:
+                if num0 > 0:
+                    curr += 1
+                    num0 -= 1
+                    # print(trail, i, curr)
+                    continue
+                else:
+                    while nums[trail] == 1:
+                        trail += 1
+                        curr -= 1
+                    trail += 1
+                    
+                    num0 += 1
+                    curr -= 1
+                    
+                    curr += 1
+                    num0 -= 1
+                    # print(trail, i, curr)
+
+        return ans
+
+
