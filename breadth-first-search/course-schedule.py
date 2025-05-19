1e6b643d885fa1f8f3a9bc0b1d72844ba3131494
Jeremy <giahuy.jeremy.do@gmail.com>
2025-05-18 22:59:40 -0400

    LC-course-schedule [Runtime: 1241 ms; Memory: 19.4 MB]

--- breadth-first-search/course-schedule.py
+++ breadth-first-search/course-schedule.py
@@ -11,24 +11,28 @@ class Solution:
         #     print(u, v)
 
         def dfs(curr_node, visited):
-            nonlocal graph, cyc
+            nonlocal graph, cyc, checked
+
+            if checked[curr_node]:
+                return
 
             if cyc:
                 return
 
             if visited[curr_node]:
-
                 cyc = True
                 return
 
             visited[curr_node] = True
-
+            
             for neigh in graph[curr_node]:
                 dfs(neigh, visited)
 
             visited[curr_node] = False
+            checked[curr_node] = True
 
         for start in range(numCourses):
+            checked = [False] * numCourses
             visited = [False] * numCourses
             cyc = False
 
