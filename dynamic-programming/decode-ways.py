7420e67e48037163baeb3d6f54c5d3d42eab3a91
Jeremy <giahuy.jeremy.do@gmail.com>
2025-05-18 23:14:00 -0400

    LC-decode-ways [Runtime: N/A; Memory: N/A]

--- dynamic-programming/decode-ways.py
+++ dynamic-programming/decode-ways.py
@@ -1,22 +1,26 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        
-        if s[0] == "0":
-            return 0
-        if len(s) == 1:
-            return 1
+        dp = [1] * (len(s) + 1)
 
-        dp = [0] * (len(s) + 1)
-        dp[-1] = 1
-
-        for i in range(len(s)-1, -1, -1):
+        for i in range(len(s) - 1, -1, -1):
             if s[i] == "0":
                 dp[i] = 0
-            else:
-                dp[i] = dp[i+1]
-
-            if i+ 1 < len(s) and (s[i] == "1" or s[i] == '2' and s[i+1] in "0123456"):
-                dp[i] += dp[i+2]
-
-    
+            if s[i] == "1":
+                if i < len(s) - 1 and s[i+1] == "0":
+                    dp[i] = dp[i+2]
+                elif i < len(s) - 1:
+                    dp[i] = dp[i+1] + dp[i+2]
+                else:
+                    dp[i] = dp[i+1]
+            if s[i] == "2":
+                if i < len(s) - 1 and s[i+1] == "0":
+                    dp[i] = dp[i+2]
+                elif i < len(s) - 1 and s[i+1] in "789":
+                    dp[i] = dp[i+1]
+                elif i < len(s) - 1:
+                    dp[i] = dp[i+1] + dp[i+2]
+                else:
+                    dp[i] = dp[i+1]
+            
+        print(dp)
         return dp[0]
