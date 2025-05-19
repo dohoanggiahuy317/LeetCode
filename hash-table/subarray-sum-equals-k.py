539c84ea26492b17fa727fc3a2a5dafe885b9719
Jeremy <giahuy.jeremy.do@gmail.com>
2025-05-19 14:37:03 -0400

    LC-subarray-sum-equals-k [Runtime: 56 ms; Memory: 20.4 MB]

--- hash-table/subarray-sum-equals-k.py
+++ hash-table/subarray-sum-equals-k.py
@@ -1,37 +1,18 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        dp = [0] * (len(nums) + 1)
+        d = Counter()
+        d[0] = 1
+        curr_sum = 0
+        ans = 0
 
         for i in range(len(nums)):
-            dp[i + 1] = dp[i] + nums[i]
+            curr_sum += nums[i]
 
-        freq = []
-        curr = 0
-        cnt = 1
+            diff = curr_sum - k
+            if diff in d:
+                ans += d[diff]
+            
+            d[curr_sum] += 1
 
-        for i in range(1, len(dp)):
-            if dp[i] != curr:
-                freq.append((curr, cnt))
-                cnt = 1
-                curr = dp[i]
-            else:
-                cnt += 1
-        freq.append((curr, cnt))
-
-        ans = 0
-        # print(dp)
-        # print(freq)
-        if k == 0:
-            for i in range(len(freq)):
-                if freq[i][0] == 0 and i != 0:
-                    ans += freq[i][1] * (freq[i][1] - 1) // 2
-                for j in range(i+1, len(freq)):
-                    if freq[j][0] - freq[i][0] == 0:
-                        ans += freq[j][1] * freq[i][1]
-        else:
-            for i in range(len(freq)):
-                for j in range(i+1, len(freq)):
-                    if freq[j][0] - freq[i][0] == k:
-                        ans += freq[j][1] * freq[i][1]
-                
         return ans
+
