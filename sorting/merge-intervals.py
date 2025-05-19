e0bfb58545e814ce9f399903d5a29e7c546dc37e
Jeremy <giahuy.jeremy.do@gmail.com>
2025-05-18 22:27:26 -0400

    LC-merge-intervals [Runtime: 19 ms; Memory: 20.8 MB]

--- sorting/merge-intervals.py
+++ sorting/merge-intervals.py
@@ -1,17 +1,18 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-
         intervals.sort(key=lambda x: x[0])
+        ans = []
+        curr_int = intervals.pop(0)
 
-        merged = []
-        for interval in intervals:
-            # if the list of merged intervals is empty or if the current
-            # interval does not overlap with the previous, simply append it.
-            if not merged or merged[-1][1] < interval[0]:
-                merged.append(interval)
+        while intervals:
+            next_int = intervals.pop(0)
+
+            if next_int[0] <= curr_int[1]:
+                curr_int[1] = max(curr_int[1], next_int[1])
             else:
-                # otherwise, there is overlap, so we merge the current and previous
-                # intervals.
-                merged[-1][1] = max(merged[-1][1], interval[1])
+                ans.append(curr_int)
+                curr_int = next_int
 
-        return merged
+        ans.append(curr_int)
+
+        return ans
