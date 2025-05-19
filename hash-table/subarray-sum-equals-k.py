36b61647ac76543d9ae9b1267c38efc731ff9f99
Jeremy <giahuy.jeremy.do@gmail.com>
2025-05-19 14:16:40 -0400

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

--- hash-table/subarray-sum-equals-k.py
+++ hash-table/subarray-sum-equals-k.py
@@ -5,17 +5,25 @@ class Solution:
         for i in range(len(nums)):
             dp[i + 1] = dp[i] + nums[i]
 
-        freq = Counter(dp)
-        # print(freq)
+        freq = Counter(dp[1:])
+        print(dp, freq)
         ans = 0
 
         for u, v in freq.items():
-            if u + k in freq and k != 0:
-                ans += freq[u+k] * freq[u]
+            if k != 0:
+                if u + k in freq and k != 0:
+                    ans += freq[u+k] * freq[u]
+                if u == k:
+                    ans += freq[u]
             else:
-                if -u in freq and u != 0:
-                    ans += freq[u+k] * freq[u]
-                elif u == 0:
-                    ans += freq[u+k] * freq[u]
+                if u == 0:
+                    ans += freq[u]
+                else:
+                    ans += freq[u] * (freq[u] - 1) // 2
+            # else:
+            #     if -u in freq and u != 0:
+            #         ans += freq[u+k] * freq[u]
+            #     elif u == 0:
+            #         ans += freq[u+k] * freq[u]
                 
         return ans
