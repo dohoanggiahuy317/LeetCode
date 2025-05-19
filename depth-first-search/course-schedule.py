4d1b5ed65234785a2c392722da2bbdf98cee50ee
Jeremy <giahuy.jeremy.do@gmail.com>
2025-05-18 22:50:50 -0400

    LC-course-schedule [Runtime: N/A; Memory: N/A]

--- depth-first-search/course-schedule.py
+++ depth-first-search/course-schedule.py
@@ -1,29 +1,35 @@
+from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = [[] for _ in range(numCourses)]
-        head = [0] * numCourses
 
-        for prereq in prerequisites:
-            start = prereq[1]
-            end = prereq[0]
+        graph = defaultdict(list)
 
-            graph[start].append(end)
-            head[end] += 1
+        for preq in prerequisites:
+            graph[preq[1]].append(preq[0])
 
-        
-        queue = deque()
-        for i in range(numCourses):
-            if head[i] == 0:
-                queue.append(i)
+        def dfs(curr_node):
+            nonlocal visited, graph, cyc
 
-        n0_visited = 0
-        while queue:
-            node = queue.popleft()
-            n0_visited += 1
+            if cyc:
+                return
 
-            for neighbor in graph[node]:
-                head[neighbor] -= 1
-                if head[neighbor] == 0:
-                    queue.append(neighbor)
+            if visited[curr_node]:
+                cyc = True
+                return
 
-        return n0_visited == numCourses
+            visited[curr_node] = True
+
+            for neigh in graph[curr_node]:
+                dfs(neigh)
+
+        for start in range(numCourses):
+            visited = [False] * numCourses
+            cyc = False
+
+            dfs(start)
+
+            if cyc:
+                return False
+
+        return True
+
