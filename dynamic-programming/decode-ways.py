dbb05d05bb7709fd07e51e58da008fb61abd72bf
Jeremy <giahuy.jeremy.do@gmail.com>
2025-05-18 23:15:00 -0400

    LC-decode-ways [Runtime: 0 ms; Memory: 17.6 MB]

--- dynamic-programming/decode-ways.py
+++ dynamic-programming/decode-ways.py
@@ -5,14 +5,14 @@ class Solution:
         for i in range(len(s) - 1, -1, -1):
             if s[i] == "0":
                 dp[i] = 0
-            if s[i] == "1":
+            elif s[i] == "1":
                 if i < len(s) - 1 and s[i+1] == "0":
                     dp[i] = dp[i+2]
                 elif i < len(s) - 1:
                     dp[i] = dp[i+1] + dp[i+2]
                 else:
                     dp[i] = dp[i+1]
-            if s[i] == "2":
+            elif s[i] == "2":
                 if i < len(s) - 1 and s[i+1] == "0":
                     dp[i] = dp[i+2]
                 elif i < len(s) - 1 and s[i+1] in "789":
@@ -21,6 +21,8 @@ class Solution:
                     dp[i] = dp[i+1] + dp[i+2]
                 else:
                     dp[i] = dp[i+1]
+            else:
+                dp[i] = dp[i+1]
             
         print(dp)
         return dp[0]
