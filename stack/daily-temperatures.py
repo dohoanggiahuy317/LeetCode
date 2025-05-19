67aaa5ac2bf8b723d7fb7d770f501dd8867f7aff
Jeremy <giahuy.jeremy.do@gmail.com>
2025-05-19 15:36:17 -0400

    LC-daily-temperatures [Runtime: N/A; Memory: N/A]

--- stack/daily-temperatures.py
+++ stack/daily-temperatures.py
@@ -1,31 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # temp = []
 
-        dp = {len(temperatures) - 1: 0}
+        # for i in range(len(temperatures)):
+        #     temp.append((i, temperatures[i]))
+        ans = [0] * len(temperatures)
+        # print(temp)
 
-        for i in range(len(temperatures) - 2, -1, -1):
-            if temperatures[i] < temperatures[i+1]:
-                dp[i] = 1
-            else:
-                if dp[i+1] == 0:
-                    dp[i] = 0
-                else:
-                    highest = i + 1
-                    while highest < len(temperatures):
-                        highest += dp[highest]
-                        if temperatures[i] < temperatures[highest]:
-                            dp[i] = highest - i
-                            break
-                        elif temperatures[i] >= temperatures[highest] and dp[highest] == 0:
-                            dp[i] = 0
-                            break
-                    if i not in dp:
-                        dp[i] = 0
-            
-         
-        ans = []
-        for x in range(len(temperatures)):
-            ans.append(dp[x])
+        for i in range(len(temperatures) - 1):
+            for j in range(i + 1, len(temperatures)):
+                if temperatures[j] > temperatures[i]:
+                    ans[i] = j - i
+                    break
 
         return ans
-
