28c2c9bfb70352e13d64aaa542e2df827d0c39d8
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-07 21:20:20 -0400

    LC-lexicographical-numbers [Runtime: 7 ms; Memory: 21.4 MB]

--- trie/lexicographical-numbers.py
+++ trie/lexicographical-numbers.py
@@ -1,5 +1,16 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        n = [str(x) for x in range(1, n+1)]
-        n.sort()
-        return list(map(int, n))
+        lexicographical_numbers = []
+        current_number = 1
+
+        for _ in range(n):
+            lexicographical_numbers.append(current_number)
+
+            if current_number * 10 <= n:
+                current_number *= 10
+            else:
+                while current_number % 10 == 9 or current_number >= n:
+                    current_number //= 10 
+                current_number += 1
+
+        return lexicographical_numbers
