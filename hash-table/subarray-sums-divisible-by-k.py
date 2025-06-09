e86d76604f223d5e7ce024b04fdd8eda6f920baf
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-09 19:52:30 -0400

    LC-subarray-sums-divisible-by-k [Runtime: 23 ms; Memory: 21.3 MB]

--- hash-table/subarray-sums-divisible-by-k.py
+++ hash-table/subarray-sums-divisible-by-k.py
@@ -1,13 +1,10 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        prods = {}
-        prefix = ans = 0
+        freq = defaultdict(int, {0:1})
+        pref = 0
+        ans = 0
         for num in nums:
-            prefix += num
-            mod = prefix%k
-            if mod in prods:
-                ans += prods[mod]
-                prods[mod] += 1
-            else:
-                prods[mod] = 1
-        return ans+(prods[0] if 0 in prods else 0)
+            pref = pref + num
+            ans += freq[pref % k]
+            freq[pref%k] += 1
+        return ans
