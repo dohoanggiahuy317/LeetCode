6c9fa93c06ea0a518b661bbfdb38f27e60cffbca
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-09 19:27:00 -0400

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

--- array/subarray-sum-equals-k.py
+++ array/subarray-sum-equals-k.py
@@ -1,18 +1,18 @@
+from collections import Counter
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        dp = Counter()
-        dp[0] = 1
-        curr_sum = 0
+        n = len(nums)
+        pref_sum = [0] * (n+1)
+        freq = Counter()
+
+        # loop through the nums arr
+        for i in range(n):
+            pref_sum[i+1] = pref_sum[i] + nums[i]
+            freq[pref_sum[i+1]] += 1
+
         ans = 0
-
-        for i in range(len(nums)):
-            curr_sum += nums[i]
-
-            diff = curr_sum - k
-            if diff in dp:
-                ans += dp[diff]
-            
-            dp[curr_sum] += 1
+        for s in pref_sum:
+            if s + k in freq:
+                ans += freq[s+k]
 
         return ans
-
