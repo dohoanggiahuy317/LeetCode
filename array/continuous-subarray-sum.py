23e3f9c793e76f31f29d5d91143b263cb032d821
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-11 21:56:52 -0400

    LC-continuous-subarray-sum [Runtime: N/A; Memory: N/A]

--- array/continuous-subarray-sum.py
+++ array/continuous-subarray-sum.py
@@ -1,14 +1,15 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         
-        freq = defaultdict(lambda: int, {0:-1})
+        freq = defaultdict(lambda: len(nums), {0:-1})
         pref = 0
 
         for i, num in enumerate(nums):
+            print(freq)
             pref = (pref + num) % k
             if pref in freq and i - freq[pref] > 1:
                 return True
-            freq[pref] = i
+            freq[pref] = min(i, freq[pref])
         
         return False
             
