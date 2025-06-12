e08c89ab076a72afe73a1fb76f16c444342e6a16
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-11 21:41:46 -0400

    LC-continuous-subarray-sum [Runtime: N/A; Memory: N/A]

--- math/continuous-subarray-sum.py
+++ math/continuous-subarray-sum.py
@@ -1,16 +1,11 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        remainder_dict = {0: -1}  # Initializing with 0 to handle cases where the subarray itself is a multiple of k
-        cum_sum = 0
-        
-        for i in range(len(nums)):
-            cum_sum += nums[i]
-            remainder = cum_sum % k
-            
-            if remainder in remainder_dict:
-                if i - remainder_dict[remainder] > 1:  # Ensure the subarray length is at least 2
-                    return True
-            else:
-                remainder_dict[remainder] = i
+        freq = defaultdict(int, {0:0})
+        pref = 0
+        for num in nums:
+            pref = (pref + num) % k
+            if (pref - k) % k in freq:
+                return True
         
         return False
+            
