c1c8f2e52936969e682d39125d6107eff1a16520
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-13 16:59:25 -0400

    LC-count-subarrays-with-score-less-than-k [Runtime: 171 ms; Memory: 30.6 MB]

--- sliding-window/count-subarrays-with-score-less-than-k.py
+++ sliding-window/count-subarrays-with-score-less-than-k.py
@@ -2,16 +2,13 @@ class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
         pref = [0] * (len(nums) + 1)
         ans = 0
+        left = 0
 
         for i, num in enumerate(nums):
             pref[i+1] = pref[i] + num
-            l = 1
 
-            while i+1-l >= 0 and k > l * (pref[i+1]-pref[i+1-l]):
-                # print(l, pref[i+1-l], pref[i+1])
-                ans += 1
-                l += 1
-            # print(pref)
-            # print()
+            while left <= i and k <= (i-left+1) * (pref[i+1]-pref[left]):
+                left += 1
+            ans += i-left + 1
             
         return ans
