064388dfa7840eba827c81fe619d59bb14dcfdb2
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-13 11:00:52 -0400

    LC-binary-subarrays-with-sum [Runtime: N/A; Memory: N/A]

--- sliding-window/binary-subarrays-with-sum.py
+++ sliding-window/binary-subarrays-with-sum.py
@@ -1,13 +1,33 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        count = {0: 1}
-        curr_sum = 0
-        total_subarrays = 0
-        
-        for num in nums:
-            curr_sum += num
-            if curr_sum - goal in count:
-                total_subarrays += count[curr_sum - goal]
-            count[curr_sum] = count.get(curr_sum, 0) + 1
+        pref = 0
+        nums += [1]
+        freq = defaultdict(int)
+        ans = 0
 
-        return total_subarrays
+        for i, num in enumerate(nums):
+            print(freq)
+            if num == 1:
+                pref += 1
+                freq[pref] = i
+
+                if pref-1-goal >= 0:
+                    right_pad = freq[pref]
+                    right = freq[pref-1]
+                    left = freq[pref-goal]
+                    left_pad = freq[pref-1-goal]
+                    # print("yp")
+                    # print(pref, dict(freq))
+                    # print(left_pad, left, right, right_pad)
+                    # print()
+
+                    if left > right:
+                        ans += right_pad * (right_pad + 1) // 2
+                    else:
+                        ans += (right_pad - right) * ((left - left_pad) * int(left != left_pad) + 1 * int(left == left_pad))
+
+
+                    
+
+
+        return ans
