59d8e0959215397b36484b5f8ba26eb0ac311cc3
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-14 15:41:50 -0400

    LC-binary-subarrays-with-sum [Runtime: 27 ms; Memory: 22.8 MB]

--- sliding-window/binary-subarrays-with-sum.py
+++ sliding-window/binary-subarrays-with-sum.py
@@ -1,28 +1,52 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        pref = 0
-        nums += [1]
-        freq = defaultdict(int, {0:-1})
+            
+        freq = defaultdict(lambda: -1, {0: -1})
         ans = 0
+        pref = 0
 
         for i, num in enumerate(nums):
-            if num == 1:
-                pref += 1
-                freq[pref] = i
+            if goal != 0:
+                if num == 1:
+                    pref += 1
+                    freq[pref] = i
+                
+                ans += freq[pref-goal+1] - freq[pref-goal]
+            else:
+                if num == 1:
+                    ans += pref * (pref+1)//2
+                    pref = 0
+                elif i == len(nums) - 1:
+                    pref += 1
+                    ans += pref * (pref+1)//2
+                else:
+                    pref += 1
 
-                if pref-1-goal >= 0:
-                    right_pad = freq[pref]
-                    right = freq[pref-1]
-                    left = freq[pref-goal]
-                    left_pad = freq[pref-1-goal]
+        return ans
 
-                    if left > right:
-                        ans += (right_pad-right) * (right_pad-right - 1) // 2
-                    else:
-                        ans += (right_pad - right) * ((left - left_pad) * int(left != left_pad) + 1 * int(left == left_pad))
+        # pref = 0
+        # nums += [1]
+        # freq = defaultdict(int, {0:-1})
+        # ans = 0
+
+        # for i, num in enumerate(nums):
+        #     if num == 1:
+        #         pref += 1
+        #         freq[pref] = i
+
+        #         if pref-1-goal >= 0:
+        #             right_pad = freq[pref]
+        #             right = freq[pref-1]
+        #             left = freq[pref-goal]
+        #             left_pad = freq[pref-1-goal]
+
+        #             if left > right:
+        #                 ans += (right_pad-right) * (right_pad-right - 1) // 2
+        #             else:
+        #                 ans += (right_pad - right) * ((left - left_pad) * int(left != left_pad) + 1 * int(left == left_pad))
 
 
                     
 
 
-        return ans
+        # return ans
