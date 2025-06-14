5ce4b28137d8863c7192230a172487229e3b78db
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-14 12:46:42 -0400

    LC-number-of-substrings-containing-all-three-characters [Runtime: N/A; Memory: N/A]

--- string/number-of-substrings-containing-all-three-characters.py
+++ string/number-of-substrings-containing-all-three-characters.py
@@ -4,14 +4,12 @@ class Solution:
         ans = 0
         first = True
         for i, char in enumerate(s):
+            l = min(map(lambda x: x[1] , list(filter(lambda x: x[0] != char, ap.items()))))
             if len(ap) == 3 and not first:
-                # print(i, char, ap)
-                # print(list(filter(lambda x: x[0] != char, ap.items())))
-                ans += 1 + min(map(lambda x: x[1] , list(filter(lambda x: x[0] != char, ap.items()))))
-                # print(ans)
+                ans += 1 + l
             ap[char] = i
             if len(ap) == 3 and first:
-                ans += 1 + min(map(lambda x: x[1] , list(filter(lambda x: x[0] != char, ap.items()))))
+                ans += 1 + l
                 first = False
 
         return ans
