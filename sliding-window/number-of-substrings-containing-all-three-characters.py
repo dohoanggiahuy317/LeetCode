fd6783dbf17c517dfb41a5eb86a696173cca5056
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-14 12:48:06 -0400

    LC-number-of-substrings-containing-all-three-characters [Runtime: N/A; Memory: N/A]

--- sliding-window/number-of-substrings-containing-all-three-characters.py
+++ sliding-window/number-of-substrings-containing-all-three-characters.py
@@ -4,12 +4,15 @@ class Solution:
         ans = 0
         first = True
         for i, char in enumerate(s):
-            l = min(map(lambda x: x[1] , list(filter(lambda x: x[0] != char, ap.items()))))
-            if len(ap) == 3 and not first:
-                ans += 1 + l
-            ap[char] = i
-            if len(ap) == 3 and first:
-                ans += 1 + l
-                first = False
+            if len(ap) == 3:
+                l = min(map(lambda x: x[1] , list(filter(lambda x: x[0] != char, ap.items()))))
+                if not first:
+                    ans += 1 + l
+                
+                ap[char] = i
+                
+                if first:
+                    ans += 1 + l
+                    first = False
 
         return ans
