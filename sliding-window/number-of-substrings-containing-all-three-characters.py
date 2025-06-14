b68b9623548607a09374fd6b02ef9fcfb6f0e4fd
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-14 12:45:58 -0400

    LC-number-of-substrings-containing-all-three-characters [Runtime: 295 ms; Memory: 17.8 MB]

--- sliding-window/number-of-substrings-containing-all-three-characters.py
+++ sliding-window/number-of-substrings-containing-all-three-characters.py
@@ -1,12 +1,17 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        s += " "
         ap = defaultdict(int)
         ans = 0
+        first = True
         for i, char in enumerate(s):
-            if len(ap) == 3:
+            if len(ap) == 3 and not first:
                 # print(i, char, ap)
-                ans += min(map(lambda x: x[1] , filter(lambda x: x[0] != char, ap.items())))
+                # print(list(filter(lambda x: x[0] != char, ap.items())))
+                ans += 1 + min(map(lambda x: x[1] , list(filter(lambda x: x[0] != char, ap.items()))))
                 # print(ans)
             ap[char] = i
-        return ans + int(len(ap) == 4)
+            if len(ap) == 3 and first:
+                ans += 1 + min(map(lambda x: x[1] , list(filter(lambda x: x[0] != char, ap.items()))))
+                first = False
+
+        return ans
