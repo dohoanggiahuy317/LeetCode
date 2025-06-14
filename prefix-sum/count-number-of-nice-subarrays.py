1dacbd5ad010e32995a3c02db5f9e0ccb0d6818e
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-14 15:35:00 -0400

    LC-count-number-of-nice-subarrays [Runtime: 109 ms; Memory: 27.2 MB]

--- prefix-sum/count-number-of-nice-subarrays.py
+++ prefix-sum/count-number-of-nice-subarrays.py
@@ -1,24 +1,18 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        freq = defaultdict(int, {0: -1})
+        freq = defaultdict(lambda: -1, {0: -1})
         ans = 0
         pref = 0
-        nums += [1]
+        # nums += [1]
 
         for i, num in enumerate(nums):
             if num % 2 == 1:
                 pref += 1
                 freq[pref] = i
+            
+            ans += freq[pref-k+1] - freq[pref-k]
+            # print(pref, dict(freq), ans)
 
-                if pref-k-1 >= 0:
-                    right_pad = freq[pref]
-                    right = freq[pref-1]
-                    left = freq[pref-k]
-                    left_pad = freq[pref-k-1]
-                
-                    if left > right:
-                            ans += (right_pad-right) * (right_pad-right - 1) // 2
-                    else:
-                        ans += (right_pad - right) * ((left - left_pad) * int(left != left_pad) + 1 * int(left == left_pad))
+
 
         return ans
