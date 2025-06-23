d8725bbaac58fd2883c60e7d816a1fef8d63ec00
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-22 20:07:35 -0400

    LC-binary-search [Runtime: N/A; Memory: N/A]

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -1,6 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-       
+        if len(nums) == 1:
+            return 0 if nums[0] == target else -1
         l, r = 0, len(nums) - 1
        
         while l < r:
