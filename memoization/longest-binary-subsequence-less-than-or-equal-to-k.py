331cf58b751290df00b063546e4245c0dc80a9b7
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-26 15:01:38 -0400

    LC-longest-binary-subsequence-less-than-or-equal-to-k [Runtime: 11 ms; Memory: 17.9 MB]

--- memoization/longest-binary-subsequence-less-than-or-equal-to-k.py
+++ memoization/longest-binary-subsequence-less-than-or-equal-to-k.py
@@ -2,9 +2,8 @@ class Solution:
     def longestSubsequence(self, s: str, k: int) -> int:
         curr = int(s, 2)
         count = 0
-        
         for i, num in enumerate(s):
-            if num == "1":
+            if num == "1" and curr > k:
                 curr -= 2 ** (len(s) - 1 - i)
                 count += 1
 
