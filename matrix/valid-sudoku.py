57fa2ee831de142660e1d39b0a6b736ee7648515
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-27 10:42:34 -0400

    LC-valid-sudoku [Runtime: 6 ms; Memory: 17.7 MB]

--- matrix/valid-sudoku.py
+++ matrix/valid-sudoku.py
@@ -1,66 +1,32 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        def check_row(i):
-            nonlocal board
-
-            row = board[i]
-            s = set()
-
-            for x in row:
-                if x != ".":
-                    if x not in s:
-                        s.add(x)
-                    else:
-                        return False
-            
-            return True
-
-        def check_col(i):
-            nonlocal board
-
-            col = list(map(lambda x: x[i], board))
-            s = set()
-
-            for x in col:
-                if x != ".":
-                    if x not in s:
-                        s.add(x)
-                    else:
-                        return False
-            
+        def check(row):
+            t = set()
+            for ele in row:
+                if ele != "." and ele in t:
+                    return False
+                elif ele != ".":
+                    t.add(ele)
             return True
 
         def check_square(i, j):
             nonlocal board
 
             square = [
-                board[i-1][j-1], board[i-1][j], board[i-1][j+1],
-                board[i][j-1],   board[i][j],   board[i][j+1],
-                board[i+1][j-1], board[i+1][j], board[i+1][j+1],
+                board[i][j],        board[i][j+1],      board[i][j+2],
+                board[i+1][j],      board[i+1][j+1],    board[i+1][j+2],
+                board[i+2][j],    board[i+2][j+1],    board[i+2][j+2],
             ]
-
-            s = set()
-            for x in square:
-                if x != ".":
-                    if x not in s:
-                        s.add(x)
-                    else:
-                        return False
-            
-            return True
-
+            return check(square)
 
         for i in range(9):
-            if not check_row(i) or not check_col(i):
+            if not check(board[i]):
                 return False
+            if not check([board[j][i] for j in range(9)]):
+                return False
+        for i in range(0, 8, 3):
+            for j in range(0, 8, 3):
+                if not check_square(i, j):
+                    return False
 
-        if (
-            not check_square(1, 1) or not check_square(1, 4) or not check_square(1, 7)
-        ) or (
-            not check_square(4, 1) or not check_square(4, 4) or not check_square(4, 7)
-        ) or (
-            not check_square(7, 1) or not check_square(7, 4) or not check_square(7, 7)
-        ): 
-            return False
-        
         return True
