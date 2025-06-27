4f61507b7677fccfb3b79d16fe0bedaf0a8a9613
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-27 09:46:45 -0400

    LC-vowel-spellchecker [Runtime: N/A; Memory: N/A]

---
+++ array/vowel-spellchecker.py
@@ -0,0 +1,31 @@
+import re
+class Solution:
+    def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
+        unvowell = defaultdict(str)
+        uncasel = defaultdict(str)
+        wordset = set()
+        for word in wordlist:
+            formatword = re.sub(r"[ueoai]", "*", word)
+            if formatword.lower() not in unvowell:
+                unvowell[formatword.lower()] = word
+
+            if word.lower() not in uncasel:
+                uncasel[word.lower()] = word
+            
+            wordset.add(word)
+
+        ans = []
+        for query in queries:
+            f = re.sub(r"[ueoai]", "*", query)
+            if query in wordset:
+                ans.append(query)
+            elif query.lower() in uncasel:
+                ans.append(uncasel[query.lower()])
+            elif f in unvowell:
+                ans.append(unvowell[f])
+            else:
+                ans.append("")
+
+        return ans
+
+        
