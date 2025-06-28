fcd06e2ee711ebd278b50eb9599be0ecb2dff05a
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-27 23:53:54 -0400

    LC-random-pick-with-weight [Runtime: 67 ms; Memory: 23.4 MB]

--- binary-search/random-pick-with-weight.py
+++ binary-search/random-pick-with-weight.py
@@ -6,26 +6,20 @@ class Solution:
             self.pref[i + 1] = self.pref[i] + num
         self.pref = self.pref[1:]
         self.s = sum(w)
-        print(self.pref, self.s)
 
     def pickIndex(self) -> int:
         tar = random.randint(1, self.s)
         l, r = 0, len(self.pref) - 1
-        # print(tar)
 
         while l < r:
             m = (l + r) // 2
-            # print(l, r, m)
             if self.pref[m] < tar:
                 l = m + 1
             elif self.pref[m] > tar:
                 r = m
             else:
-                # print(m)
-                # print()
                 return m
-        # print(l)
-        # print()
+
         return l
 
 
