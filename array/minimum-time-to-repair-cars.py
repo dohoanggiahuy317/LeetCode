1da17b6961c59da14685f53d9af87f6e8f0c8c7e
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-27 23:00:37 -0400

    LC-minimum-time-to-repair-cars [Runtime: 888 ms; Memory: 21.9 MB]

---
+++ array/minimum-time-to-repair-cars.py
@@ -0,0 +1,20 @@
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        
+        def check(k):
+            nonlocal ranks, cars
+            temp = cars
+            for rank in ranks:
+                temp -= int(math.sqrt(k/rank))
+
+            return temp <= 0
+
+        l, r = 1, min(ranks) * (cars ** 2)
+        while l <= r:
+            m = (l + r) // 2
+            if check(m):
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return l
