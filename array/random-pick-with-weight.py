be8248b381d58d863318f28cd0b148d9bc0237da
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-27 23:43:52 -0400

    LC-random-pick-with-weight [Runtime: N/A; Memory: N/A]

---
+++ array/random-pick-with-weight.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.pref = [0] * (len(w) + 1)
+        for i, num in enumerate(w):
+            self.pref[i + 1] = self.pref[i] + num
+        self.pref = self.pref[1:]
+        self.s = sum(w)
+
+    def pickIndex(self) -> int:
+        tar = random.randint(1, self.s)
+        l, r = 0, len(self.pref) - 1
+
+        while l < r:
+            m = (l + r) // 2
+            if self.pref[m] < tar:
+                r = m
+            elif self.pref[m] < tar:
+                l = m + 1
+            else:
+                return m + 1
+        return r
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
