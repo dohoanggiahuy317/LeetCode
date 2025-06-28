697a415d229ff7421c82d5b2c9afeeccccb8d0d7
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-27 23:53:36 -0400

    LC-random-pick-with-weight [Runtime: 95 ms; Memory: 23.7 MB]

--- randomized/random-pick-with-weight.py
+++ randomized/random-pick-with-weight.py
@@ -6,20 +6,27 @@ class Solution:
             self.pref[i + 1] = self.pref[i] + num
         self.pref = self.pref[1:]
         self.s = sum(w)
+        print(self.pref, self.s)
 
     def pickIndex(self) -> int:
         tar = random.randint(1, self.s)
         l, r = 0, len(self.pref) - 1
+        # print(tar)
 
         while l < r:
             m = (l + r) // 2
+            # print(l, r, m)
             if self.pref[m] < tar:
-                r = m
-            elif self.pref[m] < tar:
                 l = m + 1
+            elif self.pref[m] > tar:
+                r = m
             else:
-                return m + 1
-        return r
+                # print(m)
+                # print()
+                return m
+        # print(l)
+        # print()
+        return l
 
 
 # Your Solution object will be instantiated and called as such:
