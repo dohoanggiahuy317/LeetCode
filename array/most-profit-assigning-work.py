c12ef735c797f31795c32e8574fa791acf4b89fb
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-29 23:39:35 -0400

    LC-most-profit-assigning-work [Runtime: N/A; Memory: N/A]

--- array/most-profit-assigning-work.py
+++ array/most-profit-assigning-work.py
@@ -1,29 +1,22 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        diff_prof = zip(difficulty, profit)
-        diff_prof = sorted(diff_prof, key=lambda x: (x[0], x[1]))
-        print(diff_prof)
-
+        dif_prof = zip(difficulty, profit)
+        dif_prof = sorted(dif_prof, key=lambda x: (x[0], x[1]))
         ans = 0
-        for wor in worker:
-            if wor < diff_prof[0][0]:
-                continue
 
-            l, r = 0, len(diff_prof) - 1
-            print(wor)
-
+        for w in worker:
+            l, r = 0, len(dif_prof)
+            ind = -1
+            
             while l <= r:
                 m = (l + r) // 2
-                print(l, r, m)
-
-                if wor < diff_prof[m][0]:
-                    r = m-1
+                if dif_prof[m][0] > w:
+                    ind = m
+                    r = m - 1
                 else:
                     l = m + 1
                 
-            ans += diff_prof[r][1]
-            print(diff_prof[r])
-            print()
+            ans += max([x[1] for x in dif_prof[:ind]])
         
         return ans
                 
