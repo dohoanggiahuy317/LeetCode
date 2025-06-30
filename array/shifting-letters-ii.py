3ee92baccfaea63ee5bcc82ce7888d2663e87f14
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-30 11:19:08 -0400

    LC-shifting-letters-ii [Runtime: N/A; Memory: N/A]

--- array/shifting-letters-ii.py
+++ array/shifting-letters-ii.py
@@ -1,19 +1,12 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        trackUp, trackDown = [0] * len(s), [0] * len(s)
-
-        for st, e, d in shifts:
-            trackUp[e] += 2*d - 1
-            trackDown[st] += 2*d - 1
-
-        prefDown, prefUp = [0], [0]
-        for i in range(len(s)):
-            prefDown.append(prefDown[-1] + trackDown[len(trackDown)-1 - i])
-            prefUp.append(prefUp[-1] + trackUp[len(trackUp)-1 - i])
-
-        ans = ''
-        for i in range(len(s)):
-            char_shift = prefUp[len(s) - i] - prefDown[len(s) - (i+1)]
-            ans +=  chr((ord(s[i]) - ord("a") + char_shift) % 26 + ord("a"))
-
-        return ans
+        N = len(s)
+        changes = [0] * (N+1)
+        a = ord('a')
+        for start, end, shift in shifts:
+            changes[start] += MAP_DIRECTION[shift]
+            changes[end+1] -= MAP_DIRECTION[shift]
+        ans = []
+        for c, change in zip(s, accumulate(changes)):
+            ans.append(chr(a + (ord(c) - a + change) % 26))
+        return "".join(ans)
