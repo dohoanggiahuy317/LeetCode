3df9497030120473b6a156989198bd64d90d8c11
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-30 11:18:20 -0400

    LC-car-pooling [Runtime: 119 ms; Memory: 18.3 MB]

--- sorting/car-pooling.py
+++ sorting/car-pooling.py
@@ -1,24 +1,8 @@
-import heapq
-
 class Solution:
-    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        end_time = max(list(map(lambda x: x[2], trips)))
-        trips.sort(key = lambda x: x[1])
-        curr_cap = 0
-        queue = []
-
-        for i in range(1, end_time+1):
-            # print(i, queue, trips)
-            while queue and i >= queue[0][0]:
-                curr_cap -= heapq.heappop(queue)[2]
-
-            while trips and i >= trips[0][1]:
-                curr_cap += trips[0][0]
-                heapq.heappush(queue, (trips[0][2], trips[0][1], trips[0][0]))
-                trips.pop(0)
-
-            if curr_cap > capacity:
-                return False
-
-        return True
-            
+   def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+       destination = max(x[2] for x in trips)
+       pax_count = [0] * (destination + 1)
+       for num_passenger, from_km, to_km in trips:
+           for i in range(from_km, to_km):
+               pax_count[i] += num_passenger
+       return all(x <= capacity for x in pax_count)
