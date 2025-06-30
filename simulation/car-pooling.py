1fee7fbaacfccbf4f661f76beae6bf91aca9ecdc
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-30 11:18:27 -0400

    LC-car-pooling [Runtime: 115 ms; Memory: 18.4 MB]

--- simulation/car-pooling.py
+++ simulation/car-pooling.py
@@ -1,8 +1,8 @@
 class Solution:
    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-       destination = max(x[2] for x in trips)
-       pax_count = [0] * (destination + 1)
-       for num_passenger, from_km, to_km in trips:
-           for i in range(from_km, to_km):
-               pax_count[i] += num_passenger
-       return all(x <= capacity for x in pax_count)
+        destination = max(x[2] for x in trips)
+        pax_count = [0] * (destination + 1)
+        for num_passenger, from_km, to_km in trips:
+            for i in range(from_km, to_km):
+                pax_count[i] += num_passenger
+        return all(x <= capacity for x in pax_count)
