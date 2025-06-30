db63d7c5c7c20b9286928f17a9bcb5174f8ff1e7
Jeremy <giahuy.jeremy.do@gmail.com>
2025-06-30 11:15:11 -0400

    LC-most-profit-assigning-work [Runtime: 125 ms; Memory: 21.3 MB]

--- greedy/most-profit-assigning-work.py
+++ greedy/most-profit-assigning-work.py
@@ -2,31 +2,26 @@ class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
         dif_prof = zip(difficulty, profit)
         dif_prof = sorted(dif_prof, key=lambda x: (x[0], x[1]))
+        dif_prof = [(0, 0)] + dif_prof + [(inf, 0)]
+        
+        pref_max = defaultdict(int, {0:0})
+        for i in range(1, len(dif_prof)):
+            pref_max[i] = max(pref_max[i-1], dif_prof[i][1])
+        
         ans = 0
-        # print(dif_prof)
-        mi, ma = min(difficulty), max(difficulty)
-        maprof = max(profit)
-
         for w in worker:
-            if w <mi:
-                continue
-            if w >= ma:
-                ans += maprof
-                continue
-
             l, r = 0, len(dif_prof)
             ind = -1
             
-            while l <= r:
+            while l < r:
                 m = (l + r) // 2
-                if dif_prof[m][0] > w:
+                if dif_prof[m][0] > w:   
                     ind = m
-                    r = m - 1
+                    r = m
                 else:
                     l = m + 1
-            # print(w, ind)
-            if dif_prof[:ind]:
-                ans += max([x[1] for x in dif_prof[:ind]])
+
+            ans += pref_max[ind-1]
         
         return ans
                 
