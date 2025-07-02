5c03b6562bbe94cfc983364ec0e5634ab7f52eb0
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-01 23:22:23 -0400

    LC-lru-cache [Runtime: 159 ms; Memory: 90.6 MB]

---
+++ design/lru-cache.py
@@ -0,0 +1,44 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.dict = {}
+        self.key_order = {}
+        self.key_freq = defaultdict(int)
+        self.count = 0
+        self.curr_min = 0
+        pass
+
+    def get(self, key: int) -> int:
+        
+        if key in self.dict:
+            self.key_order[self.count] = key
+            self.key_freq[key] += 1
+            self.count += 1
+            return self.dict[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        self.key_freq[key] += 1
+        self.key_order[self.count] = key
+        self.count += 1
+        self.dict[key] = value
+
+        while len(self.key_freq) > self.cap:
+            least_key = self.key_order[self.curr_min]
+            self.key_freq[least_key] -= 1
+
+            if self.key_freq[least_key] == 0:
+                del self.key_freq[least_key]
+                del self.dict[least_key]
+
+            self.curr_min += 1
+        
+        # print(self.dict, self.key_order, self.key_freq)
+
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
