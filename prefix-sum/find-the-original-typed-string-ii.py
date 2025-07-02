c1a101473b9aec21b7662ef2387d7f92b335fa9b
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-01 21:40:26 -0400

    LC-find-the-original-typed-string-ii [Runtime: 4003 ms; Memory: 25.9 MB]

--- prefix-sum/find-the-original-typed-string-ii.py
+++ prefix-sum/find-the-original-typed-string-ii.py
@@ -1,52 +1,33 @@
-impl Solution {
-    pub fn possible_string_count(word: String, k: i32) -> i32 {
-        const MOD: i64 = 1_000_000_007;
-        let n = word.len();
-        let mut cnt = 1;
-        let mut freq: Vec<i32> = Vec::new();
-        let word_chars: Vec<char> = word.chars().collect();
+class Solution:
+    def possibleStringCount(self, word: str, k: int) -> int:
+        mod = 10**9 + 7
+        n, cnt = len(word), 1
+        freq = list()
 
-        for i in 1..n {
-            if word_chars[i] == word_chars[i - 1] {
-                cnt += 1;
-            } else {
-                freq.push(cnt);
-                cnt = 1;
-            }
-        }
-        freq.push(cnt);
+        for i in range(1, n):
+            if word[i] == word[i - 1]:
+                cnt += 1
+            else:
+                freq.append(cnt)
+                cnt = 1
+        freq.append(cnt)
 
-        let mut ans: i64 = 1;
-        for &o in &freq {
-            ans = ans * o as i64 % MOD;
-        }
+        ans = 1
+        for o in freq:
+            ans = ans * o % mod
 
-        if freq.len() >= k as usize {
-            return ans as i32;
-        }
+        if len(freq) >= k:
+            return ans
 
-        let k_usize = k as usize;
-        let mut f = vec![0; k_usize];
-        let mut g = vec![1; k_usize];
-        f[0] = 1;
-
-        for &num in &freq {
-            let mut f_new = vec![0; k_usize];
-            for j in 1..k_usize {
-                f_new[j] = g[j - 1];
-                if j as i32 - num - 1 >= 0 {
-                    f_new[j] = (f_new[j] - g[j - num as usize - 1] + MOD) % MOD;
-                }
-            }
-            let mut g_new = vec![0; k_usize];
-            g_new[0] = f_new[0];
-            for j in 1..k_usize {
-                g_new[j] = (g_new[j - 1] + f_new[j]) % MOD;
-            }
-            f = f_new;
-            g = g_new;
-        }
-
-        ((ans - g[k_usize - 1] as i64 + MOD) % MOD) as i32
-    }
-}
+        f, g = [1] + [0] * (k - 1), [1] * k
+        for i in range(len(freq)):
+            f_new = [0] * k
+            for j in range(1, k):
+                f_new[j] = g[j - 1]
+                if j - freq[i] - 1 >= 0:
+                    f_new[j] = (f_new[j] - g[j - freq[i] - 1]) % mod
+            g_new = [f_new[0]] + [0] * (k - 1)
+            for j in range(1, k):
+                g_new[j] = (g_new[j - 1] + f_new[j]) % mod
+            f, g = f_new, g_new
+        return (ans - g[k - 1]) % mod
