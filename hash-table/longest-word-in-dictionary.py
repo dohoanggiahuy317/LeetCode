48e9b819a1a4f59dbca65de659205dfc09ab02cf
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-06 19:06:50 -0400

    LC-longest-word-in-dictionary [Runtime: 11 ms; Memory: 18.1 MB]

--- hash-table/longest-word-in-dictionary.py
+++ hash-table/longest-word-in-dictionary.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        words.sort(key = lambda x: len(x))
+        words.sort(key = lambda x: (len(x), x))
         track = defaultdict(int, {"": 0})
         l, ans = 0, ""
 
@@ -8,9 +8,7 @@ class Solution:
             if word[:-1] != "" and word[:-1] not in track:
                 continue
             track[word] = track[ word[:-1] ] + 1
-            
-            if l == track[word]:
-                ans = min(ans, word)
+
             if l < track[word]:
                 ans = word
                 l = track[word]
