690f02232da15fbe7894afdd0c18d52d860ca8bd
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-06 20:31:01 -0400

    LC-balanced-binary-tree [Runtime: 7 ms; Memory: 18.9 MB]

--- depth-first-search/balanced-binary-tree.py
+++ depth-first-search/balanced-binary-tree.py
@@ -1,12 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
+
+    
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def dfs(root):
-            if root is None:
-                return [True, 0]
+        
+        ans = True
 
-            left, right = dfs(root.left), dfs(root.right)
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+        def height(root):
+            nonlocal ans
+            if not root:
+                return 0
 
-            return [balanced, 1 + max(left[1], right[1])]
+            height_left = height(root.left) 
+            height_right = height(root.right) 
+            
+            diff_check = abs(height_left - height_right) <= 1
+            if not diff_check and ans:
+                ans = False
 
-        return dfs(root)[0]
+            return max(height_left, height_right) + 1
+
+        height(root)
+        return ans
+
+
