e2a7f5c84b5d0cbdf18bc7b5f55f66d8a5f7a515
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-12 19:14:01 -0400

    LC-shortest-path-in-binary-matrix [Runtime: 235 ms; Memory: 19.6 MB]

--- breadth-first-search/shortest-path-in-binary-matrix.py
+++ breadth-first-search/shortest-path-in-binary-matrix.py
@@ -3,7 +3,7 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         start_x, start_y, init_dist = 0, 0, 1
         directions = [  (-1, -1), (-1, 0), (-1, 1), 
-                        (0, -1), (0, 1), 
+                        (0, -1), (0, 1), (0, 0),
                         (1, -1), (1, 0), (1, 1)]
 
         if grid[start_x][start_y] == 1:
@@ -19,7 +19,7 @@ class Solution:
             for dx, dy in directions:
                 nx, ny = x + dx, y + dy
                 if nx == m - 1 and ny == n - 1 and grid[nx][ny] == 0:
-                    return dist + 1
+                    return dist + (dx or dy)
 
                 if ( 
                     0 <= nx < m and
