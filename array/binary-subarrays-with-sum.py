30ad9a2d8134fb6c7d07a0f9c0c182535c769fbd
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-19 16:50:43 -0400

    LC-binary-subarrays-with-sum [Runtime: N/A; Memory: N/A]

--- array/binary-subarrays-with-sum.py
+++ array/binary-subarrays-with-sum.py
@@ -1,40 +1,13 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-
-        freq = defaultdict(int, {0: 1})
+        dp = defaultdict(int)
+        curr_sum = 0
         ans = 0
-        pref = 0
 
-        for i, num in enumerate(nums):
-            pref += num
-            ans += freq[pref-goal]
-            freq[pref] += 1
-                
+        for num in nums:
+            curr_sum += num
+            ans += dp[curr_sum - goal]
+            dp[curr_sum] += 1
+
         return ans
 
-        # pref = 0
-        # nums += [1]
-        # freq = defaultdict(int, {0:-1})
-        # ans = 0
-
-        # for i, num in enumerate(nums):
-        #     if num == 1:
-        #         pref += 1
-        #         freq[pref] = i
-
-        #         if pref-1-goal >= 0:
-        #             right_pad = freq[pref]
-        #             right = freq[pref-1]
-        #             left = freq[pref-goal]
-        #             left_pad = freq[pref-1-goal]
-
-        #             if left > right:
-        #                 ans += (right_pad-right) * (right_pad-right - 1) // 2
-        #             else:
-        #                 ans += (right_pad - right) * ((left - left_pad) * int(left != left_pad) + 1 * int(left == left_pad))
-
-
-                    
-
-
-        # return ans
