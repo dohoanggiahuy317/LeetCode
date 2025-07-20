9e94bd6f9a510cc86b60461d659fb35fa0906d57
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-19 22:43:26 -0400

    LC-determine-if-a-cell-is-reachable-at-a-given-time [Runtime: 26 ms; Memory: 18 MB]

--- math/determine-if-a-cell-is-reachable-at-a-given-time.py
+++ math/determine-if-a-cell-is-reachable-at-a-given-time.py
@@ -1,12 +1,7 @@
 class Solution:
     def isReachableAtTime(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
-
-        h = abs(fy - sy)
-        v = abs(fx - sx)
-
-        if max(h, v) == 0 and t == 1:
+        a, b = abs(fy - sy), abs(fx - sx)
+        if max(a, b) == 0 and t == 1:
             return False
-
-
-        return max(h, v) <= t
+        return max(a, b) <= t
         
