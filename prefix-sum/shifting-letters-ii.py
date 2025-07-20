f8d3e74c20ce5a498100a15cfc16cc980e709d78
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-19 21:33:29 -0400

    LC-shifting-letters-ii [Runtime: 55 ms; Memory: 41.5 MB]

--- prefix-sum/shifting-letters-ii.py
+++ prefix-sum/shifting-letters-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        MAP_DIRECTION = {0: -1, 1: 1}
+        map_direction = {0:-1, 1:1}
+        change = [0] * (len(s) + 1)
 
-        N = len(s)
-        changes = [0] * (N+1)
-        a = ord('a')
-        for start, end, shift in shifts:
-            changes[start] += MAP_DIRECTION[shift]
-            changes[end+1] -= MAP_DIRECTION[shift]
-        ans = []
-        for c, change in zip(s, accumulate(changes)):
-            ans.append(chr(a + (ord(c) - a + change) % 26))
-        return "".join(ans)
+        for start, end, direction in shifts:
+            change[start] += map_direction[direction]
+            change[end + 1] -= map_direction[direction]
+
+        ans = ""
+
+        for char, num in zip(s, accumulate(change)):
+            ans += chr((ord(char) + num - ord("a")) % 26 + ord("a"))
+        
+        return ans
