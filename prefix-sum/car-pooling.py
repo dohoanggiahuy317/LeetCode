6e31811d7ae71fbd19df5baeb8c2e40772541312
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-20 16:11:21 -0400

    LC-car-pooling [Runtime: 129 ms; Memory: 18.4 MB]

--- prefix-sum/car-pooling.py
+++ prefix-sum/car-pooling.py
@@ -1,29 +1,10 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        trips = sorted(trips, key = lambda x: x[1])
-        # print(trips)
+        max_destination = max([tr[2] for tr in trips])
+        pad_count = [0] * (max_destination + 1)
 
-        timestart = min([trip[1] for trip in trips])
-        timeend = max([trip[2] for trip in trips])
+        for num, st, en in trips:
+            for i in range(st, en):
+                pad_count[i] += num
 
-        cur_guest = []
-        cur_cap = 0
-        cur_checkpoint = 0
-
-        for cur_time in range(timestart, timeend + 1):
-            while cur_guest and cur_guest[0][0] <= cur_time:
-                _, num_guest = heapq.heappop(cur_guest)
-                cur_cap -= num_guest
-
-            while cur_checkpoint < len(trips) and trips[cur_checkpoint][1] <= cur_time:
-                num_guest, st, en = trips[cur_checkpoint]
-                heapq.heappush(cur_guest, (en, num_guest))
-                cur_cap += num_guest
-                cur_checkpoint += 1
-
-            # print(cur_time, cur_cap)
-
-            if cur_cap > capacity:
-                return False
-        
-        return True
+        return all(co <= capacity for co in pad_count)
