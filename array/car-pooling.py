8fbd279e16590911075355908f3d3afa7c97e51d
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-20 16:02:40 -0400

    LC-car-pooling [Runtime: 3 ms; Memory: 18.3 MB]

--- array/car-pooling.py
+++ array/car-pooling.py
@@ -1,6 +1,7 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         trips = sorted(trips, key = lambda x: x[1])
+        # print(trips)
 
         timestart = min([trip[1] for trip in trips])
         timeend = max([trip[2] for trip in trips])
@@ -11,7 +12,7 @@ class Solution:
 
         for cur_time in range(timestart, timeend + 1):
             while cur_guest and cur_guest[0][0] <= cur_time:
-                num_guest, _ = heapq.heappop(cur_guest)
+                _, num_guest = heapq.heappop(cur_guest)
                 cur_cap -= num_guest
 
             while cur_checkpoint < len(trips) and trips[cur_checkpoint][1] <= cur_time:
