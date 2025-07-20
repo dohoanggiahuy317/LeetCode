e3fca052fc67990036fa5121809bb96dc73ef75b
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-20 16:26:50 -0400

    LC-sort-list [Runtime: N/A; Memory: N/A]

--- divide-and-conquer/sort-list.py
+++ divide-and-conquer/sort-list.py
@@ -12,30 +12,29 @@ class Solution:
                 fa = fa.next.next
                 if fa:
                     sl = sl.next
-            
+
             half = sl.next
             sl.next = None
             return half
-        
+
         def merge(l1, l2):
             if not l1:
                 return l2
             if not l2:
                 return l1
-            
+
             if l1.val > l2.val:
-                l2.next = merge(l2.next, l1)
+                l2.next = merge(l1, l2.next)
                 return l2
-            
+
             l1.next = merge(l1.next, l2)
             return l1
 
         if not head or not head.next:
-            return head
+            return None
 
         half = split(head)
         l1 = self.sortList(half)
         l2 = self.sortList(head)
 
         return merge(l1, l2)
-                
