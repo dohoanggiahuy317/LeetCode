e6f469c812b61185e37fbbb0af0495d87146af7f
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-20 16:01:22 -0400

    LC-car-pooling [Runtime: N/A; Memory: N/A]

--- array/car-pooling.py
+++ array/car-pooling.py
@@ -5,18 +5,18 @@ class Solution:
         timestart = min([trip[1] for trip in trips])
         timeend = max([trip[2] for trip in trips])
 
-        cur_guest = deque()
+        cur_guest = []
         cur_cap = 0
         cur_checkpoint = 0
 
         for cur_time in range(timestart, timeend + 1):
-            while cur_guest and cur_guest[0][1] <= cur_time:
-                num_guest, _ = cur_guest.popleft()
+            while cur_guest and cur_guest[0][0] <= cur_time:
+                num_guest, _ = heapq.heappop(cur_guest)
                 cur_cap -= num_guest
 
             while cur_checkpoint < len(trips) and trips[cur_checkpoint][1] <= cur_time:
                 num_guest, st, en = trips[cur_checkpoint]
-                cur_guest.append((num_guest, en))
+                heapq.heappush(cur_guest, (en, num_guest))
                 cur_cap += num_guest
                 cur_checkpoint += 1
 
