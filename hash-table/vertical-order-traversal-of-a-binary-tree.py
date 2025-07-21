4c46d779f1304b670afd506312872a7c920d7146
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-21 15:47:50 -0400

    LC-vertical-order-traversal-of-a-binary-tree [Runtime: N/A; Memory: N/A]

--- hash-table/vertical-order-traversal-of-a-binary-tree.py
+++ hash-table/vertical-order-traversal-of-a-binary-tree.py
@@ -9,7 +9,7 @@ class Solution:
         tree_map = defaultdict(list)
         left, right = 0, 0
 
-        def dfs(root, x, y):
+        def dfs(root, y):
             nonlocal tree_map, left, right
             if not root:
                 return
@@ -17,18 +17,19 @@ class Solution:
             left = min(left, y)
             right = max(right, y)
 
-            tree_map[(x, y)].append(root.val)
-            dfs(root.left, x+1, y-1)
-            dfs(root.right, x+1, y+1)
+            tree_map[y].append(root.val)
+            dfs(root.left, y-1)
+            dfs(root.right, y+1)
 
         
-        dfs(root, 0, 0)
+        dfs(root, 0)
         ans = [[] for _ in range(right - left + 1)]
 
         for coor, subrow in list(tree_map.items()):
-            x, y = coor
+            y = coor
             pos = y - left
             ans[pos] += sorted(subrow)
+            # print(subrow)
 
         return ans
 
