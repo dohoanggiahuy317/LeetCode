81190f2d14ddb58521c07117f2515eb653087eab
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-26 18:12:46 -0400

    LC-lru-cache [Runtime: N/A; Memory: N/A]

--- doubly-linked-list/lru-cache.py
+++ doubly-linked-list/lru-cache.py
@@ -11,8 +11,17 @@ class LRUCache:
         self.capacity = capacity
         self.length = 0
         self.MAP = defaultdict(lambda: None)
+
+    def add_node(self, node):
+        if not self.head:
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
     
-    def helper(self, node):
+    def remove_node(self, node):
         prev_node = node.prev
         next_node = node.next
 
@@ -20,45 +29,38 @@ class LRUCache:
             prev_node.next = next_node
         if next_node:
             next_node.prev = prev_node
-        node.next  = node.prev = None
+        node.next = node.prev = None
 
-        if self.tail == node:
+        if node == self.head:
+            self.head = next_node
+        if node == self.tail:
             self.tail = prev_node
-
-        node.next = self.head
-        if self.head:
-            self.head.prev = node
-        self.head = node
-        if not self.tail:
-            self.tail = node
 
     def get(self, key: int) -> int:
         node = self.MAP[key]
         if node:
-            self.helper(node)
+            self.remove_node(node)
+            self.add_node(node)
             return node.val
+            
         return -1
         
     def put(self, key: int, value: int) -> None:
         node = self.MAP[key]
         if node:
             node.val = value
-            self.helper(node)
+            self.get(node)
         else:
             while self.length >= self.capacity:
                 last_node = self.tail
-                prev_node = last_node.prev
-                if prev_node:
-                    prev_node.next = None
+                self.remove_node(last_node)
                 del self.MAP[last_node.key]
-                self.tail = prev_node
                 self.length -= 1
-                
+            
             new_node = Node(value, key)
-            self.helper(new_node)
+            self.add_node(new_node)
             self.MAP[key] = new_node
             self.length += 1
-
 
             
         
