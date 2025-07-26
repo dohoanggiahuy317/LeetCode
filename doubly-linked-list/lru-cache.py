6377bfb22716c1e79d8a60e96778ace4b97d90c8
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-26 17:53:33 -0400

    LC-lru-cache [Runtime: N/A; Memory: N/A]

--- doubly-linked-list/lru-cache.py
+++ doubly-linked-list/lru-cache.py
@@ -1,68 +1,70 @@
-class NodeVal:
-    def __init__(self, key, val):
-        self.val = val
+class Node:
+    def __init__(self, value, key):
+        self.prev = self.next = None
+        self.val = value
         self.key = key
-        self.nex, self.pre = None, None
-
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.MAP = defaultdict(lambda: None)
-        self.length = 0
-        self.head, self.tail = None, None
+        self.head = self.tail = None
         self.capacity = capacity
+        self.length = 0
+        self.MAP = defaultdict(lambda: None)
+    
+    def helper(self, node):
+        prev_node = node.prev
+        next_node = node.next
 
-    def remove(self, node):
-        if node.pre:
-            node.pre.nex = node.nex
-        else:
-            self.head = node.nex
+        if prev_node:
+            prev_node.next = next_node
+        if next_node:
+            next_node.prev = prev_node
+        node.next  = node.prev = None
 
-        if node.nex:
-            node.nex.pre = node.pre
-        else:
-            self.tail = node.pre
+        if self.tail == node:
+            self.tail = prev_node
 
-        node.pre = node.nex = None
-
-    def add_to_front(self, node):
-        node.pre  = None
-        node.nex  = self.head
+        node.next = self.head
         if self.head:
-            self.head.pre = node
-        self.head  = node
-        if not self.tail:
-            self.tail = node
-        
+            self.head.prev = node
+        self.head = node
+
     def get(self, key: int) -> int:
         node = self.MAP[key]
-        if not node:
-            return -1
-        self.remove(node)
-        self.add_to_front(node)
-        return node.val
-
+        if node:
+            self.helper(node)
+            return node.val
+        return -1
+        
     def put(self, key: int, value: int) -> None:
-        node = self.MAP.get(key)
+        node = self.MAP[key]
         if node:
             node.val = value
-            self.remove(node)
-            self.add_to_front(node)
+            self.helper(node)
         else:
-            new_node = NodeVal(key, value)
+            while self.length >= self.capacity:
+                last_node = self.tail
+                prev_node = last_node.prev
+                if prev_node:
+                    prev_node.next = None
+                del self.MAP[last_node.val]
+                self.tail = prev_node
+                self.length -= 1
+                
+            new_node = Node(value, key)
+            self.helper(new_node)
             self.MAP[key] = new_node
-            self.add_to_front(new_node)
+            if self.length == 0:
+                self.tail = new_node
             self.length += 1
+
+
+            
         
-        while self.length > self.capacity:
-            temp = self.tail.pre
-
-            temp.nex = None
-            del self.MAP[self.tail.key]
-            self.tail = temp
-
-            self.length -= 1
 
 
-
-        
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
