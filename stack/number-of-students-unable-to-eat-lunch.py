4d8c3f5ccf11a6dacfc6aec34d037e11fadced5e
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-26 22:46:35 -0400

    LC-number-of-students-unable-to-eat-lunch [Runtime: 0 ms; Memory: 18 MB]

--- stack/number-of-students-unable-to-eat-lunch.py
+++ stack/number-of-students-unable-to-eat-lunch.py
@@ -1,16 +1,12 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        counts = [0, 0]
-        for student in students:
-            counts[student] += 1
+        cs = Counter(students)
 
-        remaining = len(sandwiches)
-        for sandwich in sandwiches:
-            if counts[sandwich] == 0:
+        rem = len(sandwiches)
+        for sand in sandwiches:
+            if cs[sand] == 0:
                 break
-            if remaining == 0:
-                break
-            counts[sandwich] -= 1
-            remaining -= 1
+            cs[sand] -= 1
+            rem -= 1
         
-        return remaining
+        return rem
