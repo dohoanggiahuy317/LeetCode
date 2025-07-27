f13320261faa195e1056a57678a2b9cb04ee83fa
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-26 23:13:44 -0400

    LC-maximum-number-of-removable-characters [Runtime: N/A; Memory: N/A]

--- array/maximum-number-of-removable-characters.py
+++ array/maximum-number-of-removable-characters.py
@@ -1,34 +1,27 @@
 class Solution:
     def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
-        def check(p, s):
+        def check(p, smap):
             j = 0
-            for i in range(len(s)):
+            for i in range(len(smap)):
                 if j == len(p):
                     break
-                if s[i] == p[j]:
+                if smap[i][1] == p[j]:
                     j += 1
 
             return j == len(p)
 
-        removed_all = ""
-        removeble_sor = sorted(removable)
-        st = 0
-        for i in range(len(s)):
-            if st < len(removeble_sor) and i == removeble_sor[st]:
-                st += 1
-            else:
-                removed_all += s[i]
+        smap = defaultdict()
+        for i, char in enumerate(s):
+            smap[i] = char
         
-        temp = []
-        for idx in removable[::-1]:
-            temp.append(removed_all)
-            removed_all = removed_all[:idx] + s[idx] + removed_all[idx:]
-            
-        temp = [s] + temp[::-1]
+        temp = [smap]
+        for idx in removable:
+            del smap[idx]
+            temp.append(list(smap.items()))
         ans = 0
 
         l, r = 0, len(temp) - 1
-        while l < r:
+        while l <= r:
             m = (l + r) // 2
             if check(p, temp[m]):
                 ans = m
