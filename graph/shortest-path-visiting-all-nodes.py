03c14c392c64e9c9cec30b6156d78ae12b7a8c6c
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-30 14:05:07 -0400

    LC-shortest-path-visiting-all-nodes [Runtime: 719 ms; Memory: 19.5 MB]

--- graph/shortest-path-visiting-all-nodes.py
+++ graph/shortest-path-visiting-all-nodes.py
@@ -6,40 +6,35 @@ class Solution:
 
         for start in range(n):
 
-            queue = deque([(start, 1)])
-            reachable = set(queue)
-            found_all = False
+            queue = deque([(start, 1 << start)])
+            reachable = set(queue) 
             step = 0
-            
-            print("start", start)
-            
-
-            while queue and not found_all:
-                # print(queue)
-                # print(reachable)
-                # print(step)
+                
+            while queue:
                 for _ in range(len(queue)):
-                    node, count = queue.popleft()
+                    node, past_node = queue.popleft()
                     
-                    if count == n:
+                    if past_node == (1 << n) - 1:
+
                         ans = min(ans, step)
-                        found_all = True
-                        break
 
                     for neigh in graph[node]:
                         
-                        new_count = count
-                        if all((neigh, i) not in reachable for i in range(count)):
-                            new_count = count + 1
-
-                        if (neigh, new_count) in reachable:
+                        present_node = past_node | (1 << neigh)
+                        if (neigh, present_node) in reachable:
                             continue
 
-                        queue.append((neigh, new_count))
-                        reachable.add((neigh, new_count))
+                        queue.append((neigh, present_node))
+                        reachable.add((neigh, present_node))
 
                 step += 1
 
-        return 0 if ans == inf else ans
+        return ans
+#     4 - 5
+#         |
+# 0 - 1 - 6 - 7
+#     |
+#     2 - 3
 
 
+     
