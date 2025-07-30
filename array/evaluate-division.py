8ddd97ae7bcd473a6456e0c1cd23ad27a3927ba5
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-30 15:50:59 -0400

    LC-evaluate-division [Runtime: 0 ms; Memory: 17.7 MB]

--- array/evaluate-division.py
+++ array/evaluate-division.py
@@ -3,17 +3,15 @@ INIT_QUOTIENT = 1
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         
-        ans = []
         eq_map = defaultdict(list)
-
         for (x, y), value in zip(equations, values):
             eq_map[x].append((y, value))
             eq_map[y].append((x, 1/value))
 
+        ans = []
+
+        # loop through query
         for divider, divisor in queries:
-            if divisor not in eq_map or divider not in eq_map:
-                ans.append(-1)
-                continue
 
             queue = deque([(divider, INIT_QUOTIENT)])
             reachable = set(queue)
@@ -22,6 +20,9 @@ class Solution:
             while queue:
                 for _ in range(len(queue)):
                     node, prev_quotient = queue.popleft()
+
+                    if node not in eq_map:
+                        break
 
                     if node == divisor:
                         result = prev_quotient
