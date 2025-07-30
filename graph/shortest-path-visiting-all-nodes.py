b571178ef0258fec51101b30dbe2ad003c95a8c9
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-30 14:20:30 -0400

    LC-shortest-path-visiting-all-nodes [Runtime: 711 ms; Memory: 19.4 MB]

--- graph/shortest-path-visiting-all-nodes.py
+++ graph/shortest-path-visiting-all-nodes.py
@@ -6,16 +6,16 @@ class Solution:
         for start in range(n):
             queue = deque([(start, 1 << start)])
             reachable = set(queue)
-            found_all = False 
+            found_all = False # optimize by ending early
             step = 0
                 
-            while queue and not found_all:
+            while queue and not found_all: # optimize by ending early
                 for _ in range(len(queue)):
                     node, past_path = queue.popleft()
                     
                     if past_path == (1 << n) - 1:
                         ans = min(ans, step)
-                        found_all = True
+                        found_all = True # optimize by ending early
                         break
 
                     for neigh in graph[node]:
