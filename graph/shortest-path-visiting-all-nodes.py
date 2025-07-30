b50c8a5bce573edb8393fb0d2ee4bf9bc7ed8b35
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-30 14:05:58 -0400

    LC-shortest-path-visiting-all-nodes [Runtime: N/A; Memory: N/A]

--- graph/shortest-path-visiting-all-nodes.py
+++ graph/shortest-path-visiting-all-nodes.py
@@ -1,31 +1,28 @@
 class Solution:
-    def shortestPathLength(self, graph: List[List[int]]) -> int:
-        
+    def shortestPathLength(self, graph: List[List[int]]) -> int
         n = len(graph)
         ans = inf
 
         for start in range(n):
-
             queue = deque([(start, 1 << start)])
             reachable = set(queue) 
             step = 0
                 
             while queue:
                 for _ in range(len(queue)):
-                    node, past_node = queue.popleft()
+                    node, past_path = queue.popleft()
                     
-                    if past_node == (1 << n) - 1:
-
+                    if past_path == (1 << n) - 1:
                         ans = min(ans, step)
 
                     for neigh in graph[node]:
                         
-                        present_node = past_node | (1 << neigh)
-                        if (neigh, present_node) in reachable:
+                        current_path = past_path | (1 << neigh)
+                        if (neigh, current_path) in reachable:
                             continue
 
-                        queue.append((neigh, present_node))
-                        reachable.add((neigh, present_node))
+                        queue.append((neigh, current_path))
+                        reachable.add((neigh, current_path))
 
                 step += 1
 
