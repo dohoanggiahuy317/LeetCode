369f5947cae9ada36b15523ddf34e798ec33e9fb
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-30 15:06:48 -0400

    LC-rotting-oranges [Runtime: N/A; Memory: N/A]

--- breadth-first-search/rotting-oranges.py
+++ breadth-first-search/rotting-oranges.py
@@ -2,6 +2,7 @@ DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        
         m, n = len(grid), len(grid[0])
         good_orange = 0
         queue = deque()
@@ -16,14 +17,15 @@ class Solution:
         reachable = set(queue)
         time = 0
 
-        while queue:
 
+        while queue:
+            
+            # Return if there is no good orange left
             if good_orange == 0:
                 return time
 
             for _ in range(len(queue)):
                 i, j = queue.popleft()
-
 
                 for x, y in DIRECTIONS:
                     ni, nj = i + x, j + y
@@ -35,17 +37,14 @@ class Solution:
                     if grid[ni][nj] == 2:
                         continue
 
+                    # turn the orange to be rotten, once it get rotten, no need to 
+                    # put it in the queue twice, so update the grid[i][j] to 2
                     grid[ni][nj] = 2
                     good_orange -= 1
 
                     queue.append((ni, nj))
                     reachable.add((ni, nj))
             
-            time += 1
+            time += 1 # time done after each infection round
 
-        return -1 if good_orange != 0 else 0
-
-
-# 2 1 1
-# 1 1 1
-# 0 1 2
+        return -1
