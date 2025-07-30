a3537d86cc4adae2e0574647d3f11eff3530966c
Jeremy <giahuy.jeremy.do@gmail.com>
2025-07-30 14:26:53 -0400

    LC-shortest-path-visiting-all-nodes [Runtime: 117 ms; Memory: 22.5 MB]

--- bit-manipulation/shortest-path-visiting-all-nodes.py
+++ bit-manipulation/shortest-path-visiting-all-nodes.py
@@ -1,35 +1,28 @@
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
         n = len(graph)
-        ans = inf
-
-        for start in range(n):
-            queue = deque([(start, 1 << start)])
-            reachable = set(queue)
-            found_all = False # optimize by ending early
-            step = 0
+        queue = deque([(start, 1 << start) for start in range(n)])
+        reachable = set(queue)
+        step = 0
+            
+        while queue:
+            for _ in range(len(queue)):
+                node, past_path = queue.popleft()
                 
-            while queue and not found_all: # optimize by ending early
-                for _ in range(len(queue)):
-                    node, past_path = queue.popleft()
+                if past_path == (1 << n) - 1:
+                    return step
+
+                for neigh in graph[node]:
                     
-                    if past_path == (1 << n) - 1:
-                        ans = min(ans, step)
-                        found_all = True # optimize by ending early
-                        break
+                    current_path = past_path | (1 << neigh)
+                    if (neigh, current_path) in reachable:
+                        continue
 
-                    for neigh in graph[node]:
-                        
-                        current_path = past_path | (1 << neigh)
-                        if (neigh, current_path) in reachable:
-                            continue
+                    queue.append((neigh, current_path))
+                    reachable.add((neigh, current_path))
 
-                        queue.append((neigh, current_path))
-                        reachable.add((neigh, current_path))
+            step += 1
 
-                step += 1
-
-        return ans
 
 
 
