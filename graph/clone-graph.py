f0b723e7084f7dd9e3c3f1aa18d6bdd4a2acf226
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-01 13:44:02 -0400

    LC-clone-graph [Runtime: N/A; Memory: N/A]

--- graph/clone-graph.py
+++ graph/clone-graph.py
@@ -6,35 +6,7 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
+from typing import Optional
 class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        if node == None:
-            return None
-        graph = {}
-
-        def bfs(node):
-            if node.val in graph.keys():
-                return
-
-            graph[node.val] = []
-
-            for neighbor in node.neighbors:
-                graph[node.val].append(neighbor.val)
-
-            for neighbor in node.neighbors:
-                bfs(neighbor)
-
-        bfs(node)
-        node_li = {}
-        new_node = Node(node.val)
-        node_li[1] = new_node
-
-        for x in graph.keys():
-            if x != 1:
-                node_li[x] = Node(x)
-        
-        for k, v in graph.items():
-            for neighbor in v:
-                node_li[k].neighbors.append(node_li[neighbor])
-
-        return new_node
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        return node
