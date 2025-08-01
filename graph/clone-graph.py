be58bc950b042b19a31b74e4be6d7a80cb614030
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-01 14:28:13 -0400

    LC-clone-graph [Runtime: 48 ms; Memory: 18 MB]

--- graph/clone-graph.py
+++ graph/clone-graph.py
@@ -8,5 +8,42 @@ class Node:
 
 from typing import Optional
 class Solution:
+
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        return node
+        
+        if not node:
+            return None
+
+        def getCopy(neigh):
+            nonlocal tree_map
+
+            if neigh not in tree_map:
+                copy_neigh = Node(neigh.val)
+                tree_map[neigh] = copy_neigh
+
+            return tree_map[neigh]
+
+
+        copy_node = Node(node.val)
+        tree_map = {node: copy_node}
+        queue = deque([(node, copy_node)])
+        reachable = set([node])
+
+
+        while queue:
+
+            for _ in range(len(queue)):
+                curr, copy_curr = queue.popleft()
+
+                for neigh in curr.neighbors:
+
+                    neigh_copy = getCopy(neigh)
+                    copy_curr.neighbors.append(neigh_copy)
+
+                    if neigh in reachable:
+                        continue
+
+                    queue.append((neigh, neigh_copy))
+                    reachable.add(neigh)
+
+        return copy_node
