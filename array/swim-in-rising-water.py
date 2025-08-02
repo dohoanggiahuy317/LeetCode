370502bb22b7d0898f9f8c01275153be7dfdb360
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-01 23:41:15 -0400

    LC-swim-in-rising-water [Runtime: N/A; Memory: N/A]

---
+++ array/swim-in-rising-water.py
@@ -0,0 +1,35 @@
+START_X, START_Y, START_TIME = 0, 0, 0
+DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        
+        m, n = len(grid), len(grid[0])
+        queue = deque([ (START_X, START_Y, START_TIME) ])
+        reachable = set(queue)
+        ans = inf
+
+        while queue:
+            for _ in range(len(queue)):
+                cur_x, cur_y, cur_time = queue.popleft()
+
+                if cur_x == m - 1 and cur_y == n - 1:
+                    ans = min(ans, cur_time)
+
+                for i, j in DIRECTIONS:
+                    n_x, n_y, n_time = cur_x + i, cur_y + j, cur_time
+
+                    if not(0 <= n_x < m and 0 <= n_y < n):
+                        continue
+                    
+                    if grid[n_x][n_y] > cur_time:
+                        n_time = grid[n_x][n_y]
+
+                    if (n_x, n_y, n_time) in reachable:
+                        continue
+
+                    queue.append( (n_x, n_y, n_time) )
+                    reachable.add((n_x, n_y, n_time))
+
+
+        return ans
