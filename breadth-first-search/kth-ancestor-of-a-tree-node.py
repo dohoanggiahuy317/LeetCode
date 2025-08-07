09fc5e8a49c7674f499491abc556ddf470398c5c
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-06 21:44:54 -0400

    LC-kth-ancestor-of-a-tree-node [Runtime: 257 ms; Memory: 51.1 MB]

--- breadth-first-search/kth-ancestor-of-a-tree-node.py
+++ breadth-first-search/kth-ancestor-of-a-tree-node.py
@@ -1,32 +1,25 @@
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.parent = None
-        self.child1 = self.child2 = None
-
 class TreeAncestor:
 
     def __init__(self, n: int, parent: List[int]):
-        self.d = {}
-        for i in range(-1, n):
-            self.d[i] = Node(i)
-        
-        for i, p in enumerate(parent):
-            self.d[i].parent = self.d[p]
-            if not self.d[p].child1:
-                self.d[p].child1 = self.d[i]
-            else: 
-                self.d[p].child2 = self.d[i]
+        self.LOG = n.bit_length()
+        self.dp = [[-1] *self.LOG for _ in range(n)]
+
+        for i in range(n):
+            self.dp[i][0] = parent[i]
+
+        for j in range(1,self.LOG):
+            for i in range(n):
+                prev = self.dp[i][j - 1]
+                self.dp[i][j] = -1 if prev < 0 else self.dp[prev][j-1]
 
     def getKthAncestor(self, node: int, k: int) -> int:
-        i = 0
-        while node != -1 and i < k:
-            node = self.d[node].parent.val
-            i += 1
+
+        for j in range(self.LOG):
+            if k & (1 << j):
+                node = self.dp[node][j]
+                if node < 0:
+                    return -1
         return node
-
-        
-
 
 # Your TreeAncestor object will be instantiated and called as such:
 # obj = TreeAncestor(n, parent)
