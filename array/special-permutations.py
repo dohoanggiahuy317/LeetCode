95837b2e7940c43fc916926adcb193050acee361
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-09 23:01:29 -0400

    LC-special-permutations [Runtime: N/A; Memory: N/A]

--- array/special-permutations.py
+++ array/special-permutations.py
@@ -3,37 +3,38 @@ class Solution:
         
         n = len(nums)
         MOD = 10**9 + 7
-        d = defaultdict(list)
 
+        d = defaultdict(list)
         for a in nums:
             for b in nums:
                 if a != b and (a % b == 0 or b % a == 0):
                     d[a].append(b)
 
         ans = 0
+        visited = set()
 
-        def dfs(val, count):
-            nonlocal n, d, visited, ans, MOD
-
+        def dfs(val: int, count: int) -> None:
+            nonlocal ans
             if val in visited:
                 return
+
 
             if count == n:
                 ans = (ans + 1) % MOD
                 return
 
-            # print(val)
-            
+
             visited.add(val)
 
             for neigh in d[val]:
                 dfs(neigh, count + 1)
 
-        for val in nums:
-            # print("STAT")
-            visited = set()
-            dfs(val, 1)
+                
+            visited.remove(val)
 
-        return ans
 
-        
+
+        for start in nums:
+            dfs(start, 1)
+
+        return ans % MOD
