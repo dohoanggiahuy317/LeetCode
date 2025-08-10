fbced24a86a18d7a05cdc187e9320e49f9ede0e6
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-09 22:31:02 -0400

    LC-minimum-common-value [Runtime: 11 ms; Memory: 37.1 MB]

--- hash-table/minimum-common-value.py
+++ hash-table/minimum-common-value.py
@@ -1,26 +1,13 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        l1 = 0
-        l2 = 0
-
-        while l1 < len(nums1) and l2 < len(nums2):
-            if nums1[l1] == nums2[l2]:
-                return nums1[l1]
-            elif nums1[l1] < nums2[l2]:
-                l1 += 1
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            
+            if nums1[i] > nums2[j]:
+                j += 1
             else:
-                l2 += 1
-
-        while l1 == len(nums1) and l2 < len(nums2):
-            if nums1[-1] == nums2[l2]:
-                return nums1[-1]
-            else:
-                l2 += 1
-
-        while l2 == len(nums2) and l1 < len(nums1):
-            if nums2[-1] == nums1[l1]:
-                return nums2[-1]
-            else:
-                l1 += 1
+                i += 1
 
         return -1
