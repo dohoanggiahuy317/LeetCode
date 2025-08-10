6d6aea9aad4eb6ba55f1a440258445c5bf88a909
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-10 13:22:00 -0400

    LC-cut-off-trees-for-golf-event [Runtime: N/A; Memory: N/A]

--- array/cut-off-trees-for-golf-event.py
+++ array/cut-off-trees-for-golf-event.py
@@ -1,18 +1,21 @@
-START_X, START_Y, INIT_CUT = 0, 0, 0
-DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
 
 class Solution:
     def cutOffTree(self, forest: List[List[int]]) -> int:
+        START_X, START_Y, INIT_CUT = 0, 0, 0
+        DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        WALL, EMPTY = 0, 1
         m, n = len(forest), len(forest[0])
 
         tree_list = []
-        for i in range(m):
-            for j in range(n):
-                if forest[i][j] != 0 and forest[i][j] != 1:
+        for i, row in enumerate(forest):
+            for j, tree in enumerate(row):
+                if tree != WALL and forest[i][j] != EMPTY:
                     tree_list.append(forest[i][j])
         tree_list.sort()
+        
         cut_idx = 0
-
+        num_tree = len(tree_list)
         queue = deque([(START_X, START_Y, cut_idx)])
         reachable = set(queue)
         step = 0
@@ -23,28 +26,29 @@ class Solution:
 # 7 6 5
         while queue:
             for _ in range(len(queue)):
-                cx, cy, cut_idx = queue.popleft()
+                c_x, c_y, c_idx = queue.popleft()
+                n_idx = c_idx
 
-                if forest[cx][cy] == tree_list[cut_idx]:
-                    cut_idx += 1
+                if forest[c_x][c_y] == tree_list[c_idx]:
+                    n_idx = c_idx + 1
 
-                if cut_idx == len(tree_list):
+                if n_idx == num_tree:
                     return step
 
                 for i, j in DIRECTIONS:
-                    nx, ny = cx + i, cy + j
+                    n_x, n_y = c_x + i, c_y + j
 
-                    if not (0 <= nx < m and 0 <= ny < n):
+                    if not (0 <= n_x < m and 0 <= n_y < n):
                         continue
 
-                    if forest[nx][ny] == 0:
+                    if forest[n_x][n_y] == WALL or forest[n_x][n_y] == EMPTY:
                         continue
 
-                    if (nx, ny, cut_idx) in reachable:
+                    if (n_x, n_y, n_idx) in reachable:
                         continue
 
-                    queue.append((nx, ny, cut_idx))
-                    reachable.add((nx, ny, cut_idx))
+                    queue.append((n_x, n_y, n_idx))
+                    reachable.add((n_x, n_y, n_idx))
 
             step += 1
 
