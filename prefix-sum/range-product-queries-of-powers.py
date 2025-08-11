ef661d68ff8ad1cc66213ce8df719b4d7a09837f
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-10 23:27:38 -0400

    LC-range-product-queries-of-powers [Runtime: N/A; Memory: N/A]

---
+++ prefix-sum/range-product-queries-of-powers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        powers = []
+        MOD = 10 ** 9 + 7
+
+        while n > 0:
+            power = int(math.log(n, 2))
+            powers.append(2 ** power)
+            n -= 2 ** power
+
+        powers.sort()
+        pref_prod = [1]
+        curr_prod = 1
+        for power in powers:
+            curr_prod = (curr_prod * power) % MOD
+            pref_prod.append(curr_prod)        
+
+        ans = []
+        for l, r in queries:
+            ans.append(pref_prod[r+1]//pref_prod[l])
+
+        return ans
