5450578b6e7529ed51a3f47fd9534839d6954cd0
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-14 15:54:13 -0400

    LC-swim-in-rising-water [Runtime: 95 ms; Memory: 18.5 MB]

--- heap-priority-queue/swim-in-rising-water.py
+++ heap-priority-queue/swim-in-rising-water.py
@@ -8,8 +8,8 @@ class Solution:
         T_X, T_Y = m - 1, n - 1
         SWIMABLE, UNSWIMABLE = True, False
         
-        # BFS
-        def bfs(target_time):
+        # can_swim
+        def can_swim(target_time):
             nonlocal S_X, S_Y, T_X, T_Y
 
             queue = deque([ (S_X, S_Y) ])
@@ -17,40 +17,37 @@ class Solution:
 
             while queue:
                 for _ in range(len(queue)):
-                    c_x, c_y = queue.popleft()
+                    cx, cy = queue.popleft()
 
-                    if c_x == T_X and c_y == T_Y:
+                    if cx == T_X and cy == T_Y:
                         return SWIMABLE
 
                     for i, j in DIRECTIONS:
-                        n_x, n_y = c_x + i, c_y + j
+                        nx, ny = cx + i, cy + j
 
-                        if not(0 <= n_x < m and 0 <= n_y < n):
+                        if not(0 <= nx < m and 0 <= ny < n):
                             continue
                         
-                        if grid[n_x][n_y] > target_time:
+                        if grid[nx][ny] > target_time:
                             continue
                         
-                        if (n_x, n_y) in reachable:
+                        if (nx, ny) in reachable:
                             continue
 
-                        queue.append( (n_x, n_y) )
-                        reachable.add((n_x, n_y))
+                        queue.append( (nx, ny) )
+                        reachable.add((nx, ny))
 
             return UNSWIMABLE
 
         # START SWIMING
         l = grid[S_X][S_Y]
-        r = -inf
-        for row in grid:
-            for cell in row:
-                r = max(cell, r)
+        r = max(max(row) for row in grid)
         ans = -1
         
         while l <= r:
             m_target_time = (l + r) // 2
 
-            if bfs(m_target_time):
+            if can_swim(m_target_time):
                 ans = m_target_time
                 r = m_target_time - 1
             else:
