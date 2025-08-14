a5d27192199b9453dfad642813b10f2549146f93
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-13 23:17:38 -0400

    LC-evaluate-division [Runtime: 0 ms; Memory: 18.2 MB]

--- breadth-first-search/evaluate-division.py
+++ breadth-first-search/evaluate-division.py
@@ -1,42 +1,47 @@
-INIT_QUOTIENT = 1
+
 
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        # Constant
+        INIT_QUOTIENT = 1
 
+        # BFS for each query
         def bfs(divider, divisor):
             nonlocal ans, eq_map
 
             queue = deque([(divider, INIT_QUOTIENT)])
-            reachable = set(queue)
+            reachable = set([divider]) # -> dont want to divide smth twice
             result = -1
 
             while queue:
                 for _ in range(len(queue)):
-                    node, prev_quotient = queue.popleft()
+                    prev_divisor, prev_quotient = queue.popleft()
 
-                    if node not in eq_map:
+                    if prev_divisor not in eq_map: # prev_divisor not appear as divider 
                         break
 
-                    if node == divisor:
+                    if prev_divisor == divisor:
                         result = prev_quotient
                         break
 
-                    for neigh, value in eq_map[node]:
+                    for neigh, value in eq_map[prev_divisor]: 
+                    # prev_divisor/neigh = value
 
                         if neigh in reachable:
                             continue
                         
-                        new_quotient = prev_quotient * value
+                        # prev_divisor/neigh * neigh/neigh_of_neigh * ...
+                        new_quotient = prev_quotient * value 
+                        
                         queue.append((neigh, new_quotient))
                         reachable.add(neigh)
 
             ans.append(result)
 
 
-        
-        eq_map = defaultdict(list)
+        eq_map = defaultdict(list) # eq_map[x] -> (y, value) where x/y = value
         for (x, y), value in zip(equations, values):
-            eq_map[x].append((y, value))
+            eq_map[x].append((y, value)) 
             eq_map[y].append((x, 1/value))
 
         ans = []
