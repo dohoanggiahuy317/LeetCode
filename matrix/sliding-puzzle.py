4b3db52bdf9b6d9d50daa1f237857cae107d3dda
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-15 16:11:49 -0400

    LC-sliding-puzzle [Runtime: 7 ms; Memory: 18 MB]

--- matrix/sliding-puzzle.py
+++ matrix/sliding-puzzle.py
@@ -5,22 +5,43 @@ SWAP_DIRECTIONS = {
 
 class Solution:
     def slidingPuzzle(self, board: List[List[int]]) -> int:
+        # CONSTANT
         m, n = len(board), len(board[0])
+        length = m * n
+        MASK = (1 << 3) - 1
 
-        TARGET = "123450"
-        board_1d = "".join( [str(val) for row in board for val in row] )
-        x0 = board_1d.index("0")
+        def pack_3bit(values):
+            pack_num = 0
+            x0 = -1
+            for i, v in enumerate(values):
+                pack_num = (pack_num << 3) | v
+                if v == 0:
+                    x0 = i
+            return pack_num, x0
 
-        def get_next_states(board_1d, x0):
-            nonlocal m, n
+        def tile_swap(board_1d, x0):
+            nonlocal length
+            
             neighs = []
+            x0_shift = (length - 1 - x0) * 3
 
-            for direction in SWAP_DIRECTIONS[x0]:
-                board_li = list(board_1d)
-                board_li[x0], board_li[direction] = board_li[direction], board_li[x0]
-                neighs.append(("".join(board_li), direction))
+            for di in SWAP_DIRECTIONS[x0]:
+                
+                di_shift = (length - 1 - di) * 3
+                di_pack = (board_1d >> di_shift) & MASK
+
+                # Clear the 3-bit at the postion that will be swapped with 0
+                neigh = board_1d & ~(MASK << di_shift)
+
+                # Set the 3-bit at postion that used to 0 to current pack 
+                neigh = neigh | (di_pack << x0_shift)
+                
+                neighs.append((neigh, di))
 
             return neighs
+
+        TARGET, _ = pack_3bit([1, 2, 3, 4, 5, 0])
+        board_1d, x0 = pack_3bit(cell for row in board for cell in row)
 
         # BFS
         queue = deque( [(board_1d, x0)] )
@@ -34,7 +55,7 @@ class Solution:
                 if curr_board_1d == TARGET:
                     return step
 
-                for neigh, new_x0 in get_next_states(curr_board_1d, curr_x0):
+                for neigh, new_x0 in tile_swap(curr_board_1d, curr_x0):
                     if (neigh, new_x0) in reachable:
                         continue
                     
