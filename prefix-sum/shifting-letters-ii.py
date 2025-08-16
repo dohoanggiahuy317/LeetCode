12debf2c8375a2193543f187fe723f74ed5b5666
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-15 23:48:18 -0400

    LC-shifting-letters-ii [Runtime: 50 ms; Memory: 42 MB]

--- prefix-sum/shifting-letters-ii.py
+++ prefix-sum/shifting-letters-ii.py
@@ -1,18 +1,20 @@
+
+
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         n = len(s)
         track = [0] * (n+1)
-        MAP = {0: -1, 1: 1}
+        di_map = {0: -1, 1: 1}
 
-        for st, en, di in shifts:
-            track[st] += MAP[di]
-            track[en+1] -= MAP[di]
+        for start, end, di in shifts:
+            track[start] += di_map[di]
+            track[end + 1] -= di_map[di]
 
+        shift_map = list(accumulate(track))
         ans = ""
-        
-        for char, amount in zip(s, accumulate(track)) :
-            ans += chr((ord(char) - ord("a") + amount) % 26 + ord("a"))
-
+        for i, ch in enumerate(s):
+            ans += chr((ord(ch) + shift_map[i] - ord("a")) % 26 + ord("a"))
 
         return ans
 
+        
