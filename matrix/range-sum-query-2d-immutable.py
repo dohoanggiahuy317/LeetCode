a53944d38d7619993fffcdba716fc21acb70e2df
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-16 15:58:41 -0400

    LC-range-sum-query-2d-immutable [Runtime: N/A; Memory: N/A]

--- matrix/range-sum-query-2d-immutable.py
+++ matrix/range-sum-query-2d-immutable.py
@@ -2,24 +2,22 @@ class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         m, n = len(matrix), len(matrix[0])
-        self.ps = [[0] * n for _ in range(m)]
+        self.matrix_sum = [[1] * n for _ in range(m)]
 
         for i in range(m):
-            prefSum = [0] * n
-            prefSum[0] = matrix[i][0]
+            row_sum = list(accumulate(matrix[i]))
             for j in range(n):
-                prefSum[j] = (prefSum[j - 1] if j > 0 else 0) + matrix[i][j]
-                square_sum = prefSum[j]
-                if i > 0:
-                    square_sum += self.ps[i - 1][j]
-                self.ps[i][j] = square_sum
+                a = row_sum[j - 1] if j > 0 else 0
+                b = self.matrix_sum[i - 1][j] if i > 0 else 0
+                self.matrix_sum[i][j] = a + b + matrix[i][j]
 
-                
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        left_square = self.ps[row2][col1 - 1] if col1 > 0 else 0
-        upper_square = self.ps[row1 - 1][col2] if row1 > 0 else 0
-        diag_square = self.ps[row1 - 1][col1 - 1] if (row1 > 0 and col1 > 0) else 0
-        return self.ps[row2][col2] - left_square - upper_square + diag_square
+        t1 = self.matrix_sum[row2][col2]
+        t2 = self.matrix_sum[row1 - 1][col2] if row1 > 0 else 0
+        t3 = self.matrix_sum[row2][col1 - 1] if row1 > 0 else 0
+        t4 = self.matrix_sum[row1 - 1][col1 - 1] if (row1 > 0 and col1 > 0) else 0
+
+        return t4 - t3 - t2 + t1
 
 
 # Your NumMatrix object will be instantiated and called as such:
