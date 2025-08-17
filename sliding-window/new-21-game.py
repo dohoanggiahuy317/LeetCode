74d4b470cd4384464a0077f0a2f3c56e58a964a9
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-16 22:09:36 -0400

    LC-new-21-game [Runtime: N/A; Memory: N/A]

--- sliding-window/new-21-game.py
+++ sliding-window/new-21-game.py
@@ -2,25 +2,26 @@ class Solution:
     def new21Game(self, n: int, k: int, maxPts: int) -> float:
         if k == 0:
             return 1
+        # dp[i] = prob reach score i
+        # dp[i] = sum(dp[i-maxPts] -> dp[i-1]) * 1/maxPts
+        # dp[i] = (pref_sum[i-1] - pref_sum[i-maxPts - 1]) * 1/maxPts
+       
+        dp = defaultdict(int, {0: 1}) # starts at 0 so prob at 0 is 1
+        pref_sum = defaultdict(int, {0: 1}) # pref_sum of dp
+
+        for i in range(1, n + 1):
+            largest = min(i - 1, k - 1)
+            smallest = i - maxPts - 1
+            prev_prob = pref_sum[largest] - (pref_sum[smallest] if smallest >= 0 else 0)
+
+            prob_i = (prev_prob) * 1/maxPts
+            
+            dp[i] = prob_i
+            pref_sum[i] = pref_sum[i - 1] + prob_i
+
         
-        dp = {0:1}
-        dp_s = {-1: 0, 0: 1}
-
-
-        for i in range(1, n+1):
-            s = dp_s[min(i - 1, k - 1)] - dp_s[max(i - maxPts - 1, -1)]
-            if s < 0:
-                s = 0
-            dp[i] = s * (1/maxPts)
-            dp_s[i] = dp_s[i - 1] + dp[i]
-
         ans = 0
-
-        for i, v in dp.items():
-            if i >= k:
-                ans += v 
-
+        for i in range(k, n+1):
+            ans += dp[i]
 
         return ans
-
-
