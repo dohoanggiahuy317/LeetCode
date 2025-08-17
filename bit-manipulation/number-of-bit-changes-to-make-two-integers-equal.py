540ac4b07fb2c0a150828fc585baf87f36513eb5
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-16 22:55:27 -0400

    LC-number-of-bit-changes-to-make-two-integers-equal [Runtime: 0 ms; Memory: 17.7 MB]

--- bit-manipulation/number-of-bit-changes-to-make-two-integers-equal.py
+++ bit-manipulation/number-of-bit-changes-to-make-two-integers-equal.py
@@ -1,18 +1,18 @@
 class Solution:
     def minChanges(self, n: int, k: int) -> int:
-        bin_n = bin(n)[2:]
-        bin_k = bin(k)[2:]
+        bn = bin(n)[2:]
+        bk = bin(k)[2:]
 
-        t = max(len(bin_n), len(bin_k))
+        if len(bn) < len(bk):
+            return -1
+        
+        bk = "0" * (len(bn) - len(bk)) + bk
 
-        bin_n = "0" * (t - len(bin_n)) + bin_n
-        bin_k = "0" * (t - len(bin_k)) + bin_k
-
-        # print(bin_n, bin_k)
         ans = 0
-        for i in range(t):
-            if bin_n[i] == "1" and bin_k[i] == "0":
+        for i in range(len(bn)):
+            if bn[i] == "1" and bk[i] == "0":
                 ans += 1
-            elif bin_n[i] == "0" and bin_k[i] == "1":
+            elif bn[i] == "0" and bk[i] == "1":
                 return -1
+
         return ans
