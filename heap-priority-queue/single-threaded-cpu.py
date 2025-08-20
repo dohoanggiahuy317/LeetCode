4fdeaa74c82452da961eaf16e096e97220e4a5b7
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-20 19:40:13 -0400

    LC-single-threaded-cpu [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/single-threaded-cpu.py
+++ heap-priority-queue/single-threaded-cpu.py
@@ -13,13 +13,13 @@ class Solution:
             # Load ready task
             while i < n and tasks[i][0] <= time:
                 enq_time, proc_time, idx = tasks[i]
-                heapq.heappush(pq, (proc_time, enq_time + proc_time, idx))
+                heapq.heappush(pq, (proc_time, idx))
                 i += 1
             
             if pq:
-                _, end_time, idx = heapq.heappop(pq)
+                proc_time, idx = heapq.heappop(pq)
                 ans.append(idx)
-                time = max(time, end_time)
+                time += proc_time # add tiem that proc the current process
             
             else: # if CPU done all task, move to next time frame
                 time += tasks[i][0]
