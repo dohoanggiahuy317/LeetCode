f47554dc416afd07bbb761e305bd4011b5adcc95
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-20 20:48:16 -0400

    LC-meeting-rooms-iii [Runtime: N/A; Memory: N/A]

--- simulation/meeting-rooms-iii.py
+++ simulation/meeting-rooms-iii.py
@@ -5,33 +5,36 @@ class Solution:
 
         occups_pq = [] # -> (end_time, room_idx)
         avail_pq = [i for i in range(n)] # -> room_idx
-        i = time = 0
+        i = curr_time = 0
         room_count = Counter()
 
         # while còn meeting chưa được vô
         while occups_pq or i < meets_num:
             # Lấy ra hết các room end meeting và add vào available list
-            while occups_pq and occups_pq[0][0] <= time:
+            while occups_pq and occups_pq[0][0] <= curr_time:
                 _, room_idx = heapq.heappop(occups_pq)
                 heapq.heappush(avail_pq, room_idx)
 
             # add meetings đang chờ vào room cho đến khi hết available room, cập nhất occup_room
-            while i < meets_num and meetings[i][0] <= time and avail_pq:
+            while i < meets_num and meetings[i][0] <= curr_time and avail_pq:
                 room_idx = heapq.heappop(avail_pq)
                 start, end = meetings[i]
                 
                 duration = end - start
-                heapq.heappush(occups_pq, (time + duration, room_idx))
+                heapq.heappush(occups_pq, (curr_time + duration, room_idx))
                 room_count[room_idx] += 1
                 i += 1
             
-            # Có free room cho next meeting, nhảy đến time đó và loop sẽ remove các meeting done
+            print(curr_time, occups_pq)
+            # Có free room cho next meeting, nhảy đến curr_time đó và loop sẽ remove các meeting done
             if avail_pq and i < meets_num:
-                time = meetings[i][0]
-            # nếu ko có room nào avail, xét time mà room tiếp theo avail
-            elif not avail_pq and occups_pq:
-                time = occups_pq[0][0]
-
+                curr_time = meetings[i][0]
+            # nếu ko có room nào avail, xét curr_time mà room tiếp theo avail
+            elif occups_pq:
+                curr_time = occups_pq[0][0]
+            # tăng
+            # else:
+            #     curr_time = max(time, meetings[i][0]) if i < meets_num else (occups_pq[0][0] if occups_pq else curr_time + 1)
             
 
         rooms = [(-c, idx) for idx, c in room_count.items()]
