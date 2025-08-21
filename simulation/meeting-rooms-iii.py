86627dc7b2607ff12af4a00d4c621bef3a4cd432
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-20 20:35:58 -0400

    LC-meeting-rooms-iii [Runtime: N/A; Memory: N/A]

--- simulation/meeting-rooms-iii.py
+++ simulation/meeting-rooms-iii.py
@@ -1,31 +1,38 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        ans = [0] * n
-        times = [0] * n
         meetings.sort()
+        meets_num = len(meetings)
 
-        for start, end in meetings:
-            flag = False
-            minind = -1
-            val = float('inf')
-            for j in range(n):
-                if times[j] < val:
-                    val = times[j]
-                    minind = j
-                if times[j] <= start:
-                    flag = True
-                    ans[j] += 1
-                    times[j] = end
-                    break
-            if not flag:
-                ans[minind] += 1
-                times[minind] += (end - start)
+        occups_pq = [] # -> (end_time, room_idx)
+        avail_pq = [i for i in range(n)] # -> room_idx
+        i = time = 0
+        room_count = Counter()
 
-        maxi = -1
-        id = -1
-        for i in range(n):
-            if ans[i] > maxi:
-                maxi = ans[i]
-                id = i
-        return id
+        # while còn meeting chưa được vô
+        while occups_pq or i < meets_num:
+            # Lấy ra hết các room end meeting và add vào available list
+            while occups_pq and occups_pq[0][0] <= time:
+                _, room_idx = heapq.heappop(occups_pq)
+                heapq.heappush(avail_pq, room_idx)
 
+            # add meetings đang chờ vào room cho đến khi hết available room, cập nhất occup_room
+            while i < meets_num and meetings[i][0] <= time and avail_pq:
+                room_idx = heapq.heappop(avail_pq)
+                start, end = meetings[i]
+                
+                duration = end - start
+                heapq.heappush(occups_pq, (time + duration, room_idx))
+                room_count[room_idx] += 1
+                i += 1
+
+            # # nếu có occup_room có người, xét time gần nhất khi có meeting end
+            # if occups_pq:
+            #     time = occups_pq[0][0]
+            time += 1
+
+        rooms = [(-c, idx) for idx, c in room_count.items()]
+        rooms.sort()
+
+        return rooms[0][1]
+
+        
