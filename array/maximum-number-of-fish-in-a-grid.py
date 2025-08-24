a1662908d01b6747ebe00744a4894dec85ffd619
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-24 14:44:58 -0400

    LC-maximum-number-of-fish-in-a-grid [Runtime: 64 ms; Memory: 18.3 MB]

--- array/maximum-number-of-fish-in-a-grid.py
+++ array/maximum-number-of-fish-in-a-grid.py
@@ -5,27 +5,28 @@ class Solution:
         m, n = len(grid), len(grid[0])
 
         def start_catch(x, y):
-            nonlocal grid, m, n, ans, visited, fish
+            nonlocal grid, m, n, visited
 
             if not (0 <= x < m and 0 <= y < n):
-                return
-            if grid[x][y] == 0:
-                return
+                return 0
             if (x, y) in visited:
-                return
-
-            fish += grid[x][y]
-            visited.add((x, y))
+                return 0
+            if grid[x][y] == 0:
+                return 0
             
+            visited.add((x, y))
+            fish = grid[x][y]
+
             for i, j in DIRECTIONS:
-                start_catch(x + i, y + j)
+                fish += start_catch(x + i, y + j)
+            
+            return fish
 
         visited = set()
         ans = 0
         for x in range(m):
             for y in range(n):
-                fish = 0
-                start_catch(x, y)
+                fish = start_catch(x, y)
                 ans = max(ans, fish)
 
         return ans
