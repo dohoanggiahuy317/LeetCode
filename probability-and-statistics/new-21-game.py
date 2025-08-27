126ce6d56969d133d1b9404418b174e5ad49fc9b
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-27 14:15:53 -0400

    LC-new-21-game [Runtime: N/A; Memory: N/A]

--- probability-and-statistics/new-21-game.py
+++ probability-and-statistics/new-21-game.py
@@ -1,27 +1,22 @@
 class Solution:
     def new21Game(self, n: int, k: int, maxPts: int) -> float:
-        if k == 0:
-            return 1
-        # dp[i] = prob reach score i
-        # dp[i] = sum(dp[i-maxPts] -> dp[i-1]) * 1/maxPts
-        # dp[i] = (pref_sum[i-1] - pref_sum[i-maxPts - 1]) * 1/maxPts
-       
-        dp = defaultdict(int, {0: 1}) # starts at 0 so prob at 0 is 1
-        pref_sum = defaultdict(int, {0: 1}) # pref_sum of dp
+        
+        dp = defaultdict(int, {0: 1})
+        pref_sum = defaultdict(int, {0: 1})
 
         for i in range(1, n + 1):
-            largest = min(i - 1, k - 1)
-            smallest = i - maxPts - 1
-            prev_prob = pref_sum[largest] - (pref_sum[smallest] if smallest >= 0 else 0)
+            left = max(0, i - maxPts)
+            right = min(k - 1, i - 1)
 
-            prob_i = (prev_prob if prev_prob > 0 else 0) * 1/maxPts
-            
-            dp[i] = prob_i
-            pref_sum[i] = pref_sum[i - 1] + prob_i
+            dp[i] = (pref_sum[right] - pref_sum[left - 1]) * 1/maxPts
+            pref_sum[i] = pref_sum[i - 1] + dp[i]
 
-        
+        # print(dp)
+        # print(pref_sum)
+        # print()
+
         ans = 0
-        for i in range(k, n+2):
+        for i in range(k, n + 1):
             ans += dp[i]
 
         return ans
