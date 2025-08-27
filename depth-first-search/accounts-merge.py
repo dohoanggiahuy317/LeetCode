8a275f3cfd13ae910012d50ba952b0c1db2e5db7
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-27 11:35:44 -0400

    LC-accounts-merge [Runtime: 21 ms; Memory: 20.4 MB]

--- depth-first-search/accounts-merge.py
+++ depth-first-search/accounts-merge.py
@@ -1,6 +1,6 @@
-class Tree:
-    def __init__(self, num_node):
-        self.parents = [i for i in range(num_node)]
+class DSU:
+    def __init__(self, n):
+        self.parents = [i for i in range(n)]
     
     def find(self, x):
         if self.parents[x] != x:
@@ -9,27 +9,39 @@ class Tree:
 
     def union(self, x, y):
         xr, yr = self.find(x), self.find(y)
+        if xr == yr:
+            return
+
         self.parents[yr] = xr
+        return
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        tree = Tree(len(accounts))
-        
-        handled_emails = defaultdict(int)
-        for i, (name, *emails) in enumerate(accounts):
-            for email in emails:
-                if (name, email) in handled_emails:
-                    tree.union(i, handled_emails[(name, email)])
+        n = len(accounts)
+        graph = DSU(n)
+        acc2idx = defaultdict(int)
+        idx2name = defaultdict(str)
+
+        for i, acc in enumerate(accounts):
+            name = acc[0]
+            idx2name[i] = name
+            for mail in acc[1:]:
+                if mail not in acc2idx:
+                    acc2idx[mail] = i
                 else:
-                    handled_emails[(name, email)] = i
+                    j = acc2idx[mail]
+                    graph.union(i, j)
+
+        acc_group = defaultdict(list)
+        for mail, idx in acc2idx.items():
+            root = graph.find(idx)
+            acc_group[root].append(mail)
+
+        ans = []
+        for root, mails in acc_group.items():
+            name = idx2name[root]
+            person = [name] + sorted(mails)
+            ans.append(person)
         
-        ans = defaultdict(list)
-        for (name, email), parent in handled_emails.items():
-            root = tree.find(parent)
-            if root not in ans:
-                ans[root] = [[name], []]
-            ans[root][1].append(email)
-
-        return [x[0] + sorted(x[1]) for x in ans.values()]
-
-           
+        return ans
+        
