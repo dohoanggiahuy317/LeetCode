2f1faa8db8bd9ac3ceb17ad85bc365563f63468a
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-27 12:17:57 -0400

    LC-single-threaded-cpu [Runtime: 320 ms; Memory: 56.6 MB]

--- heap-priority-queue/single-threaded-cpu.py
+++ heap-priority-queue/single-threaded-cpu.py
@@ -1,16 +1,29 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        tasks = sorted((s, p, i) for i, (s, p) in enumerate(tasks))
-        n, i, ctime = len(tasks), 0, 0
-        pq, ans = [], []
 
-        while len(ans) < n:
-            if not pq and ctime < tasks[i][0]:
-                ctime = tasks[i][0]
-            while i < n and tasks[i][0] <= ctime:
-                s, p, idx = tasks[i]; i += 1
-                heapq.heappush(pq, (p, idx))
-            p, idx = heapq.heappop(pq)
-            ctime += p
+        # sort lại task
+        tasks = sorted([(start, proc, idx) for idx, (start, proc) in enumerate(tasks)])
+
+        ready_idx, n = 0, len(tasks)
+        ctime = 0
+        ready_queue, ans = [], []
+
+        while ready_queue or ready_idx < n:
+            if not ready_queue and ctime < tasks[ready_idx][0]:
+                ctime = tasks[ready_idx][0]
+
+            # add tất cả các thằng ready vào queue
+            while ready_idx < n and tasks[ready_idx][0] <= ctime:
+                start, proc, idx = tasks[ready_idx]
+                ready_idx += 1
+
+                heapq.heappush(ready_queue, (proc, idx))
+
+            proc, idx = heapq.heappop(ready_queue)
             ans.append(idx)
+            ctime += proc
+
         return ans
+
+
+        
