c907ecc2d31a887eaf63fdf7a09ced86bd915f21
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-27 12:06:41 -0400

    LC-single-threaded-cpu [Runtime: 332 ms; Memory: 57 MB]

--- array/single-threaded-cpu.py
+++ array/single-threaded-cpu.py
@@ -1,38 +1,16 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        tasks = sorted([s, p, idx] for idx, (s, p) in enumerate(tasks))
-        
-        s0, p0, idx0 = tasks.pop(0)
-        pq = [(p0, idx0, s0)] # -> (proc, idx, start)
-        
-        ans = []
-        # print(tasks)
-        # print(pq)
-        # print(ans)
-        # print()
+        tasks = sorted((s, p, i) for i, (s, p) in enumerate(tasks))
+        n, i, ctime = len(tasks), 0, 0
+        pq, ans = [], []
 
-
-        # keep add when pq or task have smth
-        while pq or tasks:
-
-            # lấy ra 1 thằng task
-            if pq:
-                p, idx, s = heapq.heappop(pq)
-                ctime = s + p
-                ans.append(idx)
-
-            # add all task mà start time < cur_time:
-            while tasks and tasks[0][0] <= ctime:
-                ns, np, nidx = tasks.pop(0)
-                heapq.heappush(pq, (np, nidx, ns))
-
-            if tasks and not pq:
-                ns, np, nidx = task.pop(0)
-                pq = [(np, nidx, ns)]
-
-            # print(tasks)
-            # print(pq)
-            # print(ans)
-            # print()
-
+        while len(ans) < n:
+            if not pq and ctime < tasks[i][0]:
+                ctime = tasks[i][0]
+            while i < n and tasks[i][0] <= ctime:
+                s, p, idx = tasks[i]; i += 1
+                heapq.heappush(pq, (p, idx))
+            p, idx = heapq.heappop(pq)
+            ctime += p
+            ans.append(idx)
         return ans
