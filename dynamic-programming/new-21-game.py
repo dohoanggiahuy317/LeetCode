0892bd6b826e9297830efba0cf0b2e0bec7ea3d1
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-28 10:50:43 -0400

    LC-new-21-game [Runtime: 155 ms; Memory: 19.4 MB]

--- dynamic-programming/new-21-game.py
+++ dynamic-programming/new-21-game.py
@@ -1,22 +1,13 @@
 class Solution:
     def new21Game(self, n: int, k: int, maxPts: int) -> float:
-        
-        dp = defaultdict(int, {0: 1})
+        prev_score = defaultdict(int, {0: 1})
         pref_sum = defaultdict(int, {0: 1})
 
         for i in range(1, n + 1):
             left = max(0, i - maxPts)
-            right = min(k - 1, i - 1)
+            right = min(i - 1, k - 1)
 
-            dp[i] = max(0, pref_sum[right] - pref_sum[left - 1]) * 1/maxPts
-            pref_sum[i] = pref_sum[i - 1] + dp[i]
-
-        # print(dp)
-        # print(pref_sum)
-        # print()
-
-        ans = 0
-        for i in range(k, n + 1):
-            ans += dp[i]
-
-        return ans
+            prev_score[i] = max(0, (pref_sum[right] - pref_sum[left - 1])) * 1/maxPts
+            pref_sum[i] = pref_sum[i-1] + prev_score[i]
+        
+        return sum([prev_score[i] for i in range(k, n+1)])
