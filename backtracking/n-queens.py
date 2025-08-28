e178d846187c692c0d61aa9016128057682b2747
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-28 16:04:11 -0400

    LC-n-queens [Runtime: 4 ms; Memory: 18.1 MB]

--- backtracking/n-queens.py
+++ backtracking/n-queens.py
@@ -1,32 +1,38 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        cols = [False] * n
-        diag1 = [False] * (2*n-1)
-        diag2 = [False] * (2*n-1)
-        ans = []
-        table = []
+        
 
-        def backtrack(k):
-            if k == n:
+        def backtrack(row_ith, table):
+            nonlocal n, cols, diags1, diags2
+
+            if row_ith == n:
                 ans.append(table[:])
                 return
 
             for pos in range(n):
-                if cols[pos] == True or diag1[k+pos] == True or diag2[n-1+pos-k]:
+                if cols[pos]:
+                    continue
+                if diags1[row_ith + pos]:
+                    continue
+                if diags2[n - 1 - row_ith + pos]:
                     continue
 
                 cols[pos] = True 
-                diag1[k+pos] = True
-                diag2[n-1+pos-k] = True
-                rows = "." * pos + "Q" + "." * (n-1-pos)
-                table.append(rows)
+                diags1[pos + row_ith] = True
+                diags2[n - 1 - row_ith + pos] = True
+                table.append("." * pos + "Q" + "." * (n - 1 - pos))
 
-                backtrack(k+1)
+                backtrack(row_ith + 1, table)
 
                 cols[pos] = False 
-                diag1[k+pos] = False
-                diag2[n-1+pos-k] = False
+                diags1[pos + row_ith] = False
+                diags2[n - 1 - row_ith + pos] = False
                 table.pop()
 
-        backtrack(0)
+
+        cols = [False] * n
+        diags1 = [False] * (2 * n - 1) # diag of / direction
+        diags2 = [False] * (2 * n - 1) # diag of \ direction
+        ans = []
+        backtrack(0, [])
         return ans
