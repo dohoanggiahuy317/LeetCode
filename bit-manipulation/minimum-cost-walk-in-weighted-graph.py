56684741027ae81b97066f6e61d1e85b180deed2
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-28 15:09:42 -0400

    LC-minimum-cost-walk-in-weighted-graph [Runtime: 191 ms; Memory: 93.4 MB]

--- bit-manipulation/minimum-cost-walk-in-weighted-graph.py
+++ bit-manipulation/minimum-cost-walk-in-weighted-graph.py
@@ -1,64 +1,41 @@
+class DSU:
+    def __init__(self, n):
+        self.parents = [i for i in range(n)]
+        self.total_cost = [(1 << 32) - 1 for _ in range(n)]
+
+    def find(self, x):
+        if self.parents[x] != x:
+            self.parents[x] = self.find(self.parents[x])
+
+        return self.parents[x]
+
+    def union(self, x, y, w):
+        xr, yr = self.find(x), self.find(y)
+        # if xr == yr:
+        #     return
+
+        if xr > yr:
+            xr, yr = yr, xr
+        
+        self.parents[yr] = xr
+        self.total_cost[xr] = self.total_cost[xr] & self.total_cost[yr] & w
+        self.total_cost[yr] = self.total_cost[xr]
+
+        return
+
 class Solution:
-    def minimumCost(self, n, edges, queries):
-        # Create the adjacency list of the graph
-        adj_list = [[] for _ in range(n)]
-        for edge in edges:
-            adj_list[edge[0]].append((edge[1], edge[2]))
-            adj_list[edge[1]].append((edge[0], edge[2]))
+    def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
+        graph = DSU(n)
 
-        visited = [False] * n
-
-        # Array to store the component ID of each node
-        components = [0] * n
-        component_cost = []
-
-        component_id = 0
-
-        # Perform DFS for each unvisited node to identify components and calculate their costs
-        for node in range(n):
-            if not visited[node]:
-                # Get the component cost and mark all nodes in the component
-                component_cost.append(
-                    self._get_component_cost(
-                        node, adj_list, visited, components, component_id
-                    )
-                )
-                component_id += 1
-
-        result = []
-        for query in queries:
-            start, end = query
-
-            if components[start] == components[end]:
-                # If they are in the same component, return the precomputed cost for the component
-                result.append(component_cost[components[start]])
+        for u, v, w in edges:
+            graph.union(u, v, w)
+        
+        ans = []
+        for x, y in query:
+            xr, yr = graph.find(x), graph.find(y)
+            if xr == yr:
+                ans.append(graph.total_cost[xr])
             else:
-                # If they are in different components, return -1
-                result.append(-1)
+                ans.append(-1)
 
-        return result
-
-    # Helper function to calculate the cost of a component using BFS
-    def _get_component_cost(
-        self, node, adj_list, visited, components, component_id
-    ):
-
-        # Initialize the cost to the number that has only 1s in its binary representation
-        current_cost = -1
-
-        # Mark the node as part of the current component
-        components[node] = component_id
-        visited[node] = True
-
-        # Explore all neighbors of the current node
-        for neighbor, weight in adj_list[node]:
-            # Update the component cost by performing a bitwise AND of the edge weights
-            current_cost &= weight
-            if not visited[neighbor]:
-                # Recursively calculate the cost of the rest of the component
-                # and accumulate it into currentCost
-                current_cost &= self._get_component_cost(
-                    neighbor, adj_list, visited, components, component_id
-                )
-
-        return current_cost
+        return ans
