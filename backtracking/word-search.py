bfb9006e64af8dbc9fc82c870c6340f4dbb77548
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-29 10:04:28 -0400

    LC-word-search [Runtime: N/A; Memory: N/A]

--- backtracking/word-search.py
+++ backtracking/word-search.py
@@ -1,36 +1,37 @@
+DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        def word_search(x, y, visited, curr_word):
+            nonlocal word, found, s, m, n
 
-        n = len(word)
-        visited = set()
+            if len(curr_word) == s:
+                found = curr_word == word
+                return
+            
+            for i, j in DIRECTIONS:
+                if found:
+                    return
 
-        def backtrack(i, j, ind):
+                if not (0 <= x + i < m and 0 <= y + j < n):
+                    continue
+                
+                if visited[x + i][y + j]:
+                    continue
+                
+                visited[x][y] = True  
+                word_search(x + i, y + j, visited, curr_word + board[x][y])
+                visited[x][y] = False
 
-            if ind >= n:
-                return True
+        m, n = len(board), len(board[0])
+        s = len(word)
+        found = False
 
-            if (
-                (i, j) in visited or
-                i < 0 or j < 0 or
-                i > len(board) - 1 or j > len(board[0]) -1 or
-                word[ind] != board[i][j]
-                ):
-                return False
-
-            visited.add((i, j))
-
-            go = (
-                backtrack(i-1, j, ind + 1) or
-                backtrack(i+1, j, ind + 1) or
-                backtrack(i, j-1, ind + 1) or
-                backtrack(i, j+1, ind + 1)
-            )
-            visited.remove((i, j))
-            return go
-
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if backtrack(i, j, 0) == True:
+        for x in range(m):
+            for y in range(n):
+                if found:
                     return True
+                word_search(x, y, [[False] * n for _ in range(m)], "")
 
-        return False
+        return found
