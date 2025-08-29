d193af67b3d729f5383e2650d7f95fcd991c46b7
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-29 09:37:55 -0400

    LC-generate-parentheses [Runtime: 2 ms; Memory: 18.1 MB]

--- dynamic-programming/generate-parentheses.py
+++ dynamic-programming/generate-parentheses.py
@@ -3,16 +3,14 @@ class Solution:
         
         def addParen(close_need, num_pair, curr_str):
             nonlocal ans
-            if num_pair == n:
-                if close_need > 0:
-                    addParen(close_need - 1, num_pair, curr_str + ")")
-                else:
-                    ans.append(curr_str)
-                    return
-            else:
-                if close_need > 0:
-                    addParen(close_need - 1, num_pair, curr_str + ")")
+            if num_pair == n and close_need == 0:
+                ans.append(curr_str)
+                return
+            
+            if not num_pair == n:
                 addParen(close_need + 1, num_pair + 1, curr_str + "(")
+            if close_need > 0:
+                addParen(close_need - 1, num_pair, curr_str + ")")
 
         ans = []
         addParen(0, 0, "")
