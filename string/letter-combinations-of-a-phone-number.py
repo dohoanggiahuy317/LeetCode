c36f68b9eeecd027b32b09a80b7770f1c8dab83a
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-29 20:31:31 -0400

    LC-letter-combinations-of-a-phone-number [Runtime: 0 ms; Memory: 17.8 MB]

--- string/letter-combinations-of-a-phone-number.py
+++ string/letter-combinations-of-a-phone-number.py
@@ -1,31 +1,32 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits) == 0:
-            return[]
-
-        d = {
-            '1': [],
-            '2': ['a', 'b', 'c'],
-            '3': ['d', 'e', 'f'],
-            '4': ['g', 'h', 'i'],
-            '5': ['j', 'k', 'l'],
-            '6': ['m', 'n', 'o'],
-            '7': ['p', 'q', 'r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', 'z'],
+        LETTERS_MAPPING = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
         }
+        n = len(digits)
+
+
+        def add_number(num_added, curr_str):
+            nonlocal ans, digits, n, LETTERS_MAPPING
+
+            if num_added == n:
+                ans.append(curr_str)
+                return
+
+            num = digits[num_added]
+            for letter in LETTERS_MAPPING[num]:
+                add_number(num_added + 1, curr_str + letter)
+        
+        if n == 0:
+            return []
 
         ans = []
-        def dfs(i, prevString):
-            if i >= len(digits):
-                ans.append(prevString)
-                return
-            
-            for x in d[ digits[i] ]:
-                dfs(i+1, prevString + x)
-
-        dfs(0, "")
+        add_number(0, "")
         return ans
-
-
-            
