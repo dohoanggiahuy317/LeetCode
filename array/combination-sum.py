f7c44571849676581158c8d9038ebad5ee68608e
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-29 20:11:27 -0400

    LC-combination-sum [Runtime: 507 ms; Memory: 17.9 MB]

--- array/combination-sum.py
+++ array/combination-sum.py
@@ -1,25 +1,21 @@
 class Solution:
-    sub_li = []
-    
-    def gen_sub(self, candidates, target, sub, k):
-        s = sum(sub)
-        if s > target:
-            if sum(sub[:-1]) == target and sub[:-1] not in self.sub_li:
-                self.sub_li.append(sub[:-1])
-            return
-        if k == len(candidates):
-            return
-
-        self.gen_sub(candidates, target, sub, k+1)
-
-        sub.append(candidates[k])
-        self.gen_sub(candidates, target, sub, k)
-
-        sub.pop()
-
-
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        self.sub_li = []
-        self.gen_sub(candidates, target, [], 0)
-        return self.sub_li
         
+        def get_new_num(curr_li, curr_sum):
+            nonlocal candidates, ans, n
+
+            if curr_sum == target:
+                ans.add(tuple(sorted(curr_li)))
+                return
+
+            if curr_sum > target:
+                return
+
+            for cand in candidates:
+                get_new_num(curr_li + [cand], curr_sum + cand)
+
+
+        n = len(candidates)
+        ans = set()
+        get_new_num([], 0)
+        return [list(x) for x in ans]
