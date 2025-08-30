9c708c12a520607aa72aa684e2ef7868506c810c
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-29 20:16:54 -0400

    LC-combination-sum [Runtime: 9 ms; Memory: 18.1 MB]

--- array/combination-sum.py
+++ array/combination-sum.py
@@ -1,21 +1,22 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
-        def get_new_num(curr_li, curr_sum):
+        def get_new_num(i, curr_li, curr_sum):
             nonlocal candidates, ans, n
 
             if curr_sum == target:
-                ans.add(tuple(sorted(curr_li)))
+                ans.append(curr_li[:])
                 return
 
             if curr_sum > target:
                 return
 
-            for cand in candidates:
-                get_new_num(curr_li + [cand], curr_sum + cand)
+            for j in range(i, len(candidates)):
+                cand = candidates[j]
+                get_new_num(j, curr_li + [cand], curr_sum + cand)
 
 
         n = len(candidates)
-        ans = set()
-        get_new_num([], 0)
-        return [list(x) for x in ans]
+        ans = []
+        get_new_num(0, [], 0)
+        return ans
