2912c7d2382d88d30910c49c482ec1fe1d197b77
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-31 13:02:53 -0400

    LC-course-schedule-ii [Runtime: N/A; Memory: N/A]

--- depth-first-search/course-schedule-ii.py
+++ depth-first-search/course-schedule-ii.py
@@ -1,44 +1,23 @@
-from collections import defaultdict
-
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        indegree = [0] * numCourses
+        graph = defaultdict(list)
+
+        for dst, src in prerequisites:
+            indegree[dst] += 1
+            graph[src].append(dst)
         
-        m = defaultdict(list)
-        for a, b in prerequisites:
-            m[b].append(a)
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        ans = []
 
-        def cyc(i):
-            nonlocal rec_visit
-            rec_visit.add(i)
-            b = True
-            for ne in m[i]:
-                if ne in rec_visit:
-                    return False
-                b = b and cyc(ne)
-                
-            rec_visit.remove(i)
-            return b
+        while queue:   
+            node = queue.popleft()
+            ans.append(node)
 
-        for i in range(numCourses):
-            rec_visit = set()
-            if cyc(i) == False:
-                return []
+            for neigh in graph[node]:
+                indegree[neigh] -= 1
 
-        
-        def dfs(i):
-            nonlocal visited, stack, m, b
-            visited.add(i)
+                if indegree[neigh] == 0:
+                    queue.append(neigh)
 
-            for ne in m[i]:
-                if ne not in visited:
-                    dfs(ne)
-
-            stack.append(i)
-
-        visited = set()
-        stack = []
-        for i in range(numCourses):
-            if i not in visited:
-                dfs(i)
-
-        return stack[::-1]
+        return ans
