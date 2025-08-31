c29db327a15219ff37258a09cc21b9654fac4764
Jeremy <giahuy.jeremy.do@gmail.com>
2025-08-31 11:45:29 -0400

    LC-generate-parentheses [Runtime: N/A; Memory: N/A]

--- dynamic-programming/generate-parentheses.py
+++ dynamic-programming/generate-parentheses.py
@@ -1,17 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
-        def addParen(close_need, num_pair, curr_str):
-            nonlocal ans
+        def addParen(close_need, num_pair):
+            nonlocal ans, candidate
             if num_pair == n and close_need == 0:
-                ans.append(curr_str)
+                ans.append("".join(candidate))
                 return
             
             if not num_pair == n:
-                addParen(close_need + 1, num_pair + 1, curr_str + "(")
+                candidate.append("(")
+                addParen(close_need + 1, num_pair + 1)
+                candidate.pop()
             if close_need > 0:
-                addParen(close_need - 1, num_pair, curr_str + ")")
+                candidate.append(")")
+                addParen(close_need - 1, num_pair)
 
         ans = []
-        addParen(0, 0, "")
+        candidate = []
+        addParen(0, 0)
         return ans
