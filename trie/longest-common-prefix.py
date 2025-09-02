e5750e01cf7840291c7ee45bbf74c96293343a4a
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-02 10:42:17 -0400

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- trie/longest-common-prefix.py
+++ trie/longest-common-prefix.py
@@ -1,14 +1,29 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-            m = min(strs)
-            M = max(strs)
+        
+        def add_node(word, first):
+            nonlocal trie, all_prefix_len
 
-            ans = ""
+            node = trie
+            prefix_count, prefix_bool = 0, True
 
-            for i in range(len(m)):
-                if i < len(M) and m[i] == M[i]:
-                    ans += m[i]
-                else:
-                    break
+            for ch in word:
+                if ch not in node:
+                    node[ch] = {}
+                    prefix_bool = False
+                elif prefix_bool and not first:
+                    prefix_count += 1
+                node = node[ch]
 
-            return ans
+            if not first:
+                all_prefix_len = min(all_prefix_len, prefix_count)
+            
+        
+        trie = defaultdict()
+        all_prefix_len = inf
+        first = True
+        for s in strs:
+            add_node(s, first)
+            first = False
+
+        return strs[0][:all_prefix_len]
