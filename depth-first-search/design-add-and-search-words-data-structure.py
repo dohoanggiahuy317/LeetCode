6650d324d510f634c425b68b1690bf7ad5565411
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-03 22:28:29 -0400

    LC-design-add-and-search-words-data-structure [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/design-add-and-search-words-data-structure.py
@@ -0,0 +1,48 @@
+class TrieNode:
+    def __init__(self, ch):
+        self.ch = ch
+        self.children = {}
+        self.exist = True
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode("^")
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode(ch)
+            node = node.children[ch]
+
+        node.exist = True
+
+    def search(self, word: str) -> bool:
+        queue = deque([self.root])
+
+        for i, ch in enumerate(word):
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+
+                # If last char match a word
+                if i == len(word) - 1 and node.exist:
+                    return True
+                    
+                if ch != "." and ch not in node.children:
+                    continue
+
+                if ch != "." and ch in node.children:
+                    queue.append(node.children[ch])
+
+                if ch == ".":
+                    queue.extend(list(node.children.values()))
+
+        return False
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
