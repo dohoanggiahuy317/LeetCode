a425c40a14a0b22e23edf607b584d3bb2fd33dd3
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-05 02:41:58 -0400

    LC-find-all-possible-recipes-from-given-supplies [Runtime: N/A; Memory: N/A]

---
+++ hash-table/find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        graph = defaultdict(list)
+        indegree = defaultdict(int)
+        for i, (recipe, ingres) in enumerate(zip(recipes, ingredients)):
+            for ingre in ingres:
+                graph[ingre].append(recipe)
+                indegree[ingre] = 0 if ingre not in indegree else indegree[ingre]
+            indegree[recipe] += 1
+
+        queue = deque([ingre for ingre in indegree.keys() if indegree[ingre] == 0])
+        
+        while queue:
+            ingre = queue.popleft()
+
+            for recipe in graph[ingre]:
+                indegree[recipe] -= 1
+                if indegree[recipe] == 0:
+                    queue.append(recipe)
+
+        print(graph)
+        print(indegree)
+
+        return [recipe for recipe in recipes if indegree[recipe] == 0]
