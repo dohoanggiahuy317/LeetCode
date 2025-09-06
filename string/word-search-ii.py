ce903d418cbb7bb45a83c24bd9ee5dea5277120f
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-06 03:05:57 -0400

    LC-word-search-ii [Runtime: N/A; Memory: N/A]

--- string/word-search-ii.py
+++ string/word-search-ii.py
@@ -4,70 +4,69 @@ class TrieNode:
     def __init__(self, ch):
         self.ch = ch
         self.children = {}
+        self.exist = False
 
 class Trie:
     def __init__(self):
         self.root = TrieNode("^")
 
-    def insert_board(self, board):
-        m, n = len(board), len(board[0])
-        
-        for x in range(m):
-            for y in range(n):
-                visited = [[False] * n for _ in range(m)]
-                self.dfs_word(board, visited, self.root, x, y, 0)
-
-    def dfs_word(self, board, visited, node, x, y, step):
-
-        if step == 10:
-            return
-        
-        m, n = len(board), len(board[0])
-        ch = board[x][y]
-
-        if ch not in node.children:
-            node.children[ch] = TrieNode(ch)
-            
-        visited[x][y] = True
-
-        for dx, dy in DIRECTIONS:
-            nx, ny = x + dx, y + dy
-
-            if not (0 <= nx < m and 0 <= ny < n):
-                continue
-
-            if visited[nx][ny]:
-                continue
-
-            self.dfs_word(board, visited, node.children[ch], nx, ny, step + 1)
-        
-        visited[x][y] = False
-
-
-    def search(self, word):
+    def insert(self, word):
         node = self.root
-
         for ch in word:
             if ch not in node.children:
-                return False
-
+                node.children[ch] = TrieNode(ch)
             node = node.children[ch]
-
-        return True
+        node.exist = True
             
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         trie = Trie()
-        trie.insert_board(board)
-
-        ans = []
-
         for word in words:
-            if trie.search(word):
-                ans.append(word)
+            trie.insert(word)
 
-        return ans
+        def dfs(x, y, node, step):
+            nonlocal visited, curr_word, ans, m, n
+
+            
+            visited[x][y] = True
+            ch = board[x][y]
+            curr_word.append(ch)
+
+            if node.exist:
+                ans.add("".join(curr_word))
+            
+            if step == 10:
+                return
+
+            for dx, dy in DIRECTIONS:
+                nx, ny = x + dx, y + dy
+
+                if not (0 <= nx < m and 0 <= ny < n):
+                    continue
+
+                next_ch = board[nx][ny]
+                if next_ch not in node.children:
+                    continue
+
+                dfs(nx, ny, node.children[next_ch], step + 1)
+  
+
+            visited[x][y] = False
+            curr_word.pop()
+
+        m, n = len(board), len(board[0])
+        ans = set()
+        curr_word = []
+        visited = [[False] * n for _ in range(m)]
+        
+        for x in range(m):
+            for y in range(n):
+                if board[x][y] in trie.root.children:
+                    char = board[x][y]
+                    dfs(x, y, trie.root.children[char], 0)
+
+        return list(ans)
 
 
 
