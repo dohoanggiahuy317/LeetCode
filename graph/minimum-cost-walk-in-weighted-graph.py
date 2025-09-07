0bab5612f68d1ae25e61daba970f5ef27173a93d
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-06 22:54:36 -0400

    LC-minimum-cost-walk-in-weighted-graph [Runtime: N/A; Memory: N/A]

--- graph/minimum-cost-walk-in-weighted-graph.py
+++ graph/minimum-cost-walk-in-weighted-graph.py
@@ -6,35 +6,30 @@ class DSU:
     def find(self, x):
         if self.parents[x] != x:
             self.parents[x] = self.find(self.parents[x])
-
         return self.parents[x]
 
     def union(self, x, y, w):
         xr, yr = self.find(x), self.find(y)
-        # if xr == yr:
-        #     return
 
-        if xr > yr:
-            xr, yr = yr, xr
-        
         self.parents[yr] = xr
-        self.total_cost[xr] = self.total_cost[xr] & self.total_cost[yr] & w
+
+        self.total_cost[xr] = self.total_cost[yr] & w
         self.total_cost[yr] = self.total_cost[xr]
 
-        return
+
 
 class Solution:
     def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
+        
         graph = DSU(n)
 
         for u, v, w in edges:
             graph.union(u, v, w)
-        
+
         ans = []
-        for x, y in query:
-            xr, yr = graph.find(x), graph.find(y)
-            if xr == yr:
-                ans.append(graph.total_cost[xr])
+        for u, v in query:
+            if graph.find(u) == graph.find(v):
+                ans.append(graph.total_cost[graph.find(u)])
             else:
                 ans.append(-1)
 
