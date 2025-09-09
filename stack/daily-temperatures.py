ab933391703cad89c97e60f4484b112dd1a66e5d
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-08 20:58:29 -0400

    LC-daily-temperatures [Runtime: 91 ms; Memory: 27 MB]

--- stack/daily-temperatures.py
+++ stack/daily-temperatures.py
@@ -1,25 +1,19 @@
-from collections import defaultdict
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        n = len(temperatures)
+        stack = []
+        ans = [0] * n
 
-        d = defaultdict(list)
-        for i in range(len(temperatures)):
-            d[temperatures[i]].append(i)
-
-        ans = [0] * len(temperatures)
-
-        for i in range(len(temperatures)):
-            today = temperatures[i]
-            temp = float("INF")
-            for next_temp in range(today + 1, 101):
-                while d[next_temp] and d[next_temp][0] < i:
-                    d[next_temp].pop(0)
-                if d[next_temp]:
-                    temp = min(temp, d[next_temp][0] - i)
-                    # print(d[next_temp])
-
-            ans[i] = temp if temp != float("INF") else 0
-
+        for i, t in enumerate(temperatures):
             
+            while stack and temperatures[stack[-1]] < t:
+                ans[stack[-1]] = i - stack[-1]
+                stack.pop()
+            
+            stack.append(i)
 
         return ans
+                
+                
+            
