6fc68ab76005ee1b0dc0520cea7af8acb1a053df
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-09 17:26:44 -0400

    LC-task-scheduler [Runtime: 131 ms; Memory: 19.3 MB]

--- sorting/task-scheduler.py
+++ sorting/task-scheduler.py
@@ -3,17 +3,25 @@ class Solution:
         
         curr_time = 0
         tasks_count = Counter(tasks)
-        queue = [(0, -freq, task) for task, freq in tasks_count.items()]
+        pending = [] # -> time, -freq, task
+        ready = [] # -freq, task
+        for task, freq in tasks_count.items():
+            heapq.heappush(ready, (-freq, task))
 
-        while queue:
-            start_time, neg_freq, task = heapq.heappop(queue)
-            if start_time > curr_time:
-                curr_time = start_time
-
-            new_time = curr_time + n + 1
+        while ready or pending:
             
+            while pending and pending[0][0] <= curr_time:
+                _, neg_freq, task = heapq.heappop(pending)
+                heapq.heappush(ready, (neg_freq, task))
+            
+            if pending and not ready:
+                curr_time, neg_freq, task = heapq.heappop(pending)
+                heapq.heappush(ready, (neg_freq, task))
+            
+            neg_freq, task = heapq.heappop(ready)
+            new_time = curr_time + n + 1
             if neg_freq + 1 != 0:
-                heapq.heappush(queue, (new_time, neg_freq + 1, task))
+                heapq.heappush(pending, (new_time, neg_freq + 1, task))
             
             curr_time += 1
 
