893774af06b3c4e07711556e37f646cacbc1790c
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-09 22:47:14 -0400

    LC-largest-rectangle-in-histogram [Runtime: 262 ms; Memory: 30.4 MB]

--- array/largest-rectangle-in-histogram.py
+++ array/largest-rectangle-in-histogram.py
@@ -3,25 +3,29 @@ class Solution:
         n = len(heights)
 
         prefix_area = [0] * n
-        idx_left_stack = [-1] # stack of idx that only non-decreasing
+        idx_left_stack = [-1] # stack of idx that only decreasing
 
         for i, h in enumerate(heights):
             # pop all for non_decrease
-            while len(idx_left_stack) > 1 and heights[idx_left_stack[-1]] > h:
+            while len(idx_left_stack) > 1 and heights[idx_left_stack[-1]] >= h:
                 idx_left_stack.pop()
             
             # compute the prefix_area
             prefix_area[i] = h * (i - idx_left_stack[-1])
             idx_left_stack.append(i)
 
+        # print(prefix_area)
+            
+        
         ans = 0
         idx_right_stack = [n]
         for i in range(n - 1, -1, -1):
             h = heights[i]
-            while len(idx_right_stack) > 1 and heights[idx_right_stack[-1]] > h:
+            while len(idx_right_stack) > 1 and heights[idx_right_stack[-1]] >= h:
                 idx_right_stack.pop()
 
             suffix_area = h * (idx_right_stack[-1] - i)
+            # print(suffix_area)
             ans = max(suffix_area + prefix_area[i] - h, ans)
             idx_right_stack.append(i)
 
