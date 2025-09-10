6bf5d587198ae234431ae56eba020817746b8455
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-09 22:50:07 -0400

    LC-largest-rectangle-in-histogram [Runtime: 262 ms; Memory: 30.4 MB]

--- monotonic-stack/largest-rectangle-in-histogram.py
+++ monotonic-stack/largest-rectangle-in-histogram.py
@@ -3,31 +3,31 @@ class Solution:
         n = len(heights)
 
         prefix_area = [0] * n
-        idx_left_stack = [-1] # stack of idx that only decreasing
+        left_stack = [-1] # stack of idx that only decreasing
 
         for i, h in enumerate(heights):
             # pop all for non_decrease
-            while len(idx_left_stack) > 1 and heights[idx_left_stack[-1]] >= h:
-                idx_left_stack.pop()
+            while len(left_stack) > 1 and heights[left_stack[-1]] >= h:
+                left_stack.pop()
             
             # compute the prefix_area
-            prefix_area[i] = h * (i - idx_left_stack[-1])
-            idx_left_stack.append(i)
+            prefix_area[i] = h * (i - left_stack[-1])
+            left_stack.append(i)
 
-        # print(prefix_area)
-            
-        
+        # Calculate suffix area        
         ans = 0
-        idx_right_stack = [n]
+        right_stack = [n]
         for i in range(n - 1, -1, -1):
             h = heights[i]
-            while len(idx_right_stack) > 1 and heights[idx_right_stack[-1]] >= h:
-                idx_right_stack.pop()
 
-            suffix_area = h * (idx_right_stack[-1] - i)
-            # print(suffix_area)
+            while len(right_stack) > 1 and heights[right_stack[-1]] >= h:
+                right_stack.pop()
+
+            # use prefix and suffix to get ans
+            suffix_area = h * (right_stack[-1] - i)
             ans = max(suffix_area + prefix_area[i] - h, ans)
-            idx_right_stack.append(i)
+            
+            right_stack.append(i)
 
         return ans
         
