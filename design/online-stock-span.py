2ef9ac0aa25882786e856ef8ed8398b825ad7c0f
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-11 10:05:35 -0400

    LC-online-stock-span [Runtime: 55 ms; Memory: 22.9 MB]

--- design/online-stock-span.py
+++ design/online-stock-span.py
@@ -1,18 +1,16 @@
 class StockSpanner:
 
     def __init__(self):
-        self.stock = []
-        self.stack = []
+        self.idx_stock = []
         self.day = 0
 
     def next(self, price: int) -> int:
         
-        while self.stack and self.stock[self.stack[-1]] <= price:
-            self.stack.pop()
+        while self.idx_stock and self.idx_stock[-1][1] <= price:
+            self.idx_stock.pop()
 
-        ans = self.day - self.stack[-1] if self.stack else self.day + 1
-        self.stack.append(self.day)
-        self.stock.append(price)
+        ans = self.day - self.idx_stock[-1][0] if self.idx_stock else self.day + 1
+        self.idx_stock.append((self.day, price))
         self.day += 1
         
         return ans
