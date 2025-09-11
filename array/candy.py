3c88f40ab5574a4fde8e9591817b14295e11b6c6
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-11 01:15:48 -0400

    LC-candy [Runtime: N/A; Memory: N/A]

--- array/candy.py
+++ array/candy.py
@@ -1,13 +1,19 @@
-class Solution(object):
-    def candy(self, ratings):
-        n = len(ratings)
-        cnt = 0
-        candies = [1] * n
-        for i in range(1, n):
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        
+        flow_tracker = [0]
+        curr_min = 0
+        total = 0
+
+        for i in range(1, len(ratings)):
             if ratings[i] > ratings[i - 1]:
-                candies[i] = candies[i - 1] + 1
-        for i in range(n - 1, 0, -1):
-            if ratings[i - 1] > ratings[i]:
-                candies[i - 1] = max(candies[i] + 1, candies[i - 1])
-            cnt += candies[i - 1]
-        return cnt + candies[n - 1]
+                flow_tracker.append(flow_tracker[-1] + 1)
+            else:
+                flow_tracker.append(flow_tracker[-1] - 1)
+                curr_min = min(curr_min, flow_tracker[-1])
+            total += flow_tracker[-1]
+
+        # print(total, curr_min, flow_tracker)
+
+        ans = max(0, (1 - curr_min)) * len(ratings) + total
+        return ans
