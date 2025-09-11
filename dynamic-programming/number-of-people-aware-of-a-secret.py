886d8951734cd047d7a5391b0e051fa7bad38bb1
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-11 12:44:52 -0400

    LC-number-of-people-aware-of-a-secret [Runtime: 135 ms; Memory: 17.9 MB]

--- dynamic-programming/number-of-people-aware-of-a-secret.py
+++ dynamic-programming/number-of-people-aware-of-a-secret.py
@@ -7,15 +7,24 @@ class Solution:
         # 5 1/ 3 4 5
 
         MOD = 10 ** 9 + 7
-        queue = [1]
+        queue = [(1, 1)]
 
         for day in range(1, n + 1):
-            while queue and queue[0] + forget <= day:
+            while queue and queue[0][0] + forget <= day:
                 queue.pop(0)
 
-            for person in queue:
-                if person + delay <= day:
-                    queue.append(day)
+            new_people = 0
+            for know_day, people in queue:
+                if know_day + delay <= day:
+                    new_people += people
+                else:
+                    break
+            if new_people > 0:
+                queue.append((day, new_people))
+        
+        ans = 0
+        for _, p in queue:
+            ans = (ans + p) % MOD
 
-        return len(queue) % MOD
+        return ans
 
