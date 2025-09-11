d9bed94bc442d4362a9890dd27ec2f9e168f9774
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-11 09:41:55 -0400

    LC-online-stock-span [Runtime: N/A; Memory: N/A]

---
+++ design/online-stock-span.py
@@ -0,0 +1,22 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stock = []
+        self.stack = []
+        self.day = 0
+
+    def next(self, price: int) -> int:        
+        while self.stack and self.stock[self.stack[-1]] <= price:
+            self.stack.pop()
+
+        ans = self.day - self.stack[-1] if self.stack else 1
+        self.stack.append(self.day)
+        self.stock.append(price)
+        self.day += 1
+        
+        return ans
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
