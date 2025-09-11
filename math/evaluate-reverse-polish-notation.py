7c30c7fc4a6324b08f55bfc3650e3644aff1e326
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-11 10:43:23 -0400

    LC-evaluate-reverse-polish-notation [Runtime: 1 ms; Memory: 19.3 MB]

--- math/evaluate-reverse-polish-notation.py
+++ math/evaluate-reverse-polish-notation.py
@@ -3,24 +3,26 @@ class Solution:
         stack = []
 
         for token in tokens:
-            if token[0] == '-' and len(token) > 1:
-                stack.append(-int(token[1:]))
-            elif token[0].isnumeric():
-                stack.append(int(token))
-            else:
+            if token == "+":
                 num2 = stack.pop()
                 num1 = stack.pop()
-
-                if token == "+":
-                    val = num1 + num2
-                elif token == "-":
-                    val = num1 - num2
-                elif token == "*":
-                    val = num1 * num2
-                else:
-                    val = num1 / num2
-                    val = int(val) if val >= 0 else math.ceil(val)
-
-                stack.append(val)
+                val = num1 + num2
+            elif token == "-":
+                num2 = stack.pop()
+                num1 = stack.pop()
+                val = num1 - num2
+            elif token == "*":
+                num2 = stack.pop()
+                num1 = stack.pop()
+                val = num1 * num2
+            elif token == "/":
+                num2 = stack.pop()
+                num1 = stack.pop()
+                val = num1 / num2
+                val = int(val) if val >= 0 else math.ceil(val)
+            else:
+                val = int(token)
+            
+            stack.append(val)
 
         return stack[0]
