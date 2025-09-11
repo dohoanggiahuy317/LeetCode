09c1749e513742524a6ee0c1f7ab9ebfb0edc0fb
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-11 12:47:04 -0400

    LC-number-of-people-aware-of-a-secret [Runtime: 139 ms; Memory: 18 MB]

--- dynamic-programming/number-of-people-aware-of-a-secret.py
+++ dynamic-programming/number-of-people-aware-of-a-secret.py
@@ -1,18 +1,16 @@
 class Solution:
     def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
-        # 1 1
-        # 2 1
-        # 3 1 3
-        # 4 1 3 4
-        # 5 1/ 3 4 5
 
         MOD = 10 ** 9 + 7
         queue = [(1, 1)]
 
+        # Go through each day
         for day in range(1, n + 1):
+            # People forget
             while queue and queue[0][0] + forget <= day:
                 queue.pop(0)
 
+            # new people heard story
             new_people = 0
             for know_day, people in queue:
                 if know_day + delay <= day:
@@ -22,6 +20,7 @@ class Solution:
             if new_people > 0:
                 queue.append((day, new_people))
         
+        # answer
         ans = 0
         for _, p in queue:
             ans = (ans + p) % MOD
