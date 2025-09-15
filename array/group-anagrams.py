7bae88539b49fb9a1aa4dca7e91f57d4bf788d02
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-15 01:43:49 -0400

    LC-group-anagrams [Runtime: N/A; Memory: N/A]

--- array/group-anagrams.py
+++ array/group-anagrams.py
@@ -1,11 +1,8 @@
-from collections import defaultdict
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = defaultdict(list)
 
         for s in strs:
-            k = tuple(sorted(list(s)))
-            d[k].append(s)
+            d[Counter(s)].append(s)
 
-        return list(d.values())
+        return d.values()
