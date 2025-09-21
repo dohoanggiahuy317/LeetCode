c0994927675a473cb86f9272967c1fe21be66109
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-21 14:32:26 -0400

    LC-minimum-number-of-steps-to-make-two-strings-anagram [Runtime: 63 ms; Memory: 17.9 MB]

--- counting/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ counting/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,9 +1,10 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        sc, tc = Counter(s), Counter(t)
-
+        s_dict, t_dict = Counter(s), Counter(t)
         ans = 0
-        for char in sc.keys():
-            ans += (sc[char] - tc[char]) if sc[char] > tc[char] else 0
+
+        for char, freq in s_dict.items():
+            if freq > t_dict[char]:
+                ans += freq - t_dict[char]
 
         return ans
