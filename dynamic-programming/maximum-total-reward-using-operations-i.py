44e4d1ae9f67e332988ee13e0d9f16d4b789477f
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-20 22:07:25 -0400

    LC-maximum-total-reward-using-operations-i [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/maximum-total-reward-using-operations-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxTotalReward(self, rewardValues: List[int]) -> int:
+        
+        n = len(rewardValues)
+        rewardValues.sort()
+        dp = [rewardValues[0]] * n
+
+        for i, reward in enumerate(rewardValues):
+            local_max = 0
+
+            for j in range(i):
+                if reward > dp[j]:
+                    local_max = max(local_max, dp[j] + reward)
+
+            dp[i] = local_max
+
+        return dp[-1]
+
+
+
+        
