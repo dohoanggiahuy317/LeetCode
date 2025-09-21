00c312d56a8ba59593e8b7ec5273fc9f2c844201
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-20 21:54:05 -0400

    LC-first-bad-version [Runtime: N/A; Memory: N/A]

--- binary-search/first-bad-version.py
+++ binary-search/first-bad-version.py
@@ -3,15 +3,15 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        l, r = 1, n
-        ans = 0
+        l, r = 0, n
+        ans = -1
 
         while l <= r:
             m = (l + r) // 2
             if isBadVersion(m):
-                ans = m
-                r = m - 1
+                ans = r
+                r -= 1
             else:
-                l = m + 1
-        
+                l += 1
+
         return ans
