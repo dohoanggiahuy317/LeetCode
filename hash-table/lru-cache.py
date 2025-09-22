ed921ead4e65a4adb531411e9c2c7707999b517f
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-21 23:32:33 -0400

    LC-lru-cache [Runtime: N/A; Memory: N/A]

--- hash-table/lru-cache.py
+++ hash-table/lru-cache.py
@@ -1,70 +1,90 @@
 class Node:
-    def __init__(self, value, key):
-        self.prev = self.next = None
-        self.val = value
+    def __init__(self, key, val):
         self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.head = self.tail = None
-        self.capacity = capacity
-        self.length = 0
-        self.MAP = defaultdict(lambda: None)
+        self.cap = capacity
+        self.size = 0
+        self.key2val = {}
+        self.cache_head, self.cache_tail = None, None
 
-    def add_node(self, node):
-        if not self.head:
-            self.head = node
-            self.tail = node
-        else:
-            node.next = self.head
-            self.head.prev = node
-            self.head = node
-    
-    def remove_node(self, node):
+    def pop_out(self, node):
         prev_node = node.prev
         next_node = node.next
 
+        # handle prev_node
         if prev_node:
             prev_node.next = next_node
+        else:
+            self.cache_head = next_node
+
+        # handle next_node
         if next_node:
             next_node.prev = prev_node
-        node.next = node.prev = None
+        else:
+            self.cache_tail = prev_node
 
-        if node == self.head:
-            self.head = next_node
-        if node == self.tail:
-            self.tail = prev_node
+        # Handle node
+        node.prev = None
+        node.next = None
+
+    def update_cache_head(self, node):
+        node.next = self.cache_head
+        if self.cache_head:
+            self.cache_head.prev = node
+        
+        self.cache_head = node
+
+        if not self.cache_tail:
+            self.cache_tail = node
+
+    def update_cache_tail(self):
+        if not self.cache_tail:
+            return
+
+        # Detach the tail
+        tail = self.cache_tail
+        self.cache_tail = tail.prev
+        if self.cache_tail:
+            self.cache_tail.next = None
+        tail.prev = None
+
+        # del the key
+        key = tail.key
+        del self.key2val[key]
 
     def get(self, key: int) -> int:
-        node = self.MAP[key]
-        if node:
-            self.remove_node(node)
-            self.add_node(node)
-            return node.val
-            
-        return -1
-        
-    def put(self, key: int, value: int) -> None:
-        node = self.MAP[key]
-        if node:
-            node.val = value
-            self.get(key)
-            return
-        
-        if self.length >= self.capacity:
-            last_node = self.tail
-            self.remove_node(last_node)
-            del self.MAP[last_node.key]
-            self.length -= 1
-        
-        new_node = Node(value, key)
-        self.add_node(new_node)
-        self.MAP[key] = new_node
-        self.length += 1
+        if key not in self.key2val:
+            return -1
+        # get node
+        node = self.key2val[key]
+        self.pop_out(node)
+        self.update_cache_head(node)
+        return node.val
 
-            
+    def put(self, key: int, value: int) -> None:
         
+        if key in self.key2val:
+            node = self.key2val[key]
+            self.pop_out(node)
+            node.val = value
+        else:
+            node = Node(key, value)
+            self.key2val[key] = node
+            if self.size == self.cap:
+                self.update_cache_tail()
+                self.size -= 1
+        
+        self.update_cache_head(node)
+        self.size += 1
+
+
+
 
 
 # Your LRUCache object will be instantiated and called as such:
