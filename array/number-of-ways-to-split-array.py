5c10497a5e45db21de5f680baef6394194982a67
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-21 22:46:09 -0400

    LC-number-of-ways-to-split-array [Runtime: 168 ms; Memory: 37.7 MB]

--- array/number-of-ways-to-split-array.py
+++ array/number-of-ways-to-split-array.py
@@ -1,13 +1,18 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        l = nums[0]
-        r = sum(nums) - l
-        ans = 1 if l >= r else 0
+        n = len(nums)
+        prefix_sum = [nums[0]] * n
+        suffix_sum = [nums[-1]] * n
 
-        for i in range(1, len(nums)-1):
-            l += nums[i]
-            r -= nums[i]
+        for i in range(1, n):
+            prefix_sum[i] = prefix_sum[i - 1] + nums[i]
+        
+        for i in range(n-2, -1, -1):
+            suffix_sum[i] = suffix_sum[i + 1] + nums[i]
 
-            ans += 1 if l >=r else 0
-
+        ans = 0
+        for i in range(n-1):
+            if prefix_sum[i] >= suffix_sum[i + 1]:
+                ans += 1
+                
         return ans
