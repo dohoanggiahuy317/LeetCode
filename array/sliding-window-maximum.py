2a9da3eead8b32b25e06c9062a6afa742fcf2cd7
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-22 09:13:57 -0400

    LC-sliding-window-maximum [Runtime: N/A; Memory: N/A]

--- array/sliding-window-maximum.py
+++ array/sliding-window-maximum.py
@@ -10,7 +10,7 @@ class Solution:
         ans.append(nums[queue[0]])
 
         for i in range(k, len(nums)):
-            while queue and queue[0] < i - k:
+            while queue and queue[0] <= i - k:
                 queue.popleft()
             while queue and queue[-1] <= nums[i]:
                 queue.pop()
