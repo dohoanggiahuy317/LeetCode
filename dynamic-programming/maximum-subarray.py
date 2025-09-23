07f3c370157c7a4193ed2f44fe39c4d3ef077c7f
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-22 22:09:42 -0400

    LC-maximum-subarray [Runtime: 180 ms; Memory: 32.3 MB]

--- dynamic-programming/maximum-subarray.py
+++ dynamic-programming/maximum-subarray.py
@@ -1,18 +1,18 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         prefix_sum = list(accumulate(nums))
-        prefix_max = [nums[0]] * len(nums)
+        # prefix_max = [nums[0]] * len(nums)
         prefix_min = [nums[0]] * len(nums)
 
         for i in range(1, len(nums)):
-            prefix_max[i] = max(prefix_max[i - 1], prefix_sum[i])
+            # prefix_max[i] = max(prefix_max[i - 1], prefix_sum[i])
             prefix_min[i] = min(prefix_min[i - 1], prefix_sum[i])
         # print(prefix_sum)
         # print(prefix_max)
         # print(prefix_min)
         ans = -inf
         for i in range(len(nums)):
-            ans = max(ans, prefix_max[i])
-            ans = max(ans, prefix_max[i] - (prefix_min[i - 1] if i > 0 else 0))
+            ans = max(ans, prefix_sum[i])
+            ans = max(ans, prefix_sum[i] - (prefix_min[i - 1] if i > 0 else 0))
 
         return ans
