a5bbeb0e9a609b30a1eee0f3ae876d0be57bc23f
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-24 20:18:41 -0400

    LC-stock-price-fluctuation [Runtime: N/A; Memory: N/A]

--- hash-table/stock-price-fluctuation.py
+++ hash-table/stock-price-fluctuation.py
@@ -1,39 +1,70 @@
+class SegmentTree:
+    def __init__(self, nums, func, given):
+        self.N = len(nums)
+        self.func = func
+        self.given = given
+
+        self.tree = [given] * (self.N * 2)
+        self.tree[self.N:] = nums
+        
+        for i in range(self.N - 1, 0, -1):
+            self.tree[i] = self.func(self.tree[i << 1], self.tree[(i << 1) | 1])
+    
+    def update(self, i, val):
+        i += self.N
+        self.tree[i] = val
+        while i > 1:
+            self.tree[i >> 1] = self.func(self.tree[i], self.tree[i ^ 1])
+            i >>= 1
+    
+    def query(self, l, r):
+        l += self.N
+        r += self.N
+        ans = self.given
+        
+        while l < r:
+            if (l & 1):
+               ans = self.func(self.tree[l], ans)
+               l += 1
+            if r & 1:
+                r -= 1
+                ans = self.func(self.tree[r], ans)
+                
+            l >>= 1
+            r >>= 1
+
+        return ans
+
+
 class StockPrice:
 
     def __init__(self):
-        self.time2stock = defaultdict(int)
-        self.curr = []
-        self.maxi = []
-        self.mini = []
-        
+        self.min_tree = SegmentTree([inf] * (10 ** 5), min, inf)
+        self.max_tree = SegmentTree([-inf] * (10 ** 5), max, -inf)
+        self.curr = 0
+        self.tracker = defaultdict(int)
 
     def update(self, timestamp: int, price: int) -> None:
-        self.time2stock[timestamp] = price
-        heapq.heappush(self.curr, (-timestamp, price))
-        heapq.heappush(self.maxi, (-price, timestamp))
-        heapq.heappush(self.mini, (price, timestamp))
+        self.curr = max(self.curr, timestamp)
+        self.tracker[timestamp] = price
+        self.min_tree.update(timestamp, price)
+        self.max_tree.update(timestamp, price)
+        print(self.min_tree.tree[-10:])
 
     def current(self) -> int:
-        while self.time2stock[ -self.curr[0][0] ] != self.curr[0][1]:
-            heapq.heappop(self.curr)
-        return self.curr[0][1]
-        
-    def maximum(self) -> int:
-        while self.time2stock[ self.maxi[0][1] ] != -self.maxi[0][0]:
-            heapq.heappop(self.maxi)
+        return self.tracker[self.curr]
 
-        return -self.maxi[0][0]
+    def maximum(self) -> int:
+        return self.max_tree.query(0, self.curr + 1)
 
     def minimum(self) -> int:
-        while self.time2stock[ self.mini[0][1] ] != self.mini[0][0]:
-            heapq.heappop(self.mini)
-
-        return self.mini[0][0]
+        return self.min_tree.query(0, self.curr + 1)
+        
 
 
 # Your StockPrice object will be instantiated and called as such:
 # obj = StockPrice()
 # obj.update(timestamp,price)
-# param_2 = obj.curr()
+# param_2 = obj.current()
 # param_3 = obj.maximum()
 # param_4 = obj.minimum()
