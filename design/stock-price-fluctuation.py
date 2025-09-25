8e6b0f813f0dac6ce6cf77b554bccf6508653077
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-24 21:56:51 -0400

    LC-stock-price-fluctuation [Runtime: N/A; Memory: N/A]

--- design/stock-price-fluctuation.py
+++ design/stock-price-fluctuation.py
@@ -1,64 +1,27 @@
-class SegmentTree:
-    def __init__(self, nums, func, given):
-        self.N = len(nums)
-        self.func = func
-        self.given = given
-
-        self.tree = [given] * (self.N * 2)
-        self.tree[self.N:] = nums
-        
-        for i in range(self.N - 1, 0, -1):
-            self.tree[i] = self.func(self.tree[i << 1], self.tree[(i << 1) | 1])
-    
-    def update(self, i, val):
-        i += self.N
-        self.tree[i] = val
-        while i > 1:
-            self.tree[i >> 1] = self.func(self.tree[i], self.tree[i ^ 1])
-            i >>= 1
-    
-    def query(self, l, r):
-        l += self.N
-        r += self.N
-        ans = self.given
-        
-        while l < r:
-            if (l & 1):
-               ans = self.func(self.tree[l], ans)
-               l += 1
-            if r & 1:
-                r -= 1
-                ans = self.func(self.tree[r], ans)
-                
-            l >>= 1
-            r >>= 1
-
-        return ans
-
-
 class StockPrice:
 
     def __init__(self):
-        self.min_tree = SegmentTree([inf] * (10 ** 5), min, inf)
-        self.max_tree = SegmentTree([-inf] * (10 ** 5), max, -inf)
-        self.curr = 0
-        self.tracker = defaultdict(int)
+        self.price_set = SortedSet()
+        self.today = 0
+        self.time2price = {}
 
     def update(self, timestamp: int, price: int) -> None:
-        self.curr = max(self.curr, timestamp)
-        self.tracker[timestamp] = price
-        self.min_tree.update(timestamp, price)
-        self.max_tree.update(timestamp, price)
-        print(self.min_tree.tree[-10:])
+        if timestamp in self.time2price:
+            old_price = self.time2price[timestamp]
+            self.price_set.remove(old_price)
+
+        self.today = max(self.today, timestamp)
+        self.time2price[timestamp] = price
+        self.price_set.add(price)
 
     def current(self) -> int:
-        return self.tracker[self.curr]
+        return self.time2price[self.today]
 
     def maximum(self) -> int:
-        return self.max_tree.query(0, self.curr + 1)
+        return self.price_set[-1]
 
     def minimum(self) -> int:
-        return self.min_tree.query(0, self.curr + 1)
+        return self.price_set[0]
         
 
 
