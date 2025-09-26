072a3d812d859e1f218ce085c092c3b9f1154110
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-26 13:47:54 -0400

    LC-data-stream-as-disjoint-intervals [Runtime: N/A; Memory: N/A]

---
+++ binary-search/data-stream-as-disjoint-intervals.py
@@ -0,0 +1,54 @@
+class DSU:
+    def __init__(self, N):
+        self.parents = list(range(N))
+        self.size = [0] * N
+    
+    def find(self, x):
+        if self.parents[x] != x:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+
+    def union(self, x, y):
+        xr, yr = self.find(x), self.find(y)
+        if xr == yr:
+            return
+        
+        if xr > yr:
+            xr, yr = yr, xr
+
+        self.parents[yr] = xr
+        self.size[xr] += self.size[yr]
+        self.size[yr] = self.size[xr]
+        return
+
+class SummaryRanges:
+
+    def __init__(self):
+        self.dsu = DSU(10)
+
+    def addNum(self, value: int) -> None:
+        # check value - 1 and value + 1 next to it
+        # merge these if both already in a set
+        self.dsu.size[value] = 1
+        if self.dsu.size[value - 1] != 0:
+            self.dsu.union(value, value - 1)
+        if self.dsu.size[value + 1] != 0:
+            self.dsu.union(value, value + 1)
+
+    def getIntervals(self) -> List[List[int]]:
+        i = 0
+        ans = []
+        while i < len(self.dsu.size):
+            if self.dsu.size[i] != 0:
+                ans.append([i, i + self.dsu.size[i] - 1])
+                i += self.dsu.size[i]
+            else:
+                i += 1
+        return ans
+        
+
+
+# Your SummaryRanges object will be instantiated and called as such:
+# obj = SummaryRanges()
+# obj.addNum(value)
+# param_2 = obj.getIntervals()
