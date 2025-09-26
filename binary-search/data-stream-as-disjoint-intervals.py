dcafa3b4bbb4b3db9a6b044120d6f5bc652fa38c
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-26 14:10:40 -0400

    LC-data-stream-as-disjoint-intervals [Runtime: 1 ms; Memory: 17.9 MB]

--- binary-search/data-stream-as-disjoint-intervals.py
+++ binary-search/data-stream-as-disjoint-intervals.py
@@ -1,59 +1,32 @@
-class DSU:
-    def __init__(self, N):
-        self.parents = list(range(N))
-        self.size = [0] * N
-    
-    def find(self, x):
-        if self.parents[x] != x:
-            self.parents[x] = self.find(self.parents[x])
-        return self.parents[x]
-
-    def union(self, x, y):
-        xr, yr = self.find(x), self.find(y)
-        if xr == yr:
-            return
-        
-        if xr > yr:
-            xr, yr = yr, xr
-
-        self.size[xr] += self.size[yr]
-        self.size[yr] = self.size[xr]
-        self.parents[yr] = xr
-        # print("SUB")
-        # print(self.size)
-        # print()
-        return
-
 class SummaryRanges:
 
     def __init__(self):
-        self.dsu = DSU(10 ** 4)
+        self.intervals = SortedList()
 
     def addNum(self, value: int) -> None:
-        # check value - 1 and value + 1 next to it
-        # merge these if both already in a set
-        if self.dsu.size[value] != 0:
+        if not self.intervals:
+            self.intervals.add([value, value])
             return
         
-        self.dsu.size[value] = 1
-        if self.dsu.size[value + 1] != 0:
-            self.dsu.union(value, value + 1)
+        after_idx = bisect_right(self.intervals, [value, inf])
+        before_idx = after_idx - 1
 
-        if self.dsu.size[value - 1] != 0:
-            self.dsu.union(value - 1, value)
+        if before_idx >= 0 and self.intervals[before_idx][0] <= value <= self.intervals[before_idx][1]:
+            return
+
+        curr_start, curr_end = value, value
+        if after_idx < len(self.intervals) and self.intervals[after_idx][0] == value + 1:
+            curr_end = self.intervals[after_idx][1]
+            self.intervals.pop(after_idx)
+
+        if before_idx >= 0 and self.intervals[before_idx][1] == value - 1:
+            curr_start = self.intervals[before_idx][0]
+            self.intervals.pop(before_idx)
+
+        self.intervals.add([curr_start, curr_end])
 
     def getIntervals(self) -> List[List[int]]:
-        i = 0
-        ans = []
-        while i < len(self.dsu.size):
-            if self.dsu.size[i] != 0:
-                ans.append([i, i + self.dsu.size[i] - 1])
-                i += self.dsu.size[i]
-            else:
-                i += 1
-        # print(self.dsu.size)
-        return ans
-        
+        return list(self.intervals)
 
 
 # Your SummaryRanges object will be instantiated and called as such:
