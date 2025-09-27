91dd18a323afad8497927439820f87630a6f40fc
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-26 23:22:32 -0400

    LC-fruits-into-baskets-iii [Runtime: N/A; Memory: N/A]

--- binary-search/fruits-into-baskets-iii.py
+++ binary-search/fruits-into-baskets-iii.py
@@ -1,29 +1,68 @@
+class SegmentTree:
+    def __init__(self, nums):
+        self.N = len(nums)
+        self.tree = [0] * (self.N * 2)
+        self.tree[self.N:] = nums
+        for i in range(self.N - 1, 0, -1):
+            self.tree[i] = min(self.tree[i << 1], self.tree[(i << 1) | 1])
+    
+    def update(self, idx, val):
+        idx += self.N
+        self.tree[idx] = val
+        while idx > 1:
+            self.tree[idx >> 1] = min(self.tree[idx], self.tree[idx ^ 1])
+            idx >>= 1
+
+    def query(self, l, r):
+        l += self.N
+        r += self.N
+        ans = inf
+        while l < r:
+            if l & 1:
+                ans = min(ans, self.tree[l])
+                l += 1
+            if r & 1:
+                r -= 1
+                ans = min(ans, self.tree[r])
+            
+            l >>= 1
+            r >>= 1
+
+        return ans
+                
+
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        
+        '''
+        intuition:
+        - consider sorted_fruits = [(baskets[i], i), ...]
+        - for fruit = f, we need to find the smallest index from j to n
+        given that baskets[j] >= f
+        - having the segment tree with nums is the list of i based on sorted_fruits
+        then we query for (j, n) as all basket after j > than f
+        '''
+        
         n = len(baskets)
-        m = int(math.sqrt(n))
-        section = (n + m - 1) // m
-        count = 0
-        maxV = [0] * section
+        sorted_baskets = sorted([(basket, i) for i, basket in enumerate(baskets)])
+        tree = SegmentTree([i for _, i in sorted_baskets])
+        pos = {origin_idx: i for origin_idx, (_, i) in enumerate(sorted_baskets)}
 
-        for i in range(n):
-            maxV[i // m] = max(maxV[i // m], baskets[i])
+        ans = 0
+        for f in fruits:
+            idx = bisect_left(sorted_baskets, (f, -1))
+            if idx == n:                
+                ans += 1
+                continue
+            
+            smallest_origin_idx = tree.query(idx, n)
+            if smallest_origin_idx == inf:
+                ans += 1
+                continue
 
-        for fruit in fruits:
-            unset = 1
-            for sec in range(section):
-                if maxV[sec] < fruit:
-                    continue
-                choose = 0
-                maxV[sec] = 0
-                for i in range(m):
-                    pos = sec * m + i
-                    if pos < n and baskets[pos] >= fruit and not choose:
-                        baskets[pos] = 0
-                        choose = 1
-                    if pos < n:
-                        maxV[sec] = max(maxV[sec], baskets[pos])
-                unset = 0
-                break
-            count += unset
-        return count
+            tree.update(pos[smallest_origin_idx], inf)
+
+        return ans
+
+
+
