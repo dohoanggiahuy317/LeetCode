a77418adb22ae1678b930fc2d57c4ac3d38c9ce0
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-26 22:28:25 -0400

    LC-design-a-number-container-system [Runtime: 727 ms; Memory: 151.8 MB]

--- hash-table/design-a-number-container-system.py
+++ hash-table/design-a-number-container-system.py
@@ -1,33 +1,30 @@
 class NumberContainers:
+
     def __init__(self):
-        # Map to store number -> min heap of indices
-        self.number_to_indices = defaultdict(list)
-        # Map to store index -> number
-        self.index_to_numbers = {}
+        self.num2idx = {}
+        self.idx2num = {}
 
     def change(self, index: int, number: int) -> None:
-        # Update index to number mapping
-        self.index_to_numbers[index] = number
+        
+        # Handle idx2num
+        if index in self.idx2num:
+            prev_num = self.idx2num[index]
+            self.num2idx[prev_num].remove(index)
 
-        # Add index to the min heap for this number
-        heapq.heappush(self.number_to_indices[number], index)
+        self.idx2num[index] = number
+
+        # handle num2idx
+        if number not in self.num2idx:
+            self.num2idx[number] = SortedSet()
+
+        self.num2idx[number].add(index)
+        
 
     def find(self, number: int) -> int:
-        # If number doesn't exist in our map
-        if not self.number_to_indices[number]:
+        if number not in self.num2idx or len(self.num2idx[number]) == 0:
             return -1
-
-        # Keep checking top element until we find valid index
-        while self.number_to_indices[number]:
-            index = self.number_to_indices[number][0]
-
-            # If index still maps to our target number, return it
-            if self.index_to_numbers.get(index) == number:
-                return index
-
-            # Otherwise remove this stale index
-            heapq.heappop(self.number_to_indices[number])
-        return -1
+        return self.num2idx[number][0]
+        
 
 
 # Your NumberContainers object will be instantiated and called as such:
