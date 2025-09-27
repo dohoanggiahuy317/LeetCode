94684998c78ac663db7f4f652b55fc42c2d128a2
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-26 22:08:49 -0400

    LC-non-overlapping-intervals [Runtime: 193 ms; Memory: 49.8 MB]

--- array/non-overlapping-intervals.py
+++ array/non-overlapping-intervals.py
@@ -1,18 +1,18 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         
-        sorted_intervals = SortedList(intervals, lambda x: (x[1], x[0]))
+        sorted_intervals = sorted(intervals, key = lambda x: (x[1], x[0]))
         # print(sorted_intervals)
 
-        idx = 1
+        prev, curr = 0, 1
         ans = 0
-        while idx < len(sorted_intervals):
-            start, end = sorted_intervals[idx]
-            if start < sorted_intervals[idx - 1][1]:
-                sorted_intervals.pop(idx)
+        while curr < len(sorted_intervals):
+            start, _ = sorted_intervals[curr]
+            if start < sorted_intervals[prev][1]:
                 ans += 1
             else:
-                idx += 1
+                prev = curr
+            curr += 1
             # print(idx, sorted_intervals)
         
         return ans
