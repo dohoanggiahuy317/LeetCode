79fbf4b82523e11d0ab456773a550e58dca61350
Jeremy <giahuy.jeremy.do@gmail.com>
2025-09-27 22:07:55 -0400

    LC-finding-pairs-with-a-certain-sum [Runtime: 251 ms; Memory: 49.3 MB]

--- hash-table/finding-pairs-with-a-certain-sum.py
+++ hash-table/finding-pairs-with-a-certain-sum.py
@@ -2,15 +2,22 @@ class FindSumPairs:
 
     def __init__(self, nums1: List[int], nums2: List[int]):
         self.nums1_c = Counter(nums1)
-        self.nums2_l = nums2[:]
+        self.nums2 = nums2[:]
+        self.nums2_c = Counter(nums2)
 
     def add(self, index: int, val: int) -> None:
-        self.nums2_l[index] += val
+        old_val = self.nums2[index]
+        self.nums2_c[old_val] -= 1  
+
+        self.nums2[index] += val
+        
+        new_val = self.nums2[index]
+        self.nums2_c[new_val] += 1  
 
     def count(self, tot: int) -> int:
         ans = 0
-        for num in self.nums2_l:
-            ans += self.nums1_c[tot - num]
+        for num, freq in self.nums1_c.items():
+            ans += self.nums2_c[tot - num] * freq
 
         return ans
 
