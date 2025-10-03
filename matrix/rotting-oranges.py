92d2d78a44d26beea946e9c8e6df091782c8feb5
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-03 10:17:41 -0400

    LC-rotting-oranges [Runtime: 7 ms; Memory: 17.8 MB]

--- matrix/rotting-oranges.py
+++ matrix/rotting-oranges.py
@@ -1,53 +1,36 @@
-DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        # Constant
+        DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         m, n = len(grid), len(grid[0])
-        FRESH_ORANGE = 1
-        ROTTEN_ORANGE = 2
-        EMPTY_ORANGE = 0
+        
+        just_rotten = deque()
+        num_fresh = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    num_fresh += 1
+                if grid[i][j] == 2:
+                    just_rotten.append((i, j))
 
-        # Set up BFS
-        queue = deque()
-        good_orange_left = 0
-        for i, row in enumerate(grid):
-            for j, orange in enumerate(row):
-                if orange == FRESH_ORANGE:
-                    good_orange_left += 1
-                if orange == ROTTEN_ORANGE:
-                    queue.append((i, j))
-        reachable = set(queue)
-        time = 0
+        t = 0
+        while just_rotten or not num_fresh:
+            if num_fresh == 0:
+                return t
+            for _ in range(len(just_rotten)):
+                x, y = just_rotten.popleft()
+                
+                for dx, dy in DIRECTIONS:
+                    nx, ny = x + dx, y + dy
 
-        # Start BFS
-        while queue or not good_orange_left:
-            # Return if there is no good orange left
-            if good_orange_left == 0:
-                return time
-
-            for _ in range(len(queue)):
-                i, j = queue.popleft()
-
-                for x, y in DIRECTIONS:
-                    ni, nj = i + x, j + y
-
-                    if not ( 0 <= ni < m and 0 <= nj < n):
+                    if not (0 <= nx < m and 0 <= ny < n):
                         continue
-                    if grid[ni][nj] == EMPTY_ORANGE:
-                        continue
-                    if grid[ni][nj] == ROTTEN_ORANGE:
+                    if grid[nx][ny] != 1:
                         continue
 
-                    # turn the orange to be rotten, once it get rotten
-                    # by update the grid[i][j] to 2 (if not this orage will be 
-                    # consider again if another rotten orange touch this)
-                    grid[ni][nj] = ROTTEN_ORANGE
-                    good_orange_left -= 1
-
-                    queue.append((ni, nj))
-                    reachable.add((ni, nj))
+                    just_rotten.append((nx, ny))
+                    grid[nx][ny] = 2
+                    num_fresh -= 1
             
-            time += 1 # time done after each infection round
+            t += 1
 
         return -1
