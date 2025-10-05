b2717a047cc34e0af144ebf6de49367f5a7f1e30
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-04 21:38:54 -0400

    LC-min-cost-climbing-stairs [Runtime: 3 ms; Memory: 17.7 MB]

--- array/min-cost-climbing-stairs.py
+++ array/min-cost-climbing-stairs.py
@@ -1,11 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-
-        dp = { -1: 0, 0: cost[0]}
-
-        for i in range(1, len(cost)):
-            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
-
-
-        return min(dp[len(cost) - 1], dp[len(cost) - 2])
+        n = len(cost)
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1])
         
+        return dp[-1]
