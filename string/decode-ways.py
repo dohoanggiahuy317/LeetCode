d1b87457b688dd70c46715efb90d179ffbdb20c3
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-05 02:09:39 -0400

    LC-decode-ways [Runtime: N/A; Memory: N/A]

--- string/decode-ways.py
+++ string/decode-ways.py
@@ -1,28 +1,30 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = [1] * (len(s) + 1)
+        dp = [0] * len(s)
+        dp[-1] = 1 if s[-1] != "0" else 0
 
-        for i in range(len(s) - 1, -1, -1):
+        for i in range(len(s) - 2, -1, -1):
             if s[i] == "0":
                 dp[i] = 0
+
+            elif s[i] not in "12":
+                if s[i + 1] == "0" and i + 2 >= len(s):
+                    dp[i] = 1
+                else:
+                    dp[i] = dp[i + 2]
+
             elif s[i] == "1":
-                if i < len(s) - 1 and s[i+1] == "0":
-                    dp[i] = dp[i+2]
-                elif i < len(s) - 1:
-                    dp[i] = dp[i+1] + dp[i+2]
+                if s[i + 1] == "0" or (i + 2 < len(s) and s[i + 2] == "0"):
+                    dp[i] = dp[i + 1]
                 else:
-                    dp[i] = dp[i+1]
-            elif s[i] == "2":
-                if i < len(s) - 1 and s[i+1] == "0":
-                    dp[i] = dp[i+2]
-                elif i < len(s) - 1 and s[i+1] in "789":
-                    dp[i] = dp[i+1]
-                elif i < len(s) - 1:
-                    dp[i] = dp[i+1] + dp[i+2]
-                else:
-                    dp[i] = dp[i+1]
-            else:
-                dp[i] = dp[i+1]
+                    dp[i] = dp[i + 1] + 1
             
-        print(dp)
+            elif s[i] == "2":
+                if s[i + 1] in "0789" or (i + 2 < len(s) and s[i + 2] == "0"):
+                    dp[i] = dp[i + 1]
+                else:
+                    dp[i] = dp[i + 1] + 1
+
+        # print(dp)
         return dp[0]
+
