f388a0375b09e07a8f678e0546929579c316d792
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-05 02:53:27 -0400

    LC-decode-ways [Runtime: 0 ms; Memory: 17.6 MB]

--- dynamic-programming/decode-ways.py
+++ dynamic-programming/decode-ways.py
@@ -8,31 +8,25 @@ class Solution:
                 dp[i] = 0
 
             elif s[i] not in "12":
-                if s[i + 1] == "0" and i + 2 >= len(s):
-                    dp[i] = 1
-                else:
-                    dp[i] = dp[i + 1]
+                if s[i + 1] == "0":
+                    return 0
+                dp[i] = dp[i + 1]
 
             elif s[i] == "1":
-                if s[i + 1] == "0" and i + 2 >= len(s):
-                    dp[i] = 1
-                elif s[i + 1] == "0":
+                if s[i + 1] == "0":
                     dp[i] = dp[i + 2] if i + 2 < len(s) else 1
                 elif i + 2 < len(s) and s[i + 2] == "0":
                     dp[i] = dp[i + 1]
                 else:
-                    dp[i] = dp[i + 1] + 1
+                    dp[i] = dp[i + 1] + (dp[i + 2] if i + 2 < len(s) else 1)
             
             elif s[i] == "2":
-                if s[i + 1] == "0" and i + 2 >= len(s):
-                    dp[i] = 1
-                elif s[i + 1] in "0789":
+                if s[i + 1] in "0789":
                     dp[i] = dp[i + 2] if i + 2 < len(s) else 1
-                elif  (i + 2 < len(s) and s[i + 2] == "0"):
+                elif i + 2 < len(s) and s[i + 2] == "0":
                     dp[i] = dp[i + 1]
                 else:
-                    dp[i] = dp[i + 1] + 1
+                    dp[i] = dp[i + 1] + (dp[i + 2] if i + 2 < len(s) else 1)
 
         # print(dp)
         return dp[0]
-
