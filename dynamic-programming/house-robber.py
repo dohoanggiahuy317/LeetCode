fc43dba33bc3b8890e266a0e616896edf941068b
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-07 03:43:34 -0400

    LC-house-robber [Runtime: N/A; Memory: N/A]

--- dynamic-programming/house-robber.py
+++ dynamic-programming/house-robber.py
@@ -1,15 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) < 3:
-            return max(nums)
+        n = len(nums)
+        dp = [0] * n
 
-        x_3 = nums[-1]
-        x_2 = nums[-2]
-        x_1 = nums[-3] + nums[-1]
+        for i in range(n):
+            dp[i] = nums[i]
+            for j in range(i - 1):
+                dp[i] = max(dp[j] + nums[i], dp[i])
 
-
-        for i in range(len(nums) - 4, -1, -1):
-            temp = nums[i] + max(x_2, x_3)
-            x_3, x_2, x_1 = x_2, x_1, temp
+        # print(dp)
         
-        return max(x_1, x_2, x_3)
+        return max(dp[-1], dp[-2])
