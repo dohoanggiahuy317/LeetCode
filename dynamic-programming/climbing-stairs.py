d9b44fe3a77f40df912a0b37979beb469d41b5fb
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-07 03:36:50 -0400

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- dynamic-programming/climbing-stairs.py
+++ dynamic-programming/climbing-stairs.py
@@ -1,13 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n<=2:
-            return n
-        
-        ans=0
-        first=1
-        second=2
-        for i in range(n-2):
-            ans=first+second
-            first=second
-            second=ans
-        return ans
+        dp = [0] * n
+        dp[0], dp[1] = 1, 2
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[-1]
