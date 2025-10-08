53db35ff8a95dfc4bc8fd3d68e9687cce5269e47
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-08 16:04:21 -0400

    LC-shortest-path-visiting-all-nodes [Runtime: 703 ms; Memory: 19.5 MB]

--- dynamic-programming/shortest-path-visiting-all-nodes.py
+++ dynamic-programming/shortest-path-visiting-all-nodes.py
@@ -1,37 +1,34 @@
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
-        # Constant
         n = len(graph)
-        ALL_VISITED = (1 << n) - 1
-
-        # Set up BFS
-        queue = deque([(start, 1 << start) for start in range(n)])
-        reachable = set(queue)
-        step = 0
-            
-        while queue:
-            for _ in range(len(queue)):
-                node, past_path = queue.popleft()
-                
-                # Visited all
-                if past_path == ALL_VISITED:
-                    return step
-
-                for neigh in graph[node]:
-                    # Activate this neighbor up
-                    current_path = past_path | (1 << neigh)
-
-                    if (neigh, current_path) in reachable:
-                        continue
-
-                    queue.append((neigh, current_path))
-                    reachable.add((neigh, current_path))
-
-            step += 1
+        g = {i: neighs for i, neighs in enumerate(graph)}
         
-        return -1
+        def bfs(src):
+            queue = deque([(src, 1 << src)])
+            visited = set(queue)
+            step = 0
+
+            while queue:
+                for _ in range(len(queue)):
+                    node, k = queue.popleft()
+
+                    if k == (1 << n) - 1: #
+                        return step
+
+                    for neigh in g[node]: #
+                        new_k = k | (1 << neigh)
+                        if (neigh, new_k) in visited:
+                            continue
+                        queue.append((neigh, new_k))
+                        visited.add((neigh, new_k))
+
+                step += 1
+
+            return step
 
 
+        ans = inf
+        for i in range(n):
+            ans = min(ans, bfs(i))
 
-
-     
+        return ans
