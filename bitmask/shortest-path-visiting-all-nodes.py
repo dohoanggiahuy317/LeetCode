468b03aa00e452eaeafc2d2fe888f06e0d5e94e4
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-08 16:06:18 -0400

    LC-shortest-path-visiting-all-nodes [Runtime: 115 ms; Memory: 22.3 MB]

--- bitmask/shortest-path-visiting-all-nodes.py
+++ bitmask/shortest-path-visiting-all-nodes.py
@@ -3,32 +3,24 @@ class Solution:
         n = len(graph)
         adj_graph = {i: neighs for i, neighs in enumerate(graph)}
         
-        def bfs(src):
-            queue = deque([(src, 1 << src)])
-            visited = set(queue)
-            step = 0
+        queue = deque([(src, 1 << src) for src in range(n)])
+        visited = set(queue)
+        step = 0
 
-            while queue:
-                for _ in range(len(queue)):
-                    node, k = queue.popleft()
+        while queue:
+            for _ in range(len(queue)):
+                node, k = queue.popleft()
 
-                    if k == (1 << n) - 1: #
-                        return step
+                if k == (1 << n) - 1: #
+                    return step
 
-                    for neigh in adj_graph[node]: #
-                        new_k = k | (1 << neigh)
-                        if (neigh, new_k) in visited:
-                            continue
-                        queue.append((neigh, new_k))
-                        visited.add((neigh, new_k))
+                for neigh in adj_graph[node]: #
+                    new_k = k | (1 << neigh)
+                    if (neigh, new_k) in visited:
+                        continue
+                    queue.append((neigh, new_k))
+                    visited.add((neigh, new_k))
 
-                step += 1
+            step += 1
 
-            return step
-
-
-        ans = inf
-        for i in range(n):
-            ans = min(ans, bfs(i))
-
-        return ans
+        return step
