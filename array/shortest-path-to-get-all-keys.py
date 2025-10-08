a0836573620fc42520b5a216dc9a4561bdb47d19
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-08 00:09:08 -0400

    LC-shortest-path-to-get-all-keys [Runtime: N/A; Memory: N/A]

--- array/shortest-path-to-get-all-keys.py
+++ array/shortest-path-to-get-all-keys.py
@@ -1,85 +1,58 @@
-from collections import deque
-
-DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-KEY_DICT = {
-    "a": 0,
-    "b": 1,
-    "c": 2,
-    "d": 3,
-    "e": 4,
-    "f": 5,
-}
-
 class Solution:
     def shortestPathAllKeys(self, grid: List[str]) -> int:
-        # find k
-
-
-        # queue = (x, y, key_state, step)
-
-
-        # while
-        #     step += 1
-        #     for
-        # if find all key
-
-
         m, n = len(grid), len(grid[0])
-        k = 0
+        DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        def bfs(src):
+            nonlocal keys_set, m, n
 
+            q = deque([src])
+            visited = set(q)
+            step = 0
+
+            while q:
+                for _ in range(len(q)):
+                    x, y = q.popleft()
+
+                    if (x, y) in keys_set:
+                        keys_set.remove((x, y))
+                        key_found[grid[x][y].upper()] = True
+                        return (x, y), step
+
+                    for dx, dy in DIRECTIONS:
+                        nx, ny = x + dx, y + dy
+
+                        if not (0 <= nx < m and 0 <= ny < n):
+                            continue
+                        if grid[nx][ny] == "#":
+                            continue
+                        if grid[nx][ny].isalpha() and grid[nx][ny].isupper():
+                            if not key_found[grid[nx][ny]]:
+                                continue
+
+                        q.append((nx, ny))
+                        visited.add((nx, ny))
+
+                step += 1
+
+            return (-1, -1), -1
+
+        keys_set = set()
+        key_found = {}
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "@":
-                    start = (i, j)
-                if grid[i][j].islower():
-                    k += 1
+                if grid[i][j].isalpha() and grid[i][j].islower():
+                    keys_set.add((i, j))
+                if grid[i][j].isalpha() and grid[i][j].isupper():
+                    key_found[grid[i][j]] = False
+        
+        curr_start = (0, 0)
+        ans = 0
 
-        queue = deque()
-        init_state = (start[0], start[1], 0)
-        queue.append(init_state)
-        visited = set()
-        step = 0
+        while keys_set:
+            curr_start, step = bfs(curr_start)
+            ans += step
+            if curr_start == (-1, -1):
+                return -1
 
-
-        while queue:
-            for _ in range(len(queue)):
-                x, y, key = queue.popleft()
-                # print(x, y, bin(key))
-
-                # check co full key
-                if key == (1 << k) - 1:
-                    # print(k)
-                    # print(bin(key), bin((1 << k) - 1))
-                    # print("FULL")
-                    return step
-                
-                # di den cac avalabile oo ben canh voi key state moi
-                for dx, dy in DIRECTIONS:
-                    nx, ny = x + dx, y + dy
-
-                    if not (0 <= nx < m and 0 <= ny < n):
-                        continue
-
-                    if grid[nx][ny] == "#":
-                        continue
-                        
-                    new_key = key
-                    if grid[nx][ny].islower():
-                        new_key = key | (1 << KEY_DICT[grid[nx][ny]])
-                    
-                    if (nx, ny, new_key) in visited:
-                        continue
-                    if grid[nx][ny].isupper():
-                        if not key >> KEY_DICT[grid[nx][ny].lower()] & 1:
-                            continue
-                    
-
-                    queue.append( (nx, ny, new_key) )
-                    visited.add((nx, ny, new_key))
-
-            step += 1
-
-        return -1
-# @...a
-# .###A
-# b.BCc
+        return ans
