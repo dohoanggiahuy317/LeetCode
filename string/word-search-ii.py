d74fdd2007b33b8d8d704e4935b57ea97ddf86c8
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-09 16:12:17 -0400

    LC-word-search-ii [Runtime: N/A; Memory: N/A]

--- string/word-search-ii.py
+++ string/word-search-ii.py
@@ -1,79 +1,60 @@
-DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
 class TrieNode:
-    def __init__(self, ch):
-        self.ch = ch
+    def __init__(self, char):
+        self.char = char
         self.children = {}
         self.exist = False
 
 class Trie:
     def __init__(self):
         self.root = TrieNode("^")
-
+    
     def insert(self, word):
-        node = self.root
-        for ch in word:
-            if ch not in node.children:
-                node.children[ch] = TrieNode(ch)
-            node = node.children[ch]
-        node.exist = True
-            
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode(char)
+            curr = curr.children[char]
+        curr.exist = True
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        
-        # INSERT TRIE
         trie = Trie()
         for word in words:
             trie.insert(word)
 
-        # DFS FOR EACH WORD
-        def dfs(x, y, node, step):
-            nonlocal visited, curr_word, ans, m, n
-        
-            # no word has length > 10
-            if step == 10:
+        m, n = len(board), len(board[0])
+        DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        def dfs(x, y, curr_word, node):
+            if board[x][y] not in node.children:
                 return
             
-            # if cannot make word from this path
-            ch = board[x][y]
-            if ch not in node.children:
-                return
+            char = board[x][y]            
+            visited[(x, y)] = True
+            curr_word.append(char)
+            next_node = node.children[char]
+            if next_node.exist:
+                ans.append("".join(curr_word))
 
-            # get the word
-            visited[x][y] = True
-            curr_word.append(ch)
-            next_node = node.children[ch]
-
-            if next_node.exist: # Found word here
-                ans.add("".join(curr_word))
-
-            # explore other
             for dx, dy in DIRECTIONS:
                 nx, ny = x + dx, y + dy
+
                 if not (0 <= nx < m and 0 <= ny < n):
                     continue
-                if visited[nx][ny]:
+                if visited[(nx, ny)]:
                     continue
-                
-                dfs(nx, ny, next_node, step + 1)
-  
-
-            visited[x][y] = False
+                dfs(nx, ny, curr_word, next_node)
+            
+            visited[(x, y)] = False
             curr_word.pop()
 
-        # INIT
-        m, n = len(board), len(board[0])
-        ans = set()
-        curr_word = []
-        visited = [[False] * n for _ in range(m)]
-        
-        # LOOP
-        for x in range(m):
-            for y in range(n):
-                dfs(x, y, trie.root, 0)
+        ans = []
+        for i in range(m):
+            for j in range(n):
+                visited = defaultdict(bool)
+                dfs(i, j, [], trie.root)
 
-        return list(ans)
+        return ans
+            
 
-
-
+            
