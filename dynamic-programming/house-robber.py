08b934a424ca653a8a97f7bad254d03fb02c6e50
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-10 11:25:09 -0400

    LC-house-robber [Runtime: 0 ms; Memory: 17.7 MB]

--- dynamic-programming/house-robber.py
+++ dynamic-programming/house-robber.py
@@ -6,13 +6,10 @@ class Solution:
             return nums[0]
 
         dp = [0] * n
-        pref_max = [0] * n
 
         for i in range(n):
-            dp[i] = nums[i] + (pref_max[i - 2] if i - 2 >= 0 else 0)
-            pref_max[i] = max(dp[i], pref_max[i - 1])
-
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
         # print(dp)
         # print(pref_max)
         
-        return max(dp[-1], dp[-2])
+        return dp[-1]
