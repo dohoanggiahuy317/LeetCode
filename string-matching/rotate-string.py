d0b8f7b5c60029ed236eb49e8651813380afbb6a
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-11 21:54:58 -0400

    LC-rotate-string [Runtime: 0 ms; Memory: 17.6 MB]

--- string-matching/rotate-string.py
+++ string-matching/rotate-string.py
@@ -1,7 +1,13 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        for i in range(len(s)):
-            if goal == s[i:] + s[:i]:
+
+        goal_li = deque(list(goal))
+        s_li = list(s)
+        for i in range(len(goal_li)):
+            # print(goal_li, s_li)
+            if list(goal_li) == s_li:
                 return True
+            goal_li.append(goal_li.popleft())
+
 
         return False
