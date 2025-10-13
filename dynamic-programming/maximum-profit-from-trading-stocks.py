93504f09fe70629cded1d6d531c43667f69ebf5a
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-13 11:23:07 -0400

    LC-maximum-profit-from-trading-stocks [Runtime: 1486 ms; Memory: 56.8 MB]

--- dynamic-programming/maximum-profit-from-trading-stocks.py
+++ dynamic-programming/maximum-profit-from-trading-stocks.py
@@ -7,12 +7,21 @@ class Solution:
         dp = [[0] * (budget + 1) for _ in range(m)]
         profit = [f - p for p, f in zip(present, future)]
 
+        for j in range(budget + 1):
+            if j >= present[0]:
+                dp[0][j] = max(0, profit[0])
+
         for i in range(m):
             for j in range(budget + 1):
-                dp[i][j] = max(
-                    dp[i - 1][j],
-                    dp[i - 1][j - present[i]] + profit[i] if j - present[i] >= 0 else 0
-                )
+                if i > 0:
+                    dp[i][j] = max(
+                        dp[i - 1][j],
+                        dp[i - 1][j - present[i]] + profit[i] if j - present[i] >= 0 else -inf
+                    )
+
+            # for x in dp:
+            #     print(x)
+            # print()
 
 
         return dp[-1][-1]
