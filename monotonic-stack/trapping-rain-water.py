8eaf315a2e270341c56d89c88cc79f96b4ba8e3f
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-13 08:47:36 -0400

    LC-trapping-rain-water [Runtime: 27 ms; Memory: 19.5 MB]

--- monotonic-stack/trapping-rain-water.py
+++ monotonic-stack/trapping-rain-water.py
@@ -1,51 +1,16 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        n = len(height)
+        prefix_max = [height[0]] * n
+        suffix_max = [height[-1]] * n
 
-        start, end = 0, len(height) - 1
-
-        while start < len(height) - 1 and height[start] < height[start + 1]:
-            start += 1
-        while end > 0 and height[end] < height[end - 1]:
-            end -= 1
-
-        if end <= start:
-            return 0
-
-        track = []
-
-        for i in range(start, end + 1):
-            track.append((i, height[i]))
-
-        track.sort(key = lambda x: x[1], reverse = True)
+        for i in range(1, n):
+            prefix_max[i] = max(prefix_max[i - 1], height[i])
+        for i in range(n - 2, -1, -1):
+            suffix_max[i] = max(suffix_max[i + 1], height[i])
 
         ans = 0
-        min_range = min(track[0][0], track[1][0])
-        max_range = max(track[0][0], track[1][0])
-
-        lower_bank = min(height[min_range], height[max_range])
-        for col_ind in range(min_range, max_range + 1):
-            ans += max(0, lower_bank - height[col_ind])
-
-        track.pop(0)
-        track.pop(0)
-
-        while track:
-            next_col = track.pop(0)[0]
-            if min_range < next_col < max_range:
-                continue
-            
-            elif next_col > max_range:
-                for col_ind in range(max_range, next_col + 1):
-                    ans += max(0, height[next_col] - height[col_ind])
-                
-                max_range = next_col
-
-            elif next_col < max_range:
-                for col_ind in range(next_col, min_range + 1):
-                    ans += max(0, height[next_col] - height[col_ind])
-                
-                min_range = next_col
+        for i in range(n):
+            ans += min(suffix_max[i], prefix_max[i]) - height[i]
 
         return ans
-           
-        
