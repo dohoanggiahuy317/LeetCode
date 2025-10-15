af020fdad4ce157c0bea9bc02b074e3dcee5a5d4
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-15 12:13:13 -0400

    LC-container-with-most-water [Runtime: 103 ms; Memory: 28.5 MB]

--- greedy/container-with-most-water.py
+++ greedy/container-with-most-water.py
@@ -1,28 +1,12 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-
-        sort_height = []
-        for i in range(1, len(height) + 1):
-            sort_height.append((i, height[i-1]))
-        
-        sort_height = sorted(sort_height, key=lambda x: x[1], reverse=True)
-        maxx, maxy = sort_height.pop(0)
-
-        lx, ly = maxx, maxy
-        rx, ry = maxx, maxy
-
+        l, r = 0, len(height) - 1
         ans = 0
-
-        while sort_height:
-            curx, cury = sort_height.pop(0)
-
-            if curx < lx:
-                ans = max(ans, (rx - curx) * cury )
-                lx = curx
-                ly = cury
-            elif curx > rx:
-                ans = max(ans, (curx - lx) * cury )
-                rx = curx
-                ry = cury
-
+        while l < r:
+            ans = max(ans, (r - l) * min(height[l], height[r]))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        
         return ans
