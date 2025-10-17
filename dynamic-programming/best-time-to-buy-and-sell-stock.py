a1c7c69deac60162bb6823bb1adb12a0b63e1d76
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-17 15:23:03 -0400

    LC-best-time-to-buy-and-sell-stock [Runtime: N/A; Memory: N/A]

--- dynamic-programming/best-time-to-buy-and-sell-stock.py
+++ dynamic-programming/best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        prefix_min = [prices[0]] * n
-        suffix_max = [prices[-1]] * n
-
-        for i in range(1, n):
-            prefix_min[i] = min(prefix_min[i - 1], prices[i])
-
-        for i in range(n - 2, -1, -1):
-            suffix_max[i] = max(suffix_max[i + 1], prices[i])
         
+        prefix_min = [inf] * n
+        prefix_min[0] = prices[0]
+
         ans = 0
-        for i in range(n):
-            ans = max(ans, suffix_max[i] - prefix_min[i])
-        
+        for i, price in enumerate(prices[1:]):
+            ans = max(ans, price - prefix_min[i - 1])
+            prefix_min[i] = min(price, prefix_min[i - 1])
+
         return ans
+
+
+        
