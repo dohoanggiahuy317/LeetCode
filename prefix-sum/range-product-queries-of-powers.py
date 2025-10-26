4a62dbbac9ec530a817dd0bb822c049bad058095
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-25 22:07:16 -0400

    LC-range-product-queries-of-powers [Runtime: 191 ms; Memory: 47.5 MB]

--- prefix-sum/range-product-queries-of-powers.py
+++ prefix-sum/range-product-queries-of-powers.py
@@ -1,21 +1,48 @@
+MOD = (10 ** 9) + 7
+
+class SegmentTree:
+    def __init__(self, li):
+        self.N = len(li)
+        self.tree = [0] * (self.N * 2)
+        self.tree[self.N:] = li
+        
+        for i in range(self.N - 1, 0, -1):
+            self.tree[i] = self.tree[i << 1] + self.tree[(i << 1 )| 1]
+
+    def query(self, l, r):
+        l += self.N
+        r += self.N
+        ans = 0
+
+        while l < r:
+            if l & 1:
+                ans += self.tree[l]
+                l += 1
+            if r & 1:
+                r -= 1
+                ans += self.tree[r]
+
+            r >>= 1
+            l >>= 1
+
+        return ans
+
 class Solution:
     def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        MOD = 10 ** 9 + 7
-        exps = []
-    
-        while n > 0:
-            exp = int(math.log(n, 2))
-            exps.append(exp)
-            n -= 2 ** exp
-
-        exps.sort()
-        prefsum = [0]
-        for exp in exps:
-            new_pref = (prefsum[-1] + exp) 
-            prefsum.append(new_pref)        
-
+        
+        largest_power = int(math.log(n, 2))
+        li = []
+        while largest_power >= 0:
+            if 2 ** largest_power <= n:
+                li.append(largest_power)
+                n -= 2 ** largest_power
+            largest_power -= 1
+        
+        li.sort()
         ans = []
+        print(li)
+        tree = SegmentTree(li)
         for l, r in queries:
-            ans.append((1 << (prefsum[r+1] - prefsum[l])) % MOD)
+            ans.append(  ( 2 ** (tree.query(l, r + 1))) % MOD )
 
         return ans
