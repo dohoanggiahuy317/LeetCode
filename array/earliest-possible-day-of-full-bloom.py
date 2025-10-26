ba00218c76f013ab3e43ebeb90e293c6f3b0f9e9
Jeremy <giahuy.jeremy.do@gmail.com>
2025-10-25 22:49:00 -0400

    LC-earliest-possible-day-of-full-bloom [Runtime: 487 ms; Memory: 39.7 MB]

--- array/earliest-possible-day-of-full-bloom.py
+++ array/earliest-possible-day-of-full-bloom.py
@@ -14,36 +14,40 @@ class Solution:
         # 2 1
 
         
-        # 0: 0 2
-        # 1: 0 2
-        # 2: 1 1
+        # 0: 1 2
+        # 1: 3 2
+        # 2: 2 1
         # 3: 2 1
 
-        # 1 1 1 1 1 X
-        # - - 0 0 0 X
-        # - - - 2 - 2 2 X
-        # - - - - 3 - 3 3 X
+        # 0 0 0 X
+        # - 2 2 2 X
+        # - - - 2 2 2 X
+        # - - - - - 1 1 1 1 1 X
+
+        # 3 3 3 3 3 X
+        # - - 2 2 2 X
+        # - - - - 2 2 2 X
+        # - - - - - - 0 0 0 X
 
 
         pq = []
 
         for plant, grow in zip(plantTime, growTime):
-            heapq.heappush(pq, (-plant, -grow))
+            heapq.heappush(pq, (-grow, plant))
 
         day = 0
         ans = 0
         while pq:
-            p, g = heapq.heappop(pq)
-            p = -p
+            g, p = heapq.heappop(pq)
             g = -g
             
-            day += 1
+            day += p
             p -= 1
             
-            if p > 0:
-                heapq.heappush(pq, (-p, -g))
-            else:
-                ans = max(ans, day + g)
+            # if p > 0:
+            #     heapq.heappush(pq, (p, -g))
+            # else:
+            ans = max(ans, day + g)
 
             # print(pq, ans, day)
 
