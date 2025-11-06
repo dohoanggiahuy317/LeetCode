747fa58efe28afe0a61e50ed93513a3f87e5ac26
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-06 14:04:17 -0500

    LC-number-of-islands [Runtime: 265 ms; Memory: 28 MB]

--- union-find/number-of-islands.py
+++ union-find/number-of-islands.py
@@ -1,30 +1,33 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = [[False] * len(grid[0]) for _ in range(len(grid))]
+        m, n = len(grid), len(grid[0])
+        DIRS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        def span_island(x, y):
+            nonlocal m, n, DIRS, grid
+
+            visited.add((x, y))
+
+            for dx, dy in DIRS:
+                nx, ny = x + dx, y + dy
+
+                if not (0 <= nx < m and 0 <= ny < n):
+                    continue
+                if grid[nx][ny] == "0":
+                    continue
+                if (nx, ny) in visited:
+                    continue
+
+                span_island(nx, ny)
+                
+        visited = set()
         ans = 0
-
-        def dfs(i, j):
-            nonlocal grid, visited
-
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
-                return
-
-            if grid[i][j] == "0" or visited[i][j]:
-                return
-            
-            visited[i][j] = True
-
-            dfs(i-1, j)
-            dfs(i+1, j)
-            dfs(i, j-1)
-            dfs(i, j+1)
-
-            
-
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if not visited[i][j] and grid[i][j] == "1":
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "0":
+                    continue
+                if (i, j) not in visited:
                     ans += 1
-                    dfs(i, j)
+                    span_island(i, j)
 
         return ans
