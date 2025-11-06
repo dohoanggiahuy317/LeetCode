ac72930e900adddbeb7152fb49b290c8cd87afb6
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-06 11:01:15 -0500

    LC-word-break-ii [Runtime: 1 ms; Memory: 17.7 MB]

--- backtracking/word-break-ii.py
+++ backtracking/word-break-ii.py
@@ -1,21 +1,19 @@
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+    def wordBreak(self, target: str, word_dict: List[str]) -> List[str]:
+
         dp = defaultdict(list)
-
-        for i in range(len(s)):
-
-            for word in wordDict:
+        for i in range(len(target)):
+            for word in word_dict:
                 if i < len(word) - 1:
                     continue
 
-                if len(dp[i - len(word)]) > 0 or i == len(word) - 1:
-                    if s[i - len(word) + 1: i + 1] == word:
-                        if i == len(word) - 1:
-                            dp[i].append([word])
-                        else:
-                            for fragmentation in dp[i - len(word)]:
-                                dp[i].append(fragmentation + [word])
+                if target[i - len(word) + 1: i + 1] == word:
+                    if i == len(word) - 1:
+                        dp[i].append([word])
+                    else:
+                        for fragmentation in dp[i - len(word)]:
+                            dp[i].append(fragmentation + [word])
 
-        sentences = dp[len(s) - 1]
+        sentences = dp[len(target) - 1]
 
         return [" ".join(part) for part in sentences]
