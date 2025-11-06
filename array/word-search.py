1a4fa3a4e50b384bdaf9645fdfa17090904aa4e8
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-06 14:38:10 -0500

    LC-word-search [Runtime: N/A; Memory: N/A]

--- array/word-search.py
+++ array/word-search.py
@@ -1,43 +1,39 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
-        DIRS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        DIRS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
         def dfs(x, y, i):
-            nonlocal found, visited, board
-            
+            nonlocal word, board, m, n, DIRS, visited, found
+
             if i == len(word) - 1:
                 found = True
-                return 
-
-            visited.add((x, y))
+                return
 
             for dx, dy in DIRS:
                 nx, ny = x + dx, y + dy
-                
 
                 if not (0 <= nx < m and 0 <= ny < n):
+                    continue
+                if board[nx][ny] != word[i + 1]:
                     continue
                 if (nx, ny) in visited:
                     continue
 
-                if board[nx][ny] != word[i + 1]:
-                    continue
-
+                visited.add((nx, ny))
                 dfs(nx, ny, i + 1)
-            
-            visited.remove((x, y))
-            return 
+                visited.remove((nx, ny))
 
 
+        found = False
         for i in range(m):
             for j in range(n):
+                visited = set()
                 if board[i][j] == word[0]:
-                    visited = set()
-                    found = False
                     dfs(i, j, 0)
-                    
-                    if found:
-                        return True
+
+                if found:
+                    return True
 
         return False
+    
