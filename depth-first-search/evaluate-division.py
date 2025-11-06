48627c5ffcef98b66febd3bc014565b00c00b411
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-06 10:33:17 -0500

    LC-evaluate-division [Runtime: 0 ms; Memory: 17.7 MB]

--- depth-first-search/evaluate-division.py
+++ depth-first-search/evaluate-division.py
@@ -3,39 +3,36 @@ class Solution:
         graph = defaultdict(list)
 
         for (a, b), v in zip(equations, values):
-            graph[a].append((b, v))
-            graph[b].append((a, 1/v))
+            graph[a].append((b, v)) # a / b = value
+            graph[b].append((a, 1/v)) # b / a = 1/value
 
-        def bfs(a, b):
-            nonlocal graph, ans
+        def bfs(c, d):
+            nonlocal graph
 
-            queue = deque([(a, 1)])
-            reachable = set(queue)
+            if c not in graph or d not in graph:
+                return -1
+
+            queue = deque([(c, 1)])
+            visited = set([c])
 
             while queue:
                 for _ in range(len(queue)):
-                    curr, quot = queue.popleft()
+                    node, val = queue.popleft()
 
-                    if curr == b:
-                        return quot
+                    if node == d:
+                        return val
 
-                    for neigh, neigh_quot in graph[curr]:
-                        if neigh in reachable:
+                    for neigh, neigh_val in graph[node]:
+                        if neigh in visited:
                             continue
-                        new_quot = quot * neigh_quot
-                        queue.append((neigh, new_quot))
-                        reachable.add(neigh)
+                        
+                        queue.append((neigh, val * neigh_val))
+                        visited.add(neigh)
 
             return -1
 
-
         ans = []
-        for a, b in queries:
-            if a not in graph or b not in graph:
-                ans.append(-1)
-                continue
-                
-            v = bfs(a, b)
-            ans.append(v)
+        for c, d in queries:
+            ans.append(bfs(c, d))
 
         return ans
