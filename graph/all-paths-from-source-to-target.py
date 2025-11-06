ea8307e85ecddab0400ebdc8d1148e7210cd3ce2
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-06 14:13:38 -0500

    LC-all-paths-from-source-to-target [Runtime: 5 ms; Memory: 19.3 MB]

--- graph/all-paths-from-source-to-target.py
+++ graph/all-paths-from-source-to-target.py
@@ -1,28 +1,22 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-
-        # CONSTANT
         n = len(graph)
-        src = 0
-        dst = n - 1
 
-        def parsing_node(x, curr_path):
-            nonlocal adj_graph, ans, dst
-
-            if x == dst:
-                ans.append(curr_path[:])
+        def dfs(x):
+            nonlocal graph, n, ans, path
+            if x == n - 1:
+                path.append(x)
+                ans.append(path[:])
+                path.pop()
                 return
 
-            for neigh in adj_graph[x]:
-                parsing_node(neigh, curr_path + [neigh])
+            path.append(x)
+            for neigh in graph[x]:
+                dfs(neigh)
+            path.pop()
 
-        
-        # INIT
-        adj_graph = {}
-        for i, neighs in enumerate(graph):
-            adj_graph[i] = neighs
 
-        
         ans = []
-        parsing_node(src, [src])
+        path = []
+        dfs(0)
         return ans
