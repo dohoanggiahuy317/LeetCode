83877090f0335cfb4e7cf0bb55a3de1b6c1b0e72
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-14 20:34:28 -0800

    LC-add-two-numbers [Runtime: 6 ms; Memory: 17.9 MB]

--- linked-list/add-two-numbers.py
+++ linked-list/add-two-numbers.py
@@ -5,40 +5,45 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        mask = ListNode()
-        curr = mask
-
-        head1, head2 = l1, l2
-        rem = 0
-        while head1 and head2:
-            val = head1.val + head2.val + rem
-            rem = 1 if val >= 10 else 0
-            val %= 10
-
-            curr.next = ListNode(val)
-            curr = curr.next
-            head1 = head1.next
-            head2 = head2.next
-
-        while head1:
-            val = head1.val + rem
-            rem = 1 if val >= 10 else 0
-            val %= 10
-
-            curr.next = ListNode(val)
-            curr = curr.next
-            head1 = head1.next
-        while head2:
-            val = head2.val + rem
-            rem = 1 if val >= 10 else 0
-            val %= 10
-
-            curr.next = ListNode(val)
-            curr = curr.next
-            head2 = head2.next
-
-        if rem == 1:
-            curr.next = ListNode(1)
-
-        return mask.next
         
+        def get_val(val, carry):
+            if val >= 10:
+                val %= 10
+                carry = 1
+            else:
+                carry = 0
+
+            return val, carry
+
+        ptr = ListNode()
+        temp = ptr
+        carry = 0
+
+        while l1 and l2:
+            val = l1.val + l2.val + carry
+            val, carry = get_val(val, carry)
+
+            ptr.next = ListNode(val)
+            ptr = ptr.next
+            l1 = l1.next
+            l2 = l2.next
+
+        while l1:
+            val = l1.val + carry
+            val, carry = get_val(val, carry)
+            ptr.next = ListNode(val)
+            l1 = l1.next
+            ptr = ptr.next
+        
+        while l2:
+            val = l2.val + carry
+            val, carry = get_val(val, carry)
+            ptr.next = ListNode(val)
+            l2 = l2.next
+            ptr = ptr.next
+
+        if carry == 1:
+            ptr.next = ListNode(1)
+
+
+        return temp.next
