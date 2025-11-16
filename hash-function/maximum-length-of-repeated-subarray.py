ce3d1ec4fa3e2e68023951b709d8f40cca2c6fd4
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-15 18:47:36 -0800

    LC-maximum-length-of-repeated-subarray [Runtime: N/A; Memory: N/A]

---
+++ hash-function/maximum-length-of-repeated-subarray.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        
+        ans = 0
+        for i in range(len(nums1)):
+            temp = 0
+
+            j1 = i
+            j2 = 0
+            while j1 < len(nums1) and j2 < len(nums2):
+                if nums1[j1] == nums2[j2]:
+                    j1 += 1
+                    j2 += 1
+                    temp += 1
+                else:
+                    break
+
+            ans = max(ans, temp)
+
+        return ans
