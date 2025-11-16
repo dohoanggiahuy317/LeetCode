7499493628602c76f189426f1014a41f637df05c
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-15 18:48:41 -0800

    LC-maximum-length-of-repeated-subarray [Runtime: N/A; Memory: N/A]

--- dynamic-programming/maximum-length-of-repeated-subarray.py
+++ dynamic-programming/maximum-length-of-repeated-subarray.py
@@ -17,4 +17,19 @@ class Solution:
 
             ans = max(ans, temp)
 
+        for i in range(len(nums2)):
+            temp = 0
+
+            j2 = i
+            j1 = 0
+            while j1 < len(nums1) and j2 < len(nums2):
+                if nums1[j1] == nums2[j2]:
+                    j1 += 1
+                    j2 += 1
+                    temp += 1
+                else:
+                    break
+
+            ans = max(ans, temp)
+
         return ans
