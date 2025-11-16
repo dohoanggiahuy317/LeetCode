e2ea421f9c5eb6f904846beb73c4cc685be51047
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-15 19:17:57 -0800

    LC-number-of-flowers-in-full-bloom [Runtime: 234 ms; Memory: 42.8 MB]

--- prefix-sum/number-of-flowers-in-full-bloom.py
+++ prefix-sum/number-of-flowers-in-full-bloom.py
@@ -1,18 +1,35 @@
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
-        MAX_DAY = max([day[1] for day in flowers])
-        tracker = [0] * (MAX_DAY + 2)
+        tracker = defaultdict(int)
 
         for start, end in flowers:
             tracker[start] += 1
             tracker[end + 1] -= 1
 
-        prefix_tracker = list(accumulate(tracker))
-        # print(prefix_tracker)
+        prefix_tracker = []
+        MIN_DAY = inf
+        MAX_DAY = -inf
+        for day in sorted(tracker.keys()):
+            freq = tracker[day] + (prefix_tracker[-1][1] if prefix_tracker else 0)
+            prefix_tracker.append((day, freq))
+            MIN_DAY = min(MIN_DAY, day)
+            MAX_DAY = max(MAX_DAY, day)
+        
         ans = []
         for person in people:
-            if 0 <= person <= MAX_DAY + 1:
-                ans.append(prefix_tracker[person])
-            else:
+            if not (MIN_DAY <= person <= MAX_DAY):
                 ans.append(0)
+            else:
+                l, r = 0, len(prefix_tracker) - 1
+                temp = -1
+                while l <= r:
+                    m = (l + r) // 2
+                    if person >= prefix_tracker[m][0]:
+                        temp = m
+                        l = m + 1
+                    else:
+                        r = m - 1
+
+                ans.append(prefix_tracker[temp][1])
+                
         return ans
