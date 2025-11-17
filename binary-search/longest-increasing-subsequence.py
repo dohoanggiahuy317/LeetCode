d8940daaeebedd7233cf8e8bfebfb370ca77b686
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-16 18:07:39 -0800

    LC-longest-increasing-subsequence [Runtime: N/A; Memory: N/A]

--- binary-search/longest-increasing-subsequence.py
+++ binary-search/longest-increasing-subsequence.py
@@ -1,11 +1,13 @@
 class SegmentTree:
     def __init__(self, n):
         self.N = n
-        self.tree = [0] * (2 * n)
-    
-    def update(self, i, x):
+        self.tree = [1] * (self.N * 2)
+        self.tree[self.N:]
+
+    def insert(self, i, val):
         i += self.N
-        self.tree[i] = x
+        self.tree[i] = val
+
         while i > 0:
             self.tree[i >> 1] = max(self.tree[i], self.tree[i ^ 1])
             i >>= 1
@@ -13,48 +15,45 @@ class SegmentTree:
     def query(self, l, r):
         l += self.N
         r += self.N
-        ans = 0
-
+        ans = -inf
         while l < r:
-            if (l & 1):
+            if l % 2 == 1:
                 ans = max(ans, self.tree[l])
                 l += 1
-            if (r & 1):
+            if r % 2 == 1:
                 r -= 1
                 ans = max(ans, self.tree[r])
-            
             l >>= 1
             r >>= 1
-
         return ans
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         """
-        sorted_nums = [..., (nums[i], i), ....]
-        sorted_nums[k] = (nums[i], i) 
-        -> need to find k' such that sorted_nums[k'] = (nums[j], j) 
-        nums[j] < nums[i] and j < i
+        sorted_nums = [...(nums[i], i),...]
+        assume sorted_nums[k] = (nums[i], i)
 
-        create a tree represent if we see a nums[i] in the sorted_nums
-        -> the segment tree gonna help to solve nums[j] < nums[i]
+        need to find all k' such that: 
+        sorted_nums[k'] = (nums[j], j)
+        nums[j'] < nums[j] and j < i
 
-        loop from left to right gonna help to solve j < i
+        -> the segment tree mimic the sorted_nums
+        -> loop from left to right of the nums
+        for each i, query the max from first index -> k index
         """
 
-        n = len(nums)
-        tree = SegmentTree(n)
-
         sorted_nums = sorted([(num, i) for i, num in enumerate(nums)])
-        pos = {num: k for k, (num, i) in enumerate(sorted_nums)}
+        pos = {(num, i): idx for idx, (num, i) in enumerate(sorted_nums)}
 
-        ans = 0
+
+        ans = 1
+        n = len(nums)
+        segment_tree = SegmentTree(n)
         for i, num in enumerate(nums):
-            k = pos[num]
-            length = tree.query(0, k)
-
-            ans = max(ans, length + 1)
-
-            tree.update(k, length + 1)
+            k = pos[(num, i)]
+            lis = segment_tree.query(0, k) + 1
+            ans = max(ans, lis)
+            segment_tree.insert(k, lis)
 
         return ans
+            
