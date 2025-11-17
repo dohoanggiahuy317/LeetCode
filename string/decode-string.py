83acf1541f94f2818f1983e626a3020b8039fffd
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-16 16:08:37 -0800

    LC-decode-string [Runtime: 0 ms; Memory: 17.7 MB]

--- string/decode-string.py
+++ string/decode-string.py
@@ -1,24 +1,21 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+
+        prefix, freq = "", 0
         stack = []
 
-        mul = 0
-        prefix = ""
         for char in s:
-            if char.isalpha():
-                prefix += char
-            
-            elif char.isdigit():
-                mul = mul * 10 + int(char)
-            
+            if char.isdigit():
+                freq = freq * 10 + int(char)
             elif char == "[":
-                stack.append([mul, prefix])
-                mul = 0
+                stack.append((prefix, freq))
+                freq = 0
                 prefix = ""
-
             elif char == "]":
-                prev_mul, prev_prefix = stack.pop()
-                prefix = prev_prefix + prefix * prev_mul
+                prev_prefix, prev_freq = stack.pop()
+                prefix = prev_prefix + prev_freq * prefix
+            else:
+                prefix += char
 
         return prefix
 
