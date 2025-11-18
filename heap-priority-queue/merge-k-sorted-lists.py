c696422cc6821417dd436a5bb1aeab84ee613543
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-17 18:00:16 -0800

    LC-merge-k-sorted-lists [Runtime: 7 ms; Memory: 20.4 MB]

--- heap-priority-queue/merge-k-sorted-lists.py
+++ heap-priority-queue/merge-k-sorted-lists.py
@@ -5,24 +5,28 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        def merge(l1, l2):
-            if not l1:
-                return l2
-            if not l2:
-                return l1
-            
-            if l1.val > l2.val:
-                l2.next = merge(l1, l2.next)
-                return l2
-            else:
-                l1.next = merge(l1.next, l2)
-                return l1
-        if not lists:
-            return None
+        
+        queue = []
 
-        while len(lists) > 1:
-            l1 = lists.pop()
-            l2 = lists.pop()
-            lists.append(merge(l1, l2))
+        for i, li in enumerate(lists):
+            if li:
+                heapq.heappush(queue, (li.val, i))
 
-        return lists[0]
+        ans = ListNode()
+        ptr = ans
+
+        while queue:
+            node_val, node_idx = heapq.heappop(queue)
+            node = lists[node_idx]
+
+            ptr.next = node
+            ptr = ptr.next
+
+            node = node.next
+            lists[node_idx] = node
+            if lists[node_idx]:
+                heapq.heappush(queue, (node.val, node_idx))
+
+
+        return ans.next
+        
