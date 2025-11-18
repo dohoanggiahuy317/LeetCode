586aed51776493f008e6df965e16125f49b69d2b
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-17 16:19:04 -0800

    LC-find-all-anagrams-in-a-string [Runtime: 184 ms; Memory: 18.1 MB]

--- hash-table/find-all-anagrams-in-a-string.py
+++ hash-table/find-all-anagrams-in-a-string.py
@@ -1,14 +1,17 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        
         k = len(p)
-        freq = Counter(s[:k])
         freq_p = Counter(p)
+        freq = Counter(s[:k])
         ans = [0] if freq == freq_p else []
 
-        for right in range(k, len(s)):
-            freq[s[right-k]] -= 1
-            freq[s[right]] += 1
-            if freq == freq_p:
-                ans.append(right-k+1)
+        for r in range(k, len(s)):
 
+            freq[s[r]] += 1
+            freq[s[r - k]] -= 1
+
+            if freq == freq_p:
+                ans.append(r - k + 1)
+            
         return ans
