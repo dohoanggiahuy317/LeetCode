2bf6a59327248ba22a7431394ee4714f0611fd90
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-17 16:52:20 -0800

    LC-cheapest-flights-within-k-stops [Runtime: N/A; Memory: N/A]

--- breadth-first-search/cheapest-flights-within-k-stops.py
+++ breadth-first-search/cheapest-flights-within-k-stops.py
@@ -1,30 +1,33 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = defaultdict(list)
+        
+        best = [inf] * n
+        
+        graph = [[] for _ in range(n)]
+        for f, t, p in flights:
+            graph[f].append((t, p))
 
-        for s, d, cost in flights:
-            graph[s].append((d, cost))
+        queue = [(0, 0, src)]
+        best[src] = 0
 
+        while queue:
+            cost, step, city = heapq.heappop(queue)
 
-        visited = [[inf] * (k + 2) for _ in range(n)]
-        visited[src][0] = 0
-        pq = [(0, src, 0)]
-        step = 0
-
-        while pq:
-            cost, u, s = heapq.heappop(pq)
-            if u == dst:
+            if city == dst:
                 return cost
-            if s == k + 1:
+
+            if cost > best[city]:
                 continue
 
-            for neigh, w in graph[u]:
-                new = cost + w
-                if new < visited[neigh][s]:
-                    visited[neigh][s] = new
-                    heapq.heappush(pq, (new, neigh, s + 1))
+            if step == k + 1:
+                continue
 
-            # print(visited)
+            for neigh_city, neigh_cost in graph[city]:
+                new_cost = cost + neigh_cost
+                if new_cost < best[neigh_city]:
+                    
+                    best[neigh_city] = new_cost
+                    heapq.heappush(queue, (new_cost, step + 1, neigh_city))
+
 
         return -1
-
