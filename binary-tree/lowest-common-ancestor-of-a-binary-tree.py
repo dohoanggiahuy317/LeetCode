d848ab56e6ec7500bdfa03a39fee89437b571ab2
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-19 13:51:14 -0500

    LC-lowest-common-ancestor-of-a-binary-tree [Runtime: 53 ms; Memory: 22.1 MB]

--- binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,7 +7,7 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
+        if not root or root == p or root == q:
             return root
         
         left, right = self.lowestCommonAncestor(root.right, p, q), self.lowestCommonAncestor(root.left, p, q)
