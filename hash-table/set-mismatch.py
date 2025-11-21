4f190e79e7971d17b011276ba37c564bed4492a3
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-20 23:54:41 -0500

    LC-set-mismatch [Runtime: N/A; Memory: N/A]

--- hash-table/set-mismatch.py
+++ hash-table/set-mismatch.py
@@ -4,7 +4,7 @@ class Solution:
         ans = []
         n = len(nums)
 
-        for i in range(1, n):
+        for i in range(1, n + 1):
             if c[i] == 0 or c[i] == 2:
                 ans.append(i)
 
