de4dc91d2eec6637e5a5262ec5c636b025473c4e
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-20 23:36:22 -0500

    LC-unique-length-3-palindromic-subsequences [Runtime: 477 ms; Memory: 18.4 MB]

--- bit-manipulation/unique-length-3-palindromic-subsequences.py
+++ bit-manipulation/unique-length-3-palindromic-subsequences.py
@@ -6,10 +6,8 @@ class Solution:
         for letter in letters:
             i, j = s.index(letter), s.rindex(letter)
             between = set()
-            
             for k in range(i + 1, j):
                 between.add(s[k])
-            
             ans += len(between)
 
         return ans
