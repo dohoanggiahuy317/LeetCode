a509b1b7793322a3f9513e6bd160970ec172d6b5
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-20 23:54:35 -0500

    LC-set-mismatch [Runtime: N/A; Memory: N/A]

--- bit-manipulation/set-mismatch.py
+++ bit-manipulation/set-mismatch.py
@@ -2,6 +2,7 @@ class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         c = Counter(nums)
         ans = []
+        n = len(nums)
 
         for i in range(1, n):
             if c[i] == 0 or c[i] == 2:
