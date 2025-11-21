ffb1160d506a36968fb2ae9b6da0ca2651496abc
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-20 23:50:06 -0500

    LC-set-mismatch [Runtime: N/A; Memory: N/A]

--- array/set-mismatch.py
+++ array/set-mismatch.py
@@ -1,5 +1,6 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
+        nums.sort()
         for i, num in enumerate(nums):
             if num != i + 1:
                 return [i, i + 1]
