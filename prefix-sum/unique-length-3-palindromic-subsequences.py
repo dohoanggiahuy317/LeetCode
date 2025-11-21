13637cca3a885096f52f90c7c53c3f7fb16fee39
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-20 23:09:51 -0500

    LC-unique-length-3-palindromic-subsequences [Runtime: N/A; Memory: N/A]

--- prefix-sum/unique-length-3-palindromic-subsequences.py
+++ prefix-sum/unique-length-3-palindromic-subsequences.py
@@ -1,14 +1,19 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
+        num_of_distinct_char = [0] * len(s)
+        char_endpoint = {}
+
+        for i, char in enumerate(s):
+            if char not in char_endpoint:
+                char_endpoint[char] = [i, -inf]
+                num_of_distinct_char[i] = 1 if i == 0 else num_of_distinct_char[i - 1] + 1
+            else:
+                char_endpoint[char][1] = max(char_endpoint[char][1], i)
+                num_of_distinct_char[i] = num_of_distinct_char[i - 1]
+
         ans = 0
+        for (start, end) in char_endpoint.values():
+            if end != -inf:
+                ans += num_of_distinct_char[end] - (num_of_distinct_char[start - 1] if start > 0 else 0)
 
-        for i in range(26):
-            char = chr(i + ord("a"))
-
-            if char in s:
-                l, r = s.index(char), s.rindex(char)
-
-                if r != l:
-                    ans += len(set(list(s[l+1: r])))
-        
         return ans
