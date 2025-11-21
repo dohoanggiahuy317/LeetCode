b8a9fdc0ddb6ec5c684840f2c71988bc9fbb4de7
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-20 23:55:24 -0500

    LC-set-mismatch [Runtime: 16 ms; Memory: 19 MB]

--- hash-table/set-mismatch.py
+++ hash-table/set-mismatch.py
@@ -1,11 +1,13 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         c = Counter(nums)
-        ans = []
+        ans = [-1, -1]
         n = len(nums)
 
         for i in range(1, n + 1):
-            if c[i] == 0 or c[i] == 2:
-                ans.append(i)
+            if c[i] == 0:
+                ans[1] = i
+            if c[i] == 2:
+                ans[0] = i
 
         return ans
