d65fcbc5e87d860e97f0cd9c0f587b781def5bf1
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-29 12:38:42 -0500

    LC-k-concatenation-maximum-sum [Runtime: N/A; Memory: N/A]

---
+++ array/k-concatenation-maximum-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def kConcatenationMaxSum(self, arr: List[int], k: int) -> int:
+        MOD = 10 ** 9 + 7
+
+        s = sum(arr)
+        pref_sum = list(accumulate(arr))
+        s1 = max(pref_sum)
+        suff_sum = list(accumulate(arr[::-1]))
+        s2 = max(suff_sum)
+
+        if s >= 0:
+            return ((((k - 2) % MOD) * (s % MOD)) % MOD + (s1 % MOD + s2 % MOD) % MOD) % MOD
+
+        else:
+            pref_sum = list(accumulate(arr + arr))
+            pref_min = []
+
+            for i, s in enumerate(pref_sum):
+                pref_min.append(s if i == 0 else min(s, pref_min[-1]))
+            
+            ans = -inf
+            for i, s in enumerate(pref_sum):
+                ans = max(ans, max(s, (s - pref_min[i]) if i > 0 else s))
+            
+            return ans % MOD
+                
+                
