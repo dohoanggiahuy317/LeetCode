8fd68783be502a280a9f16e4c3a5e90f4b65a65b
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-29 22:52:19 -0500

    LC-cheapest-flights-within-k-stops [Runtime: 23 ms; Memory: 19.5 MB]

--- dynamic-programming/cheapest-flights-within-k-stops.py
+++ dynamic-programming/cheapest-flights-within-k-stops.py
@@ -1,27 +1,32 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-                
-        graph = defaultdict(list)
+        
+        best = defaultdict(lambda: inf)
+        
+        graph = [[] for _ in range(n)]
         for f, t, p in flights:
             graph[f].append((t, p))
 
-        cost = [inf] * n
-        cost[src] = 0
-
-        queue = [(0, src, 0)]
+        queue = [(0, 0, src)]
+        best[(0, src)] = 0
 
         while queue:
-            curr_price, city, curr_stop = heapq.heappop(queue)
+            cost, step, city = heapq.heappop(queue)
 
-            if city == dst and curr_stop <= k + 1:
-                return curr_price
-            
-            if curr_stop == k + 1:
+            if city == dst:
+                return cost
+
+            if cost > best[city]:
                 continue
 
-            for neighbor, p in graph[city]:
-                if cost[neighbor] > curr_price + p:
-                    cost[neighbor] = curr_price + p
-                    heapq.heappush(queue, (cost[neighbor], neighbor, curr_stop + 1))
+            if step == k + 1:
+                continue
+
+            for neigh_city, neigh_cost in graph[city]:
+                new_cost = cost + neigh_cost
+                if new_cost < best[(step + 1, neigh_city)]:
+                    best[(step + 1, neigh_city)] = new_cost
+                    heapq.heappush(queue, (new_cost, step + 1, neigh_city))
+
 
         return -1
