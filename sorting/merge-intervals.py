8b6080ef42075b8819766f483ba8f83559c2ce88
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-30 13:01:28 -0500

    LC-merge-intervals [Runtime: 12 ms; Memory: 22.1 MB]

--- sorting/merge-intervals.py
+++ sorting/merge-intervals.py
@@ -1,18 +1,24 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda x: x[0])
+        lines = []
+        for st, en in intervals:
+            lines.append((st, -1))
+            lines.append((en, 1))
+
+        lines.sort()
+
         ans = []
-        curr_int = intervals.pop(0)
+        num_events = 0
+        curr_start, curr_end = -1, -1
 
-        while intervals:
-            next_int = intervals.pop(0)
+        for event, kind in lines:
+            prev_num_events = num_events
+            num_events -= kind
 
-            if next_int[0] <= curr_int[1]:
-                curr_int[1] = max(curr_int[1], next_int[1])
-            else:
-                ans.append(curr_int)
-                curr_int = next_int
-
-        ans.append(curr_int)
+            if prev_num_events == 0 and num_events == 1:
+                curr_start = event
+            if num_events == 0:
+                curr_end = event
+                ans.append((curr_start, curr_end))
 
         return ans
