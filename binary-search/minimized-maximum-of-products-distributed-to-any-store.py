ae3f44b4e9c974783ac2a6bd41a5be152fc6a917
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-29 22:09:36 -0500

    LC-minimized-maximum-of-products-distributed-to-any-store [Runtime: N/A; Memory: N/A]

--- binary-search/minimized-maximum-of-products-distributed-to-any-store.py
+++ binary-search/minimized-maximum-of-products-distributed-to-any-store.py
@@ -1,3 +1,26 @@
 class Solution:
-    def minimizedMaximum(self, n: int, q: List[int]) -> int:
-        return bisect_left(range(1,max(q)),1,key=lambda x:sum(ceil(v/x) for v in q)<=n)+1
+    def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
+        
+
+        def can_distribute(t):
+            nonlocal n
+
+            cnt = 0
+
+            for q in quantities:
+                cnt += math.ceil(q / t)
+
+            return cnt <= n
+
+        l, r = 0, max(quantities)
+        ans = inf
+
+        while l <= r:
+            m = (l + r) >> 1
+            if can_distribute(m):
+                ans = m
+                r = m - 1
+            else:
+                l = m + 1
+
+        return ans
