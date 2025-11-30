72c6050513dbfd3b507c39a0f525bbd2bebb4795
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-29 22:27:49 -0500

    LC-minimum-obstacle-removal-to-reach-corner [Runtime: 1231 ms; Memory: 43.8 MB]

--- breadth-first-search/minimum-obstacle-removal-to-reach-corner.py
+++ breadth-first-search/minimum-obstacle-removal-to-reach-corner.py
@@ -6,29 +6,27 @@ class Solution:
         cost = [[inf] * n for _ in range(m)]
         cost[0][0] = 0
 
-        queue = [(0, 0)]
-        ans = inf
+        queue = [(0, 0, 0)]
 
         while queue:
-            x, y = heapq.heappop(queue)
+            curr_cost, x, y = heapq.heappop(queue)
 
             if (x, y) == (m - 1, n - 1):
-                ans = min(ans, cost[x][y])
-                return ans
+                return curr_cost
 
             for dx, dy in DIRS:
                 nx, ny = x + dx, y + dy
 
                 if not (0 <= nx < m and 0 <= ny < n):
                     continue
-                new_cost = cost[x][y]
+                new_cost = curr_cost
                 
                 if grid[nx][ny] == 1:
-                    new_cost = cost[x][y] + 1
+                    new_cost = curr_cost + 1
                 
                 if new_cost < cost[nx][ny]:
                     cost[nx][ny] = new_cost
-                    heapq.heappush(queue, (nx, ny))
+                    heapq.heappush(queue, (new_cost, nx, ny))
 
-        return ans
+        return -1
             
