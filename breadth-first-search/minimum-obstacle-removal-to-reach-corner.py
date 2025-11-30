f00d5226fe2422115cfd1fb20f7da2d98f040afe
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-29 22:15:43 -0500

    LC-minimum-obstacle-removal-to-reach-corner [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/minimum-obstacle-removal-to-reach-corner.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minimumObstacles(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        DIRS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        cost = [[inf] * n for _ in range(m)]
+        cost[0][0] = 0
+
+        queue = deque([(0, 0)])
+        ans = inf
+
+        while queue:
+            x, y = queue.pop()
+
+            if (x, y) == (m - 1, n - 1):
+                ans = min(ans, cost[x][y])
+                continue
+
+            for dx, dy in DIRS:
+                nx, ny = x + dx, y + dy
+
+                if not (0 <= nx < m and 0 <= ny < n):
+                    continue
+                new_cost = cost[x][y]
+                
+                if grid[nx][ny] == 1:
+                    new_cost = cost[x][y] + 1
+                
+                if new_cost < cost[nx][ny]:
+                    cost[nx][ny] = new_cost
+                    queue.append((nx, ny))
+
+        return ans
+            
