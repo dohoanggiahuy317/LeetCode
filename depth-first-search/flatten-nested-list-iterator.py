fdd75192e6ec3fe1209e06a9e1e62af6b8d97db5
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-30 18:06:40 -0500

    LC-flatten-nested-list-iterator [Runtime: N/A; Memory: N/A]

--- depth-first-search/flatten-nested-list-iterator.py
+++ depth-first-search/flatten-nested-list-iterator.py
@@ -1,54 +1,27 @@
-# """
-# This is the interface that allows for creating nested lists.
-# You should not implement it, or speculate about its implementation
-# """
-#class NestedInteger:
-#    def isInteger(self) -> bool:
-#        """
-#        @return True if this NestedInteger holds a single integer, rather than a nested list.
-#        """
-#
-#    def getInteger(self) -> int:
-#        """
-#        @return the single integer that this NestedInteger holds, if it holds a single integer
-#        Return None if this NestedInteger holds a nested list
-#        """
-#
-#    def getList(self) -> [NestedInteger]:
-#        """
-#        @return the nested list that this NestedInteger holds, if it holds a nested list
-#        Return None if this NestedInteger holds a single integer
-#        """
-
 class NestedIterator:
     def __init__(self, nestedList: [NestedInteger]):
-        self.stack = [(nestedList, 0)]
+        
+        self.stack = []
+
+        n = len(nestedList)
+        for i in range(n-1, -1, -1):
+            self.stack.append(nestedList[i])
+
     
     def next(self) -> int:
-        num = self.stack.pop()
-        return num.getInteger()
+        elem = self.stack.pop()
+
+        while not elem.isInteger():
+            elem_li = elem.getList()
+            new_elem = elem_li[0]
+            rest_elems = elem_li[1:]
+
+            for rest_elem in rest_elems:
+                self.stack.append(rest_elem)
+
+            elem = new_elem
+
+        return elem.getInteger()
     
     def hasNext(self) -> bool:
-
-        while self.stack:
-            li, idx = self.stack.pop()
-
-            if idx + 1 < len(li):
-                self.stack.append((li, idx + 1))
-
-            if li[idx].isInteger():
-                self.stack.append(li[idx])
-                break
-
-            elif li[idx].getList():
-                self.stack.append((li[idx].getList(), 0))
-
-        if self.stack:
-            return True
-
-        return False
-         
-
-# Your NestedIterator object will be instantiated and called as such:
-# i, v = NestedIterator(nestedList), []
-# while i.hasNext(): v.append(i.next())
+        return len(self.stack) > 0
