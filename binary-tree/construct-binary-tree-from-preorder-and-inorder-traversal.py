4613bad02c11e2fa8bf5ae3269d670892f87ed98
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-30 20:29:04 -0500

    LC-construct-binary-tree-from-preorder-and-inorder-traversal [Runtime: 11 ms; Memory: 19.7 MB]

--- binary-tree/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ binary-tree/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -9,51 +9,29 @@ class Solution:
         
         tree_map = {}
         inorder_idx = {value: i for i, value in enumerate(inorder)}
-        # print(inorder_idx)
         
-        i = 0
-        jump = 1
+        def build_tree(l, r, l1, r1): # inclusive
+            print(l, r, l1, r1)
+            
+            if l > r:
+                return None
+                
+                            
+            node = TreeNode(preorder[l])            
+            node_idx = inorder_idx[node.val]
+            num_left = node_idx - l1
+            num_right = r1 - node_idx
+            
+            
+            if l + 1 < len(preorder):
+                node.left = build_tree( l + 1, l + num_left, l1,            node_idx - 1)
+                node.right = build_tree(l + num_left + 1, r, node_idx + 1, r1)
+            
+            return node
+            
+        root = build_tree(0, len(preorder) - 1, 0, len(preorder) - 1)
         
-        while i < len(preorder):
-            
-            node_val = preorder[i]
-            node_idx = inorder_idx[node_val]
-            if node_val not in tree_map:
-                tree_map[node_val] = TreeNode(node_val)
-                
-            if i + 1 < len(preorder):
-                child = preorder[i + 1]
-                if child not in tree_map:
-                    tree_map[child] = TreeNode(child)
-                
-                child_idx = inorder_idx[child]
-                
-                if child_idx < node_idx:
-                    tree_map[node_val].left = tree_map[child]
-                elif child_idx > node_idx:
-                    tree_map[node_val].right = tree_map[child]
-                
-                jump = 1
-                # print(tree_map[preorder[0]])
-                    
-            if i + 2 < len(preorder):
-                next_child = preorder[i + 2]
-                if next_child not in tree_map:
-                    tree_map[next_child] = TreeNode(next_child)
-                
-                next_child_idx = inorder_idx[next_child]
-                
-                if child_idx < node_idx and next_child_idx < node_idx:
-                    jump = 1
-                elif child_idx > node_idx and next_child_idx > node_idx:
-                    jump = 1
-                else:
-                    tree_map[node_val].right = tree_map[next_child]
-                    jump = 2
-                    
-            i += jump
-            
-        return tree_map[preorder[0]]
+        return root
                 
                 
             
