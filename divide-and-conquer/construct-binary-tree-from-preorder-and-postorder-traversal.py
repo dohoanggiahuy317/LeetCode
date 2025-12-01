7997642fcb114e1d23c4275343e13e041c4c43b2
Jeremy <giahuy.jeremy.do@gmail.com>
2025-11-30 19:26:12 -0500

    LC-construct-binary-tree-from-preorder-and-postorder-traversal [Runtime: N/A; Memory: N/A]

--- divide-and-conquer/construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ divide-and-conquer/construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -4,26 +4,42 @@ class Solution:
         tree_map = defaultdict()
         
         while len(preorder) > 2:
+            b = True
+            
             for i in range(n - 2):
-                if sorted(preorder[i:i + 3]) == sorted(postorder[i:i+3]):
-                    if preorder[i] not in tree_map:
-                        tree_map[preorder[i]] = TreeNode(preorder[i])
-                    if preorder[i+1] not in tree_map:
-                        tree_map[preorder[i+1]] = TreeNode(preorder[i+1])
-                    if preorder[i+2] not in tree_map:
-                        tree_map[preorder[i+2]] = TreeNode(preorder[i+2])
-                    
-                    tree_map[preorder[i]].left = tree_map[preorder[i+1]]
-                    tree_map[preorder[i]].right = tree_map[preorder[i+2]]
+                for j in range(n - 2):
+                    if Counter(preorder[i:i + 3]) == Counter(postorder[j:j+3]):
+                        if preorder[i] not in tree_map:
+                            tree_map[preorder[i]] = TreeNode(preorder[i])
+                        if preorder[i+1] not in tree_map:
+                            tree_map[preorder[i+1]] = TreeNode(preorder[i+1])
+                        if preorder[i+2] not in tree_map:
+                            tree_map[preorder[i+2]] = TreeNode(preorder[i+2])
+                        
+                        tree_map[preorder[i]].left = tree_map[preorder[i+1]]
+                        tree_map[preorder[i]].right = tree_map[preorder[i+2]]
+                        # print(preorder, postorder)
+                        preorder.pop(i + 2)
+                        preorder.pop(i + 1)
 
-                    preorder.pop(i + 1)
-                    preorder.pop(i + 2)
-
-                    postorder.pop(i)
-                    postorder.pop(i + 1)
+                        postorder.pop(j + 1)
+                        postorder.pop(j)
+                        
+                        b = False
+                        break
+                        
+                if not b:
+                    break
 
         if len(preorder) == 2:
+            if preorder[0] not in tree_map:
+                tree_map[preorder[0]] = TreeNode(preorder[0])
+            if preorder[1] not in tree_map:
+                tree_map[preorder[1]] = TreeNode(preorder[1])
             tree_map[preorder[0]].left = tree_map[preorder[1]]
+        
+        if preorder[0] not in tree_map:
+            tree_map[preorder[0]] = TreeNode(preorder[0])
 
         return tree_map[preorder[0]] 
 
