0c3b1c964d5e63f6dfb6791d87ba302f7082c67f
Jeremy <giahuy.jeremy.do@gmail.com>
2025-12-02 21:58:01 -0500

    LC-find-maximum-balanced-xor-subarray-length [Runtime: 669 ms; Memory: 51.3 MB]

--- bit-manipulation/find-maximum-balanced-xor-subarray-length.py
+++ bit-manipulation/find-maximum-balanced-xor-subarray-length.py
@@ -3,31 +3,19 @@ class Solution:
         n = len(nums)
 
         parity = [0] * n
-        
-
-        for i, num in enumerate(nums):
-            parity[i] = ((num % 2) * 2 - 1) + (parity[i - 1] if i > 0 else 0)
-
         prefix_xor = [0] * n
-        lowest_idx = defaultdict(lambda: float("INF"))
+        seen = {(0, 0): -1}
         ans = 0
-        # print(parity)
         
-
         for j, num in enumerate(nums):
             prefix_xor[j] = num ^ (prefix_xor[j - 1] if j > 0 else 0)
+            parity[j] = (1 if num % 2 else -1) + (parity[j - 1] if j > 0 else 0)
 
-            lowest_idx[prefix_xor[j]] = min(lowest_idx[prefix_xor[j]], j)
-
-            i = lowest_idx[prefix_xor[j]]
-            if parity[j] == parity[i]:
-                # print(i, j)
+            key = (prefix_xor[j], parity[j])
+            if key not in seen:
+                seen[key] = j
+            else:
+                i = seen[key]
                 ans = max(ans, j - i)
-
-            if prefix_xor[j] == 0 and parity[j] == 0:
-                ans = max(ans, j + 1)
-        
-            # print(prefix_xor)
-            # print(lowest_idx)
 
         return ans
