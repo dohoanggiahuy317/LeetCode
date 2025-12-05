9015a2ad954fb47f8987c5a3cbab51b8a16464c7
Jeremy <giahuy.jeremy.do@gmail.com>
2025-12-05 16:40:12 -0500

    LC-russian-doll-envelopes [Runtime: N/A; Memory: N/A]

--- binary-search/russian-doll-envelopes.py
+++ binary-search/russian-doll-envelopes.py
@@ -1,52 +1,41 @@
-class SegmentTree:
-    def __init__(self, n):
-        self.N = n
-        self.tree = [0] * (2 * self.N)
-
-    def update(self, i, x):
-        i += self.N
-        self.tree[i] = x
-        while i > 1:
-            self.tree[i >> 1] = max(self.tree[i], self.tree[i ^ 1])
-            i >>= 1
-    def query(self, l, r):
-        ans = 0
-        l += self.N
-        r += self.N
-        while l < r:
-            if (l & 1):
-                ans = max(ans, self.tree[l])
-                l += 1
-            if r & 1:
-                r -= 1
-                ans = max(ans, self.tree[r])
-                
-            l >>= 1
-            r >>= 1
-        return ans
-
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        envelopes.sort(key=lambda x: (x[0], -x[1]))
-        envelopes_y = sorted([y for _, y in envelopes])
-        # print(envelopes)
-        # print(envelopes_y)
-        n = len(envelopes)
-        tree = SegmentTree(n)        
 
-        ans = 1
-        for i in range(n):
-            x, y = envelopes[i]
+        def finding_lis(li):
+            
+            ans = []
+            ans.append(li[0])
 
-            y_idx = bisect_left(envelopes_y, y)
-        
-            best = tree.query(0, y_idx) + 1
-            ans = max(ans, best)
+            for i in range(1, len(li)):
+                num = li[i]
 
-            tree.update(y_idx, best)
+                if ans[-1] < num:
+                    ans.append(num)
 
-        return ans
+                elif ans[-1] > num:
+                    l, r = 0, len(ans) - 1
+                    idx = len(ans)
+                    while l <= r:
+                        m = (l + r) // 2
 
+                        if ans[m] > num:
+                            idx = m
+                            r = m - 1
+                        else:
+                            l = m + 1
+                    ans[idx] = num
+            
+            return len(ans)
 
+        envelopes.sort(key = lambda x: (x[0], -x[1]))
+        curr_width = 0
+        heights = []
+
+        for w, h in envelopes:
+            if w > curr_width:
+                curr_width = w
+                heights.append(h)
+
+        return finding_lis(heights)
 
 
