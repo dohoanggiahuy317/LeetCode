a712ea52ce913879547e0d66b3cccd651fadd420
Jeremy <giahuy.jeremy.do@gmail.com>
2025-12-05 01:34:13 -0500

    LC-24-game [Runtime: 73 ms; Memory: 18 MB]

--- backtracking/24-game.py
+++ backtracking/24-game.py
@@ -9,39 +9,43 @@ class Solution:
                 return
             
             if len(cards) == 1:
-                if cards[0] == 24:
+                if abs(cards[0] - 24) <= 0.1:
                     found = True
                 return 
 
-            for i1 in range(len(cards)):
-                num1 = cards[i1]
-                for j in range(len(cards)):
-                    i = i1
-                    if i1 == j:
+            for i in range(len(cards)):
+                num1 = cards[i]
+                for j in range(i + 1, len(cards)):
+                    if i == j:
                         continue
 
-                    if i1 > j:
-                        i -= 1
-
                     num2 = cards[j]
-
                     cards.pop(j)
 
+                    cards[i] = num1 + num2
+                    backtrack()
+
                     cards[i] = num1 - num2
+                    backtrack()
+
+                    cards[i] = num2 - num1
                     backtrack()
 
                     cards[i] = num1 * num2
                     backtrack()
 
-                    cards[i] = num1 + num2
-                    backtrack()
-
                     if num2 != 0:
                         cards[i] = num1 / num2
                         backtrack()
-
-                    cards[i] = num1
+                    
+                    if num1 != 0:
+                        cards[i] = num2 / num1
+                        backtrack()
+                    
                     cards.insert(j, num2)
+                    cards[i] = num1
+                    
+                    
 
 
         found = False
