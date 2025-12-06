cb5f303784eb70b28d7d1b2188d787612b0c93c1
Jeremy <giahuy.jeremy.do@gmail.com>
2025-12-05 23:44:35 -0500

    LC-russian-doll-envelopes [Runtime: 155 ms; Memory: 52.8 MB]

--- binary-search/russian-doll-envelopes.py
+++ binary-search/russian-doll-envelopes.py
@@ -1,29 +1,32 @@
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
         
-        def find_LIS(li):
+        def find_LIS(arr):
             LIS = []
-            LIS.append(li[0])
+            LIS.append(arr[0])
 
-            for i in range(1, len(li)):
-                num = li[i]
+            for i in range(1, len(arr)):
+                num = arr[i]
 
                 if LIS[-1] < num:
                     LIS.append(num)
-                elif LIS[-1] > num:
+                else:
                     l, r = 0, len(LIS) - 1
                     idx = -1
+
                     while l <= r:
                         m = (l + r) // 2
-                        if LIS[m] >= num:
+                        if num <= LIS[m]:
                             idx = m
                             r = m - 1
                         else:
                             l = m + 1
+                    
                     LIS[idx] = num
-            
+
             return len(LIS)
 
-
         envelopes.sort(key = lambda x: (x[0], -x[1]))
+
         return find_LIS([envelope[1] for envelope in envelopes])
+
