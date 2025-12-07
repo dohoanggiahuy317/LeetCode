b23f3789a7d4201fe7393ac5c1b55cdba635caea
Jeremy <giahuy.jeremy.do@gmail.com>
2025-12-07 15:47:41 -0500

    LC-redundant-connection [Runtime: 2 ms; Memory: 18.3 MB]

--- breadth-first-search/redundant-connection.py
+++ breadth-first-search/redundant-connection.py
@@ -1,10 +1,30 @@
+class DSU:
+    def __init__(self, n):
+        self.parents = [i for i in range(n)]
+    
+    def find(self, x):
+        if self.parents[x] != x:
+            self.parents[x] = self.find(self.parents[x])
+
+        return self.parents[x]
+
+    def union(self, x, y):
+        xr, yr = self.find(x), self.find(y)
+
+        if xr == yr:
+            return False
+        
+        if xr > yr:
+            xr, yr = yr, xr
+
+        self.parents[yr] = xr
+        return True
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        visited = set()
+
+        dsu = DSU(len(edges))
 
         for x, y in edges:
-            if x in visited and y in visited:
+            if not dsu.union(x - 1, y - 1):
                 return [x, y]
-
-            visited.add(x)
-            visited.add(y)
