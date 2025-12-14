76d33902da54d6cee705c288ff5194aa23b8efdd
Jeremy <giahuy.jeremy.do@gmail.com>
2025-12-13 22:29:36 -0500

    LC-count-the-number-of-complete-components [Runtime: 61 ms; Memory: 18.4 MB]

--- depth-first-search/count-the-number-of-complete-components.py
+++ depth-first-search/count-the-number-of-complete-components.py
@@ -1,42 +1,45 @@
+class DSU:
+    def __init__(self, n):
+        self.parents = [i for i in range(n)]
+        self.edges = [0] * n
+        self.size = [1] * n
+
+    def find(self, x):
+        if self.parents[x] != x:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+
+    def union(self, x, y):
+        xr, yr = self.find(x), self.find(y)
+
+        if xr == yr:
+            self.edges[xr] += 1
+            return
+
+        if xr > yr:
+            xr, yr = yr, xr
+
+        self.parents[yr] = xr
+        self.edges[xr] += self.edges[yr] + 1
+        self.edges[yr] = self.edges[xr]
+
+        self.size[xr] += self.size[yr]
+        self.size[yr] = self.size[xr]
+
+
 class Solution:
-    def countCompleteComponents(self, n: int, edges: list[list[int]]) -> int:
-        # Create adjacency list representation of the graph
-        graph = [[] for _ in range(n)]
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+        dsu = DSU(n)
+        for x, y in edges:
+            dsu.union(x, y)
 
-        # Build graph from edges
-        for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
+        parents = set([dsu.find(x) for x in range(n)])
 
-        visited = [False] * n
-        complete_components = 0
-
-        # Process each unvisited vertex
-        for vertex in range(n):
-            if not visited[vertex]:
-                # BFS to find all vertices in the current component
-                component = []
-                queue = [vertex]
-                visited[vertex] = True
-
-                while queue:
-                    current = queue.pop(0)
-                    component.append(current)
-
-                    # Process neighbors
-                    for neighbor in graph[current]:
-                        if not visited[neighbor]:
-                            queue.append(neighbor)
-                            visited[neighbor] = True
-
-                # Check if component is complete (all vertices have the right number of edges)
-                is_complete = True
-                for node in component:
-                    if len(graph[node]) != len(component) - 1:
-                        is_complete = False
-                        break
-
-                if is_complete:
-                    complete_components += 1
-
-        return complete_components
+        count = 0
+        for parent in parents:
+            si, ed = dsu.size[parent], dsu.edges[parent]
+            # print(parent, si, ed, si * (si - 1) // 2)
+            if (si * (si - 1) // 2) == ed:
+                count += 1
+        return count
