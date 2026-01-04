6fb70011859a7d924cee9dbdde76275dde26877b
Jeremy <giahuy.jeremy.do@gmail.com>
2026-01-03 22:19:40 -0800

    Synchronize existing submission history (10/22)

---
+++ array/maximum-ascending-subarray-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        curr = nums[0]
+        ans = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                curr += nums[i]
+                ans = max(ans, curr)
+            else:
+                curr = nums[i]
+                ans = max(ans, curr)
+
+        return ans
