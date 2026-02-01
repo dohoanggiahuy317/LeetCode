be977c1c74b5f0ab8823dfcff8df7990d27c0122
Jeremy <giahuy.jeremy.do@gmail.com>
2026-02-01 01:08:06 -0500

    LC-squares-of-a-sorted-array [Runtime: N/A; Memory: N/A]

--- two-pointers/squares-of-a-sorted-array.py
+++ two-pointers/squares-of-a-sorted-array.py
@@ -1,3 +1,17 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        return sorted(list(map(lambda x: x ** 2, nums)))
+        neg_squares = [num ** 2 for num in nums if num < 0]
+        pos_squares = [num ** 2 for num in nums if num >= 0]
+
+        neg_idx = len(neg_squares) - 1
+        pos_idx = 0
+        squares = []
+
+        for pos_idx, pos_square in enumerate(pos_squares):
+            while neg_idx >= 0 and pos_square > neg_squares[neg_idx]:
+                squares.append(neg_squares[neg_idx])
+                neg_idx -= 1
+
+            squares.append(pos_square)
+
+        return squares
