3764a7053f1979d821d3cab765a7ef7a077db0d6
Jeremy <giahuy.jeremy.do@gmail.com>
2026-02-15 18:04:29 -0500

    LC-sort-even-and-odd-indices-independently [Runtime: N/A; Memory: N/A]

--- array/sort-even-and-odd-indices-independently.py
+++ array/sort-even-and-odd-indices-independently.py
@@ -1,13 +1,15 @@
 class Solution:
     def sortEvenOdd(self, nums: List[int]) -> List[int]:
-        even = sorted(nums[::2], reverse = True)
-        odd = sorted(nums[1::2])
-        
-        for i in range(len(nums)):
-            if i % 2 == 1:
-                nums[i] = odd.pop()
-                
-            else:
-                nums[i] = even.pop()
-                
-        return nums
+        i = j = 0
+        out = []
+
+        while i < len(even) and j < len(odd):
+            out.append(even[i])
+            out.append(odd[j])
+            i += 1
+            j += 1 
+
+        # append leftovers
+        out.extend(even[i:])
+        out.extend(odd[j:])
+        return out
