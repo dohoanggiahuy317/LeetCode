7b8174777635f5a9e118da13f74a06200052daa2
Jeremy <giahuy.jeremy.do@gmail.com>
2026-02-14 22:42:52 -0500

    LC-shopping-offers [Runtime: N/A; Memory: N/A]

---
+++ backtracking/shopping-offers.py
@@ -0,0 +1,25 @@
+class Solution:
+    def shoppingOffers(self, price: List[int], special: List[List[int]], needs: List[int]) -> int:
+        n = len(price)
+        special = sorted(special, reverse = True, key = lambda x: x[n])
+        special_idx = 0
+
+        remain_needs = []
+        cost = 0
+
+        while special_idx < len(special):
+            if all(need >= special[special_idx][i] for i, need in enumerate(needs)):
+                for i, need in enumerate(needs):
+                    remain_needs.append(need - special[special_idx][i])
+                
+                cost += special[special_idx][n]
+                needs = remain_needs[:]
+            else:
+                special_idx += 1
+
+
+        for i, need in enumerate(needs):
+            if need > 0:
+                cost += need * price[i]
+
+        return cost
