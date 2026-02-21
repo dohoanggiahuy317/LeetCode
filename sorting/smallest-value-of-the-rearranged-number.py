1a995af6e0f782ff31cc2ca52eeb7628af81bb84
Jeremy <giahuy.jeremy.do@gmail.com>
2026-02-21 18:28:50 -0500

    LC-smallest-value-of-the-rearranged-number [Runtime: 0 ms; Memory: 19.4 MB]

--- sorting/smallest-value-of-the-rearranged-number.py
+++ sorting/smallest-value-of-the-rearranged-number.py
@@ -1,18 +1,16 @@
 class Solution:
     def smallestNumber(self, num: int) -> int:
-        digits = list(str(abs(num)))
-        digits.sort()
+        res = 0
+        if num > 0:
+            arr = sorted([int(x) for x in str(num) if x != '0'])
+            main_num = arr[0] * (10 ** (len(str(num)) - 1))
+            res = main_num + int(''.join(str(x) for x in arr[1:]) if len(arr) > 1 else '0')
+               
+        elif num < 0:
+            num = abs(num)
+            arr = sorted([int(x) for x in str(num)])
+            arr = arr[::-1]
+            res = ''.join(str(x) for x in arr)
+            res = int(res) * -1
 
-        if num < 0:
-            smallest = "".join(digits[::-1])
-            return - int(smallest)
-        
-        for i, num in enumerate(digits):
-            if num != "0":
-                digits[i], digits[0] = digits[0], digits[i]
-                break
-
-        smallest = "".join(digits)
-        return int(smallest)
-
-        
+        return res
