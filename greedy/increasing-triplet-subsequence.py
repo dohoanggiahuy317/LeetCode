37980ffaff15f289d7981b0c92098d46023187f2
Jeremy <giahuy.jeremy.do@gmail.com>
2026-02-22 14:58:17 -0500

    LC-increasing-triplet-subsequence [Runtime: 38 ms; Memory: 38.9 MB]

--- greedy/increasing-triplet-subsequence.py
+++ greedy/increasing-triplet-subsequence.py
@@ -1,16 +1,22 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        if len(nums) < 3:
+        if len(nums) <= 2:
             return False
-
-        x, y = float("INF"), float("INF")
-
-        for num in nums:
-            if num <= x:
-                x = num
-            elif num <= y:
-                y = num
+            
+        min_x = 0 # maintain the index min_x that has minimum value
+        min_y = -1
+        for i in range(1, len(nums)):
+            if min_y == -1: # if min_y does not exist then try to create it
+                if nums[min_x] < nums[i]: # we found a candidate for a[x] < a[y]
+                    min_y = i
+                else:
+                    min_x = i # minimize min_x otherwise
             else:
-                return True
-
+                if nums[min_y] < nums[i]: # This is a solution
+                    return True
+                else: 
+                    if nums[min_x] < nums[i]: # not a solution, but let's try to minimize min_y
+                        min_y = i
+                    else:
+                        min_x = i # cannot minimize min_y, at least update min_x
         return False
