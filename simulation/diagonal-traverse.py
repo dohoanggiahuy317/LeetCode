b5d721373c1e80c4e3cc70f60c02065d886ce213
Jeremy <giahuy.jeremy.do@gmail.com>
2026-02-21 20:05:22 -0500

    LC-diagonal-traverse [Runtime: 4 ms; Memory: 21.6 MB]

--- simulation/diagonal-traverse.py
+++ simulation/diagonal-traverse.py
@@ -1,20 +1,42 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+
+        UP, DOWN = 1, -1
     
         m, n = len(mat), len(mat[0])
+        i, j = 0, 0
+        direction = UP
         zigzag = []
 
-        for diag in range(m + n - 1):
-            for i in range(diag + 1):
-                
-                if diag % 2 == 0:
-                    i = diag - i
+        for _ in range(m * n):
+            zigzag.append(mat[i][j])
 
-                j = diag - i
-                
-                if not (0 <= i <= m - 1 and 0 <= j <= n - 1):
-                    continue
-                    
-                zigzag.append(mat[i][j])
+            if direction == UP:
+                if i == 0:
+                    if j == n - 1:
+                        i = 1
+                    else:
+                        j += 1
+                    direction = DOWN
+                elif j == n - 1:
+                    i += 1
+                    direction = DOWN
+                else:
+                    i -= 1
+                    j += 1
+            
+            else:
+                if j == 0:
+                    if i == m - 1:
+                        j = 1
+                    else:
+                        i += 1
+                    direction = UP
+                elif i == m - 1:
+                    j += 1
+                    direction = UP
+                else:
+                    i += 1
+                    j -= 1
 
         return zigzag
