a600c5be74df2e5a11a5471aa373f621d536d52d
Jeremy <giahuy.jeremy.do@gmail.com>
2026-02-23 12:53:07 -0500

    LC-make-a-square-with-the-same-color [Runtime: 3 ms; Memory: 19.6 MB]

--- matrix/make-a-square-with-the-same-color.py
+++ matrix/make-a-square-with-the-same-color.py
@@ -1,23 +1,25 @@
 class Solution:
     def canMakeSquare(self, grid: List[List[str]]) -> bool:
         
-        TOP_LEFT = [(-1, -1), (-1, 0), (0, -1)]
-        TOP_RIGHT = [(-1, 0), (-1, 1), (0, 1)]
-        BOT_LEFT = [(0, -1), (-1, 1), (1, 0)]
-        BOT_RIGHT = [(1, 0), (1, 1), (0, 1)]
-        DIRS = [TOP_LEFT, TOP_RIGHT, BOT_LEFT, BOT_RIGHT]
+        DIRS = [ ((-1, -1), (-1, 0)), 
+                 ((-1, 1),  (0, 1)),
+                 ((1, 1),   (1, 0)),
+                 ((1, -1),  (0, -1)),
+                 ((0, -1),  (-1, -1))
+                ]
 
         m, n = len(grid), len(grid[0])
         for x in range(m):
             for y in range(n): # iterate thru each cell
-                
-                for orient in DIRS: # check all directions
-                    for dx, dy in orient: # check each neighbor cell
+                cell_color = grid[x][y]
+
+                for rect in DIRS: # check all neighbor 2x1 rect
+                    for dx, dy in rect: # check each cell
                         nx, ny = x + dx, y + dy
 
                         if not (0 <= nx < m and 0 <= ny < n):
                             break
-                        if grid[nx][ny] == "B":
+                        if grid[nx][ny] != cell_color:
                             break
                     else:
                         return True
