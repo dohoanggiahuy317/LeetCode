6fa59eec1441016d4a3142e6624b45cfbf4782f6
Jeremy <giahuy.jeremy.do@gmail.com>
2026-02-26 20:37:07 -0500

    LC-special-positions-in-a-binary-matrix [Runtime: N/A; Memory: N/A]

--- matrix/special-positions-in-a-binary-matrix.py
+++ matrix/special-positions-in-a-binary-matrix.py
@@ -2,14 +2,28 @@ class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
         m, n = len(mat), len(mat[0])
         count = 0
+        seen_x = {}
+        seen_y = {}
 
-        for i, row in enumerate(mat):
-            if sum(row) != 1:
-                continue
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] != 1:
+                    continue
 
-            j = row.index(1)
-
-            if sum([mat[k][j] for k in range(m)]) == 1:
-                count += 1
-
+                if i in seen_x or j in seen_y:
+                    if i in seen_x and not seen_x[i]:
+                        seen_x[i] = True
+                        count -= 1
+                    if j in seen_y and not seen_y[j]:
+                        seen_y[j] = True
+                        count -= 1
+                else:
+                    seen_x[i] = False
+                    seen_y[j] = False
+                    count += 1
+                
+                # print(seen_x)
+                # print(seen_y)
+                # print()
+            
         return count
