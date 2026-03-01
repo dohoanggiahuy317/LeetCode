98d28dc6da1343f176ad8f65c05c8c91143643d5
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-01 00:57:02 -0500

    LC-happy-number [Runtime: N/A; Memory: N/A]

--- math/happy-number.py
+++ math/happy-number.py
@@ -1,15 +1,11 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def sq_sum_func(n):
-            if n == 1 or n == 7:
-                return True
-            if n < 10:
+        seen = set()
+
+        while n != 1:
+            if n in seen:
                 return False
-            
-            sq_sum = 0
-            for i in str(n):
-                sq_sum += int(i)**2
-                
-            return sq_sum_func(sq_sum)
-        
-        return sq_sum_func(n)
+
+            n = sum([int(digit) ** 2 for digit in str(n)])
+
+        return True
