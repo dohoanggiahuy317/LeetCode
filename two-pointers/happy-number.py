8e724dd0859e54cb6ca23d01ad647ff84c57be0f
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-01 00:57:25 -0500

    LC-happy-number [Runtime: 3 ms; Memory: 19.1 MB]

--- two-pointers/happy-number.py
+++ two-pointers/happy-number.py
@@ -5,7 +5,9 @@ class Solution:
         while n != 1:
             if n in seen:
                 return False
-
+                
+            seen.add(n)
             n = sum([int(digit) ** 2 for digit in str(n)])
+            
 
         return True
