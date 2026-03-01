56a57c6352d6f89300d6a38a53cbb77c2966b4ae
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-01 00:37:41 -0500

    LC-isomorphic-strings [Runtime: N/A; Memory: N/A]

--- hash-table/isomorphic-strings.py
+++ hash-table/isomorphic-strings.py
@@ -1,26 +1,15 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        x1 = {}
-        x2 = {}
-
-        ans1 = ""
-        ans2 = ""
-
-        count = 0
-        for s1 in s:
-            if s1 not in x1:
-                count += 1
-                x1[s1] = count
-            ans1 += str(x1[s1]) 
-
-        count = 0
-        for s2 in t:
-            if s2 not in x2:
-                count += 1
-                x2[s2] = count 
-            ans2 += str(x2[s2]) 
-
-        # print(x1, x2, ans1, ans2) 
-
-        return ans1 == ans2
         
+        if len(s) != len(t): # constraint
+            return False
+
+        char_map = {}
+
+        for char_s, char_t in zip(s, t):
+            if char_s in char_map and char_map[char_s] != char_t:
+                return False
+
+            char_map[char_s] = char_t
+
+        return True
