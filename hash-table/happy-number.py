0094897e489697222f0866465b1baf637a4b0735
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-01 00:58:23 -0500

    LC-happy-number [Runtime: 0 ms; Memory: 19.5 MB]

--- hash-table/happy-number.py
+++ hash-table/happy-number.py
@@ -5,9 +5,16 @@ class Solution:
         while n != 1:
             if n in seen:
                 return False
-                
+
             seen.add(n)
-            n = sum([int(digit) ** 2 for digit in str(n)])
+            temp = n
+            n = 0
+            
+            while temp > 0:
+                n += (temp % 10) ** 2
+                temp //= 10
+            
+
             
 
         return True
