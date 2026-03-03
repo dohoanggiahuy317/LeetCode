6b2bd2d915194be2fffbd3b18004316cd78899d8
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-03 15:08:50 -0500

    LC-contains-duplicate-ii [Runtime: 31 ms; Memory: 36.7 MB]

--- array/contains-duplicate-ii.py
+++ array/contains-duplicate-ii.py
@@ -1,15 +1,16 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        d = {}
 
-        for i in range(len(nums)):
-            x = nums[i]
-            if x not in d:
-                d[x] = i
-            else:
-                if i - d[x] <= k:
-                    return True
-                d[x] = i
+        indices_map = {}
         
+        # loop qua nums và lưu cái index của số val gần nhát
+        for j, num in enumerate(nums):
+
+            # nếu mình có gặp lại số val, thì check khoảng cái j (index mới) - i (index gần nhất)
+            # nếu mà khoảng cách <= k: return True
+            if num in indices_map and j - indices_map[num] <= k:
+                return True
+            
+            indices_map[num] = j
+
         return False
-                
