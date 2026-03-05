00d80f541a3c5c4173538dcf7a2481d843c28beb
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-05 10:55:24 -0500

    LC-number-of-arithmetic-triplets [Runtime: 18 ms; Memory: 19.3 MB]

--- two-pointers/number-of-arithmetic-triplets.py
+++ two-pointers/number-of-arithmetic-triplets.py
@@ -5,10 +5,14 @@ class Solution:
         #pair_count[j] = number of i such as i < j and nums[j] - nums[i] = diff
         triplet = 0
 
+        for j in range(n):
+            for i in range(j):
+                if nums[j] - nums[i] == diff:
+                    pair_count[j] += 1
+
         for k in range(n):
             for j in range(k):
                 if nums[k] - nums[j] == diff:
-                    pair_count[k] += 1
                     triplet += pair_count[j]
 
         return triplet
