38fb98550f14c1779332a289d0bd5e9a5a9eac23
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-05 11:14:26 -0500

    LC-number-of-arithmetic-triplets [Runtime: 31 ms; Memory: 19.3 MB]

--- hash-table/number-of-arithmetic-triplets.py
+++ hash-table/number-of-arithmetic-triplets.py
@@ -1,18 +1,16 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
         n = len(nums)
-        pair_count = defaultdict(int)
-        #pair_count[j] = number of i such as i < j and nums[j] - nums[i] = diff
         triplet = 0
 
         for j in range(n):
-            for i in range(j):
-                if nums[j] - nums[i] == diff:
-                    pair_count[j] += 1
-
-        for k in range(n):
-            for j in range(k):
-                if nums[k] - nums[j] == diff:
-                    triplet += pair_count[j]
+            count_i_k = 0
+            
+            for i_k in range(n):  
+                if i_k != j and abs(nums[j] - nums[i_k]) == diff:
+                   count_i_k += 1
+            
+            if count_i_k == 2:
+                triplet += 1
 
         return triplet
