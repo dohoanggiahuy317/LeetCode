847e55be60aa51f193f662e601ea4bed505ab4e1
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-07 10:50:50 -0500

    LC-remove-element [Runtime: 0 ms; Memory: 19.2 MB]

--- array/remove-element.py
+++ array/remove-element.py
@@ -1,15 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-        i = 0
-        
-        while i < len(nums):
-            num = nums[i]
-            if num != val:
-                k += 1
-                i += 1
-            else:
-                nums.pop(i)
-
-        # print(nums)
-        return k
+        w = 0
+        for r in range(len(nums)):
+            if nums[r] != val:          # keep condition
+                nums[w] = nums[r]
+                w += 1
+        return w  # new length, nums[:w] is valid
