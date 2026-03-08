a9e31570228a8aebe9219336cbdb4b62fbf1c03e
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-08 12:01:11 -0400

    LC-check-if-n-and-its-double-exist [Runtime: N/A; Memory: N/A]

--- hash-table/check-if-n-and-its-double-exist.py
+++ hash-table/check-if-n-and-its-double-exist.py
@@ -2,6 +2,9 @@ class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         s_arr = set(arr)
 
+        if arr.count(0) == 2:
+            return True
+
         for num in s_arr:
             if num * 2 in s_arr:
                 return True
