7bc67dac8520c42c7183df74de16f41c5f70b3ce
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-08 18:51:07 -0400

    LC-group-anagrams [Runtime: 10 ms; Memory: 22 MB]

--- hash-table/group-anagrams.py
+++ hash-table/group-anagrams.py
@@ -1,8 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        track = defaultdict(list)
+        # dictionary [key: [str1, str2, ...]] trong d str1, str2, ... la anagram
+        anagrams = defaultdict(list)
 
-        for s in strs:
-            track["".join(sorted(list(s)))].append(s)
+        # loop qua tung word trong strs
+        for word in strs:
+            # sort word
+            sorted_word = "".join(sorted(list(word)))
 
-        return list(track.values())
+            # look up cai sorted word trong dict vaf them vao cai list tuong ung
+            anagrams[sorted_word].append(word)
+
+
+        #return dict.values()
+        return list(anagrams.values())
