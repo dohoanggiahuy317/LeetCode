ca89ca9726509cb68dda4f16de4e865b1b07e0c8
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-08 12:03:13 -0400

    LC-ransom-note [Runtime: 7 ms; Memory: 19.5 MB]

--- string/ransom-note.py
+++ string/ransom-note.py
@@ -1,3 +1,3 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return Counter(ransomNote) == Counter(magazine)
+        return Counter(ransomNote) <= Counter(magazine)
