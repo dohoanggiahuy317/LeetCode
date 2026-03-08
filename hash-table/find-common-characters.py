8028e9d97b250d1a8395d15fb33c5d50905614a4
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-08 12:30:58 -0400

    LC-find-common-characters [Runtime: 11 ms; Memory: 19.5 MB]

--- hash-table/find-common-characters.py
+++ hash-table/find-common-characters.py
@@ -1,23 +1,14 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        ans = []
+        common = Counter(words[0])
 
-        bd = []
+        for word in words[1:]:
+            word_counter = Counter(word)
 
-        for word in words:
-            d = Counter()
-            for char in word:
-                d[char] += 1
-            bd.append(d)
-
-        for u in words[0]:
-            ans.append(u)
-            for d in bd:
-                if d[u] <= 0:
-                    ans.pop(-1)
-                    break
-                print(d)
-                d[u] -= 1
-            print()
+            for char, freq in common.items():
+                if char not in word_counter:
+                    common[char] = 0
+                else:
+                    common[char] = min(word_counter[char], freq)
             
-        return ans
+        return list(common.elements())
