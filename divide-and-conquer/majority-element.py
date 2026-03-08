94787a9d7178182b89b847ed934760a1823ce7a5
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-08 15:15:58 -0400

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- divide-and-conquer/majority-element.py
+++ divide-and-conquer/majority-element.py
@@ -1,11 +1,4 @@
-
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = Counter(nums)
-        n = len(nums) // 2
-
-        for num, count in counts.items():
-            if count > n:
-                return num
+        return Counter(nums).most_common()
 
