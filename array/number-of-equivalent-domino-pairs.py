34e51e1bae56cccf9aeb2729723036a18eadd85a
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-09 21:34:09 -0400

    LC-number-of-equivalent-domino-pairs [Runtime: 4 ms; Memory: 26 MB]

--- array/number-of-equivalent-domino-pairs.py
+++ array/number-of-equivalent-domino-pairs.py
@@ -1,9 +1,13 @@
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        num = [0] * 100
-        ret = 0
-        for x, y in dominoes:
-            val = x * 10 + y if x <= y else y * 10 + x
-            ret += num[val]
-            num[val] += 1
-        return ret
+        dominoes_map = defaultdict(int)
+        ans = 0
+
+        for l, r in dominoes:
+            if l > r:
+                l, r = r, l
+
+            ans += dominoes_map[(l, r)]
+            dominoes_map[(l, r)] += 1
+
+        return ans
