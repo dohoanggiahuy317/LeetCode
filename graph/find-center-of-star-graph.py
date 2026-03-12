d93acc0bb444681ecccfd68dabd0d68595bc81a1
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-12 10:43:43 -0400

    LC-find-center-of-star-graph [Runtime: N/A; Memory: N/A]

--- graph/find-center-of-star-graph.py
+++ graph/find-center-of-star-graph.py
@@ -1,18 +1,5 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        n = len(edges) + 1
-        nodes = [0] * (n + 1)
-        center, max_edges = 0, 0
-
-        for u, v in edges:
-            nodes[u] += 1
-            nodes[v] += 1
-
-            if nodes[u] > max_edges:
-                center, max_edges = u, nodes[u]
-            
-            if nodes[v] > max_edges:
-                center, max_edges = v, nodes[v]
-        
-        return center
-            
+        if edges[0][0] == edges[0][0] or edges[0][0] == edges[0][1]:
+            return edges[0][0]
+        return edges[0][1]
