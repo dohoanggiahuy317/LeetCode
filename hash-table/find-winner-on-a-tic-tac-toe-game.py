b4fbae5c20297a263f79e7f5f7044a61a85e39dc
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-11 21:32:58 -0400

    LC-find-winner-on-a-tic-tac-toe-game [Runtime: N/A; Memory: N/A]

---
+++ hash-table/find-winner-on-a-tic-tac-toe-game.py
@@ -0,0 +1,18 @@
+class Solution:
+    def tictactoe(self, moves: List[List[int]]) -> str:
+
+        TIC_TAK_TOE_MAP = {
+            (0, 0): ["row1", "col1"],
+            (0, 0): ["row1", "col1"],
+            (0, 0): ["row1", "col1"],
+            (0, 0): ["row1", "col1"],
+            (0, 0): ["row1", "col1"],
+            (0, 0): ["row1", "col1"],
+            (0, 0): ["row1", "col1"],
+            (0, 0): ["row1", "col1"],
+            (0, 0): ["row1", "col1"],
+        }
+
+        A_track = {
+
+        }
