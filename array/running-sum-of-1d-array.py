b8d3a2bd17b2135738073da4d0d453aa518affa5
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-14 10:17:15 -0400

    LC-running-sum-of-1d-array [Runtime: 0 ms; Memory: 19.4 MB]

--- array/running-sum-of-1d-array.py
+++ array/running-sum-of-1d-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         arr = [0]*len(nums)
-        cur_sum = 0 # Tổng từ số đầu tiên đến số thứ i
+        curr_sum = 0 # Tổng từ số đầu tiên đến số thứ i
         
         for i, num in enumerate(nums):
             curr_sum += num
