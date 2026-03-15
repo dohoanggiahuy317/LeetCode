0083d0b0b5911689fe58e2a7dde8c3c15dc5cb3a
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-15 16:10:44 -0400

    LC-3sum [Runtime: 1448 ms; Memory: 22.9 MB]

--- array/3sum.py
+++ array/3sum.py
@@ -1,21 +1,29 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        n = len(nums)
         nums.sort()
         triplet = []
-        for i in range(len(nums)):
-            if not (i == 0 or nums[i] != nums[i - 1]):
+
+        for i, num in enumerate(nums):
+            if i != 0 and nums[i] == nums[i - 1]:
                 continue
-            j = i + 1
-            seen = set()
-            while j < len(nums):
+            
+            target_used = {nums[k]: False for k in range(i + 1, n)}
+            target_count = Counter(nums[i + 1:])
+
+            for j in range(i + 1, n):
                 target = - nums[i] - nums[j]
 
-                if target in seen:
-                    triplet.append([nums[i], nums[j], target])
-                    while j < len(nums) - 1 and nums[j] == nums[j + 1]:
-                        j += 1
-                
-                seen.add(nums[j])
-                j += 1
+                if target not in target_used:
+                    continue
+                if target_used[target]:
+                    continue
+                if nums[j] == target and target_count[target] == 1:
+                    continue
+
+                triplet.append([nums[i], target, nums[j]])
+                target_used[target] = True
+                target_used[nums[j]] = True
 
         return triplet
+
