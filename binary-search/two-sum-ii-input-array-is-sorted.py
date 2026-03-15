70bbfad83c79cad66355dbcdd6693dbd921cd295
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-15 17:59:35 -0400

    LC-two-sum-ii-input-array-is-sorted [Runtime: 3 ms; Memory: 20.4 MB]

--- binary-search/two-sum-ii-input-array-is-sorted.py
+++ binary-search/two-sum-ii-input-array-is-sorted.py
@@ -1,12 +1,12 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        i, j = 0, len(numbers) - 1
-
-        while i < j:
-            if numbers[i] + numbers[j] > target:
-                j -= 1
-            elif numbers[i] + numbers[j] < target:
-                i += 1
-            else:
-                return [i + 1, j + 1]
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            if nums[l] + nums[r] > target:
+                r -= 1
+            elif nums[l] + nums[r] < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
