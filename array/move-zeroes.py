b7737132b8afcaa67e0ad3b3415fc6880adf5620
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-15 19:02:05 -0400

    LC-move-zeroes [Runtime: 3 ms; Memory: 20.7 MB]

--- array/move-zeroes.py
+++ array/move-zeroes.py
@@ -5,15 +5,16 @@ class Solution:
         """
         n = len(nums)
 
-        first_0_idx = -1
         for i, num in enumerate(nums):
             if num == 0:
                 first_0_idx = i   
                 break
+        else:
+            return nums
 
         for j in range(first_0_idx + 1, len(nums)):
             if nums[j] != 0:
                 nums[first_0_idx], nums[j] = nums[j], nums[first_0_idx]
                 first_0_idx += 1
-        
+
         return nums
