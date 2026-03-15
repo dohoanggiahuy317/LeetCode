02554e26f6852d6e54339755b0a3538ee22e8236
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-15 16:14:05 -0400

    LC-valid-triangle-number [Runtime: 1516 ms; Memory: 12.6 MB]

--- greedy/valid-triangle-number.py
+++ greedy/valid-triangle-number.py
@@ -5,16 +5,18 @@ class Solution(object):
         :rtype: int
         """
         
-        # Sort array and initialize count as 0
         n = len(nums)
         nums.sort()
         count = 0
-        for i in range(0, n-2):
+
+        for i in range(0, n - 2):
             k = i + 2
+
             for j in range(i + 1, n):
-                while (k < n and nums[i] + nums[j] > nums[k]):
+                while k < n and nums[i] + nums[j] > nums[k]:
                     k += 1
-                if(k>j):
-                    count += k - j - 1
+                
+                count += max(0, k - j - 1)
+        
         return count
  
