a61e2fa63b61b3079603595b1738a836b021f3bb
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-15 23:10:05 -0400

    LC-merge-two-2d-arrays-by-summing-values [Runtime: 0 ms; Memory: 19.4 MB]

--- two-pointers/merge-two-2d-arrays-by-summing-values.py
+++ two-pointers/merge-two-2d-arrays-by-summing-values.py
@@ -1,19 +1,11 @@
 class Solution:
-    def mergeArrays(
-        self, nums1: List[List[int]], nums2: List[List[int]]
-    ) -> List[List[int]]:
-        key_to_sum = {}
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        d = defaultdict(int)
 
-        # Copying the array nums1 to the map.
-        for nums in nums1:
-            key_to_sum[nums[0]] = nums[1]
+        for id_, num in nums1:
+            d[id_] += num
 
-        # Adding the values to existing keys or create new entries.
-        for nums in nums2:
-            key_to_sum[nums[0]] = key_to_sum.get(nums[0], 0) + nums[1]
+        for id_, num in nums2:
+            d[id_] += num
 
-        merged_array = []
-        for key, value in sorted(key_to_sum.items()):
-            merged_array.append([key, value])
-
-        return merged_array
+        return sorted(list(map(list, d.items())))
