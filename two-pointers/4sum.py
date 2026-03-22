6049f91afc63f7830b18de92a68309bfb4a1ce9f
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-22 02:20:28 -0400

    LC-4sum [Runtime: N/A; Memory: N/A]

--- two-pointers/4sum.py
+++ two-pointers/4sum.py
@@ -2,24 +2,26 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         n = len(nums)
         nums.sort()
-        ans = set()
+        ans = []
 
         for a in range(n):
-            for b in range(a + 1, n):
-                
-                c = b + 1
-                d = n - 1
-                while c < d:
-                    if nums[a] + nums[b] + nums[c] + nums[d] > target:
-                        d -= 1
-                    elif nums[a] + nums[b] + nums[c] + nums[d] < target:
-                        c += 1
-                    else:
-                        quad = tuple(sorted([nums[a], nums[b], nums[c], nums[d]]))
-                        if quad not in ans:
-                            ans.add(quad)
-                        c += 1
-                        d -= 1
+            if a > 0 and nums[a] == nums[a - 1]:
+                continue
 
-        return [list(quad) for quad in ans]
+            for b in range(a + 1, n):
+                if b > a + 1 and nums[b] == nums[b - 1]:
+                    continue
+                
+                d = n - 1
+                for c in range(b + 1, n):
+                    if c > b + 1 and nums[c] == nums[c - 1]:
+                        continue
+
+                    while d > c + 1 and nums[a] + nums[b] + nums[c] + nums[d] > target:
+                        d -= 1
+                    
+                    if nums[a] + nums[b] + nums[c] + nums[d] == target:
+                        ans.append([nums[a], nums[b], nums[c], nums[d]])
+
+        return ans
                     
