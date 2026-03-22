4ae65eb29f3d1afe07d9a6f1ccb1294013b2ecf9
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-22 03:02:20 -0400

    LC-valid-palindrome-ii [Runtime: N/A; Memory: N/A]

--- two-pointers/valid-palindrome-ii.py
+++ two-pointers/valid-palindrome-ii.py
@@ -1,19 +1,23 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         l, r = 0, len(s) - 1
-
-        b = False
+        deleted = False
 
         while l < r:
             if s[l] == s[r]:
                 l += 1
                 r -= 1
-            else:
-                if not b:
-                    b = True
-                    # print(s[l:r], s[l + 1:r + 1])
-                    return s[l:r] == s[l:r][::-1] or s[l + 1:r + 1] == s[l + 1:r + 1][::-1] 
+            elif not deleted:
+                if s[l] == s[r - 1]:
+                    r -= 1
+                elif s[l + 1] == s[r]:
+                    l += 1
+                else:
+                    return False
 
+                deleted = True
+            else:
                 return False
 
         return True
+                
