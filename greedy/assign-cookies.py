b02dd2b777f5c6f2a28feddcdb3347f122a59982
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-22 13:35:35 -0400

    LC-assign-cookies [Runtime: N/A; Memory: N/A]

--- greedy/assign-cookies.py
+++ greedy/assign-cookies.py
@@ -6,8 +6,10 @@ class Solution:
         for child in g:
             while j < len(s) and s[j] < child:
                 j += 1
-            
+
             if j < len(s) and s[j] >= child:
                 num_child += 1
+            
+            j += 1
 
         return num_child
