666c28c64ee40f299ba3ee8cff1333d9347a2340
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-21 22:05:36 -0400

    LC-find-the-longest-substring-containing-vowels-in-even-counts [Runtime: N/A; Memory: N/A]

--- prefix-sum/find-the-longest-substring-containing-vowels-in-even-counts.py
+++ prefix-sum/find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -1,27 +1,24 @@
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
-        mapy = [-2] * 32
-        mapy[0] = -1
+        VOWELS = "ueoai"
 
-        max_len = 0
-        mask = 0
+        def valid_string(s, k):
+            for i in range(len(s) - k + 1):
+                freq = Counter(s[i: i + k])
+                if all(freq[c] % 2 == 0 for c in VOWELS):
+                    return True
+            
+            return False
 
-        for i, char in enumerate(s):
-            if char == 'a':
-                mask ^= 1
-            elif char == 'e':
-                mask ^= 2
-            elif char == 'i':
-                mask ^= 4
-            elif char == 'o':
-                mask ^= 8
-            elif char == 'u':
-                mask ^= 16
+        l, r = 0, len(s)
+        ans = 0
+        while l <= r:
+            m = (l + r) >> 1
 
-            prev = mapy[mask]
-            if prev == -2:
-                mapy[mask] = i
+            if valid_string(s, m):
+                ans = m
+                l = m + 1
             else:
-                max_len = max(max_len, i - prev)
+                r = m - 1
 
-        return max_len
+        return ans
