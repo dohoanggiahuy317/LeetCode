15650d61c8684de9b7941711803b98f916499c97
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-22 02:57:02 -0400

    LC-3sum [Runtime: 627 ms; Memory: 22.3 MB]

--- array/3sum.py
+++ array/3sum.py
@@ -4,24 +4,27 @@ class Solution:
         n = len(nums)
         result = []
 
-        for i in range(n):
+        for i in range(n - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            k = n - 1
+            left, right = i + 1, n - 1
 
-            for j in range(i + 1, n):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
 
-                while k > j:
-                    total = nums[i] + nums[j] + nums[k]
-                    if total > 0:
-                        k -= 1
-                    else:
-                        break
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
 
-                if j < k and nums[i] + nums[j] + nums[k] == 0:
-                    result.append([nums[i], nums[j], nums[k]])
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
 
         return result
