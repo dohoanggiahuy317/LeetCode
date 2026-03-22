f37528d0275f61b996f8884f3c4552137304991f
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-22 02:58:19 -0400

    LC-4sum [Runtime: 398 ms; Memory: 19.3 MB]

--- two-pointers/4sum.py
+++ two-pointers/4sum.py
@@ -13,16 +13,23 @@ class Solution:
                 if b > a + 1 and nums[b] == nums[b - 1]:
                     continue
                 
-                d = n - 1
-                for c in range(b + 1, n):
-                    if c > b + 1 and nums[c] == nums[c - 1]:
-                        continue
+                left, right = b + 1, n - 1
+                while left < right:
+                    total = nums[a] + nums[b] + nums[left] + nums[right]
 
-                    while d > c + 1 and nums[a] + nums[b] + nums[c] + nums[d] > target:
-                        d -= 1
-                    
-                    if d > c and nums[a] + nums[b] + nums[c] + nums[d] == target:
-                        ans.append([nums[a], nums[b], nums[c], nums[d]])
+                    if total < target:
+                        left += 1
+                    elif total > target:
+                        right -= 1
+                    else:
+                        ans.append([nums[a], nums[b], nums[right], nums[left]])
+                        left += 1
+                        right -= 1
+
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
 
         return ans
                     
