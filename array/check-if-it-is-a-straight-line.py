0e3de28dabf900ef2d0c64497790e6c1cacc9c11
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-22 23:43:05 -0400

    LC-check-if-it-is-a-straight-line [Runtime: 0 ms; Memory: 19.4 MB]

--- array/check-if-it-is-a-straight-line.py
+++ array/check-if-it-is-a-straight-line.py
@@ -3,7 +3,10 @@ class Solution:
         x1, y1 = coordinates[0]
         x2, y2 = coordinates[1]
 
-        a = (x2 - x1)/(y2 - y1) if y2 != y1 else 0
+        if x1 == x2:
+            return all(x == x1 for x, _ in coordinates)
+
+        a = (y2 - y1)/(x2 - x1)
         b = y1 - a * x1
 
         return all(a * x + b == y for x, y in coordinates)
