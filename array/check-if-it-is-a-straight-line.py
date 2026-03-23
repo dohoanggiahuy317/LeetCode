9d210f7d8a35921c6230d2a12d726a4e1ebbf778
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-22 23:35:58 -0400

    LC-check-if-it-is-a-straight-line [Runtime: N/A; Memory: N/A]

---
+++ array/check-if-it-is-a-straight-line.py
@@ -0,0 +1,5 @@
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        x1, y1 = coordinates[0]
+        x2, y2 = coordinates[1]
+        return all((x2 - x1)/(y2 - y1) == (x - x1)/(y - y1) for x, y in coordinates[1:])
