6bb4714c65a2e694870af2758ad08570a12b756a
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-29 10:58:49 -0400

    LC-richest-customer-wealth [Runtime: 0 ms; Memory: 19.3 MB]

--- matrix/richest-customer-wealth.py
+++ matrix/richest-customer-wealth.py
@@ -1,8 +1,3 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        max_sum = -inf
-
-        for acc in accounts:
-            max_sum = max(max_sum, sum(acc))
-
-        return max_sum
+        return max([sum(acc) for acc in accounts])
