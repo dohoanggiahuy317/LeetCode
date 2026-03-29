867eb2d018f6a1c9ef9bcc6b86862c655bda4b4c
Jeremy <giahuy.jeremy.do@gmail.com>
2026-03-29 12:30:24 -0400

    LC-valid-anagram [Runtime: 11 ms; Memory: 19.4 MB]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -1,3 +1,33 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        
+        # char_freq cho s và t
+        # a n a g r a m
+        s_freq = {}
+        for char in s:
+            if char not in s_freq:
+                s_freq[char] = 0
+            s_freq[char] += 1
+            
+        t_freq = {}
+        for char in t:
+            if char not in t_freq:
+                t_freq[char] = 0
+            t_freq[char] += 1
+            
+        return s_freq == t_freq
+            
+        
+        # for char in s_freq.keys():
+        #     if char not in t_freq:
+        #         return False
+        #     if s_freq[char] != t_freq[char]:
+        #         return False
+                
+        # for char in t_freq.keys():
+        #     if char not in s_freq:
+        #         return False
+        #     if s_freq[char] != t_freq[char]:
+        #         return False
+                
+        # return True
