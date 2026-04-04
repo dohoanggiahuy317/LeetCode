6d293ec458b3d8beefe4fc5a320066d3de915275
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-04 00:18:12 -0400

    LC-count-vowel-substrings-of-a-string [Runtime: N/A; Memory: N/A]

---
+++ hash-table/count-vowel-substrings-of-a-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countVowelSubstrings(self, word: str) -> int:
+        freq = defaultdict()
+        ans = 0
+        l = 0
+
+        for r, char in enumerate(word):
+            if char in "ueoai":
+                freq[char] = r
+
+                if len(freq) == 5:
+                    ans += min(freq.values()) - l + 1
+            else:
+                l = r + 1
+            
+        return ans
