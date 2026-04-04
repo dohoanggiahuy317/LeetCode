1d406a07e1970ee4a059f6129c0df528517150b3
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-03 23:04:42 -0400

    LC-maximum-average-subarray-i [Runtime: 51 ms; Memory: 29 MB]

--- sliding-window/maximum-average-subarray-i.py
+++ sliding-window/maximum-average-subarray-i.py
@@ -5,7 +5,7 @@ class Solution:
 
         for i in range(k, len(nums)):
             curr_sum += nums[i]
-            curr_sum -= nums[i - 1 - k]
+            curr_sum -= nums[i - k]
             best_sum = max(curr_sum, best_sum)
 
         return best_sum / k
