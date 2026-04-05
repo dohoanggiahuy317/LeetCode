ea792eb2545c909c20d515e6e89da343d69c1d65
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-04 23:59:05 -0400

    LC-permutation-in-string [Runtime: 43 ms; Memory: 19.4 MB]

--- sliding-window/permutation-in-string.py
+++ sliding-window/permutation-in-string.py
@@ -1,31 +1,22 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        k, n = len(s1), len(s2)
+        s1_c = Counter(s1)
+        s2_c = Counter(s2[:k])
 
-        def generate_hash(string):
-            hash_value = 0 
-            for ch in string:
-                hash_value += ((ord(ch) - 96) ** 4)
-            return hash_value 
-        
-        def remove_left(ch, hash_value):
-            return hash_value - ((ord(ch) - 96) ** 4) 
+        if s2_c == s1_c:
+            return True
 
-        def add_right(ch, hash_value):
-            return hash_value + ((ord(ch) - 96) ** 4) 
-        
-        left, right = 0, len(s1) - 1
-        hashed = generate_hash(s2[: right])
-        original = generate_hash(s1)
-        print(original)
-        while right < len(s2):
-            hashed = add_right(s2[right], hashed)
-            right += 1
-            if hashed == original:
+        for r in range(k, n):
+
+            s2_c[s2[r]] += 1
+            s2_c[s2[r - k]] -= 1
+
+            if s1_c == s2_c:
                 return True
-            hashed = remove_left(s2[left], hashed)
-            left += 1
-        return False
 
+
+        return False
 
 
 
