84b575bb585189ef4651d038461651efd2361ba5
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-05 11:22:02 -0400

    LC-group-anagrams [Runtime: N/A; Memory: N/A]

--- string/group-anagrams.py
+++ string/group-anagrams.py
@@ -1,16 +1,21 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # dictionary [key: [str1, str2, ...]] trong d str1, str2, ... la anagram
-        anagrams = defaultdict(list)
-
-        # loop qua tung word trong strs
-        for word in strs:
-            # sort word
-            sorted_word = "".join(sorted(list(word)))
-
-            # look up cai sorted word trong dict vaf them vao cai list tuong ung
-            anagrams[sorted_word].append(word)
-
-
-        #return dict.values()
-        return list(anagrams.values())
+        
+        groups = [[strs[0]]]
+        
+        
+        #loop qua cac string con lai
+        for s in strs[1:]:
+            got_anagram = False
+            for group in groups:
+                if Counter(s) == Counter(group[0]):
+                    group.append(s)
+                    got_anagram = True
+            
+            if not got_anagram:
+                groups.append([s])
+                
+        return groups
+        
+        
+        
