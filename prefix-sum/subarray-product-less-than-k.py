9cdf5ef2a2fec6c21544ea46e667f0ad46cd38b5
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-05 02:05:56 -0400

    LC-subarray-product-less-than-k [Runtime: 63 ms; Memory: 22.1 MB]

--- prefix-sum/subarray-product-less-than-k.py
+++ prefix-sum/subarray-product-less-than-k.py
@@ -1,19 +1,17 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        if k <= 1:
-            return 0
-
+        
         l = 0
-        curr = 1
+        curr_prod = 1
         ans = 0
 
-        for r in range(len(nums)):
-            curr *= nums[r]
+        for r, num in enumerate(nums):
+            curr_prod *= num
 
-            while curr >= k:
-                curr //= nums[l]
+            while l <= r and curr_prod >= k:
+                curr_prod //= nums[l]
                 l += 1
-
-            ans += r - l + 1
+            
+            ans += (r - l + 1)
 
         return ans
