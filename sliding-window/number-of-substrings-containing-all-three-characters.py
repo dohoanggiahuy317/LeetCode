e5f54678ca4111f73ede7625c7e1d587b6dbd786
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-09 23:52:38 -0400

    LC-number-of-substrings-containing-all-three-characters [Runtime: 170 ms; Memory: 19.5 MB]

--- sliding-window/number-of-substrings-containing-all-three-characters.py
+++ sliding-window/number-of-substrings-containing-all-three-characters.py
@@ -1,12 +1,21 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        freq = defaultdict(lambda: -1)
+        l = 0
+        freq = Counter()
         ans = 0
 
-        for i, char in enumerate(s):
-            freq[char] = i    
-            if len(freq) == 3:            
-                ans += 1 + min(freq.values())
+        for r, char in enumerate(s):
+            freq[char] += 1
+
+            while len(freq) == 3:
+                freq[s[l]] -= 1
+
+                if freq[s[l]] == 0:
+                    del freq[s[l]]
+
+                l += 1
+
+                ans += len(s) - r
 
         return ans
             
