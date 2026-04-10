7fa01bcd80502f8f83aa80071de1589b8f66570c
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-09 23:35:54 -0400

    LC-longest-repeating-character-replacement [Runtime: 2194 ms; Memory: 19.7 MB]

---
+++ sliding-window/longest-repeating-character-replacement.py
@@ -0,0 +1,30 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        lefts = {chr(ord("A") + letter_ord): 0 for letter_ord in range(26)}
+        swap_used = {chr(ord("A") + letter_ord): 0 for letter_ord in range(26)}
+        ans = 0
+
+        for r, char in enumerate(s):
+
+            for letter_ord in range(26):
+                letter = chr(ord("A") + letter_ord)
+
+                if letter == char:
+                    continue
+
+                swap_used[letter] += 1
+                
+                while swap_used[letter] > k:
+                    if s[lefts[letter]] != letter:
+                        swap_used[letter] -= 1
+
+                    lefts[letter] += 1
+
+            ans = max(ans, r - min(lefts.values() if lefts else [0]) + 1)
+
+            # print(lefts)
+            # print(swap_used)
+            # print(ans)
+
+        return ans
