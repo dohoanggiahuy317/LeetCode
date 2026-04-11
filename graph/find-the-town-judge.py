6491ec9673ca0f328870a57425f730f47ba2bafe
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-11 11:17:12 -0400

    LC-find-the-town-judge [Runtime: 20 ms; Memory: 22.5 MB]

--- graph/find-the-town-judge.py
+++ graph/find-the-town-judge.py
@@ -1,19 +1,20 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        d = {}
-
-        for i in range(1, n+1):
-            d[i] = {
-                "t": set(),
-                "f": set()
-            }
+        trust_other = defaultdict(int)
+        other_trust = defaultdict(int)
 
         for a, b in trust:
-            d[a]["t"].add(b)
-            d[b]["f"].add(a)
+            trust_other[a] += 1
+            other_trust[b] += 1
 
-        for x, y in d.items():
-            if len(y["t"]) == 0 and len(y["f"]) == n-1:
-                return x
+        for i in range(1, n + 1):
+            if trust_other[i] != 0:
+                continue
+            if other_trust[i] != n - 1:
+                continue
+
+            return i
 
         return -1
+
+        
