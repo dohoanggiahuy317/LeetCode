a9afd02afca91565a3c95ea15cea70a8003bab76
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-10 23:54:31 -0400

    LC-grumpy-bookstore-owner [Runtime: 15 ms; Memory: 22.1 MB]

--- sliding-window/grumpy-bookstore-owner.py
+++ sliding-window/grumpy-bookstore-owner.py
@@ -1,10 +1,16 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
 
-        curr_stf = sum()
+        curr_stf = sum(c for c, g in zip(customers[:minutes], grumpy[:minutes]) if g == 1)
+        max_stf = curr_stf
 
+        for r in range(minutes, len(customers)):
+            if grumpy[r] == 1:
+                curr_stf += customers[r]
+            if grumpy[r - minutes] == 1:
+                curr_stf -= customers[r - minutes]
 
+            max_stf = max(max_stf, curr_stf)
 
-
-
-        
+        alr_stf = sum(c for c, g in zip(customers, grumpy) if g == 0)
+        return max_stf + alr_stf
