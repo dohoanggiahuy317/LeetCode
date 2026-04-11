a91d642095eeb74b403ae8bd4a6b440387424c26
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-10 23:52:31 -0400

    LC-minimum-recolors-to-get-k-consecutive-black-blocks [Runtime: 0 ms; Memory: 19.4 MB]

--- string/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ string/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,19 +1,14 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
-        l = 0
         swap_used = blocks[:k].count("W")
         ans = swap_used
 
         for r in range(k, len(blocks)):
-
             swap_used += int(blocks[r] != "B")
 
-            if r - l + 1 > k:
-                if blocks[l] == "W":
-                    swap_used -= 1
-                l += 1
-            
-            if r - l + 1 >= k:
-                ans = min(ans, swap_used)
+            if blocks[r - k] == "W":
+                swap_used -= 1
+        
+            ans = min(ans, swap_used)
 
         return ans
