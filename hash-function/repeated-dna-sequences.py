552d90f6f905f4ce39d9ea6e36033575327431a6
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-11 13:25:30 -0400

    LC-repeated-dna-sequences [Runtime: N/A; Memory: N/A]

--- hash-function/repeated-dna-sequences.py
+++ hash-function/repeated-dna-sequences.py
@@ -1,20 +1,22 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        DNA_MAP = {"A": 3, "C": 2, "G": 1, "T": 0}
-        MASK = 4 ** 9 # Can use binary lifting for optimize here (?)
-
-        seq = 0
-        for char in s[:10]:
-            seq = seq * 4 + DNA_MAP[char]
-
-        visited, ans = set([seq]), set()
-
+        
+        BIT_MAP = {"A": 0, "C": 1, "G": 2, "T": 3}
+        
+        curr_window = 0
+        for i in range(10):
+            curr_window = curr_window * 4 + BIT_MAP[s[i]]
+            
+        seen = set()
+        ans = set()
+        seen.add(curr_window)
+        
         for r in range(10, len(s)):
-            l = r - 9
-
-            seq = (seq - DNA_MAP[s[l - 1]] * MASK) * 4 + DNA_MAP[s[r]]
-            if seq in visited:
-                ans.add(s[l: r + 1])
-            visited.add(seq)
-
+            curr_window = (curr_window - BIT_MAP[s[r - 10]] * (4 ** 9)) * 4 + BIT_MAP[s[r]]
+            
+            if curr_window in seen:
+                ans.add(s[r - 9: r + 1])
+            
+            seen.add(curr_window)
+        
         return list(ans)
