ed985db3820884b0add046443ce6077e43e2ecbc
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-12 23:25:01 -0400

    LC-longest-happy-prefix [Runtime: 279 ms; Memory: 20.5 MB]

--- rolling-hash/longest-happy-prefix.py
+++ rolling-hash/longest-happy-prefix.py
@@ -2,17 +2,18 @@ class Solution:
     def longestPrefix(self, s: str) -> str:
         n = len(s)
 
-        DICT_MAP = {chr(ord("a") + i): i for i in range(26)}
         MOD = 10 ** 9 + 7
-
+        BASE = 31
+        DICT_MAP = {chr(ord("a") + i): i + 1 for i in range(BASE)}
+        
         prefix = 0
         suffix, power = 0, 1
         ans = 0
 
         for i in range(n - 1):
-            prefix = (prefix * 26 + DICT_MAP[s[i]]) % MOD
+            prefix = (prefix * BASE + DICT_MAP[s[i]]) % MOD
             suffix = (DICT_MAP[s[n - 1 - i]] * power + suffix) % MOD
-            power = (power * 26) % MOD
+            power = (power * BASE) % MOD
 
             if prefix == suffix:
                 ans = i + 1
