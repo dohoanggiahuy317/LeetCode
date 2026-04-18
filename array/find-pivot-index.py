b8eaaefb023bd57853ba5ce36a75bb755ee2ed5f
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-18 11:27:46 -0400

    LC-find-pivot-index [Runtime: 1 ms; Memory: 20.3 MB]

--- array/find-pivot-index.py
+++ array/find-pivot-index.py
@@ -1,17 +1,12 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        pref_sum = list(accumulate(nums))
-        suff_sum = list(accumulate(nums[::-1]))[::-1]
+        pref_sum = 0
         sum_nums = sum(nums)
 
         for i, num in enumerate(nums):
-            p, s = 0, sum_nums
-            if i != 0:
-                p = pref_sum[i - 1]
-            if i != len(nums) - 1:
-                s = suff_sum[i + 1]
-
-            if p == s:
+            suff_sum = sum_nums - pref_sum - num
+            if suff_sum == pref_sum:
                 return i
+            pref_sum += num
 
         return -1
