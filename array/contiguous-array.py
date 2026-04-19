a7441117444e0a21234f8bcfbdd0fe29c73a8442
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-18 23:34:08 -0400

    LC-contiguous-array [Runtime: N/A; Memory: N/A]

--- array/contiguous-array.py
+++ array/contiguous-array.py
@@ -4,7 +4,7 @@ class Solution:
         pref, ans = 0, 0
 
         for j, num in enumerate(nums):
-            pref += 1 if num == 1 else -1
+            pref += int(num)
             ans = max(ans, j - freq[pref])
             freq[pref] = min(j, freq[pref])
 
