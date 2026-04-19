3eeb14b761f850b8534ffe3b13981872e4b84c2d
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-18 23:14:44 -0400

    LC-product-of-array-except-self [Runtime: 47 ms; Memory: 33.1 MB]

--- array/product-of-array-except-self.py
+++ array/product-of-array-except-self.py
@@ -1,14 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        prefL = [1] * n
+
+        pref_prod = defaultdict(lambda : 1)
         for i, num in enumerate(nums):
-            prefL[i] = num * (prefL[i - 1] if i > 0 else 1)
+            pref_prod[i] = pref_prod[i - 1] * num
 
         ans = [1] * n
-        curr = 1
-        for i in range(len(nums) - 1, -1, -1):
-            ans[i] = curr * (prefL[i - 1] if i > 0 else 1)
-            curr *= nums[i]
+        suff_prod = 1
+        for i in range(n - 1, -1, -1):
+            ans[i] = suff_prod * pref_prod[i - 1]
+            suff_prod *= nums[i]
 
         return ans
