5d748b2297d1da0169757538cf0f080feec3ce24
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-22 01:13:49 -0400

    LC-count-number-of-nice-subarrays [Runtime: 116 ms; Memory: 27.5 MB]

--- array/count-number-of-nice-subarrays.py
+++ array/count-number-of-nice-subarrays.py
@@ -1,18 +1,15 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        freq = defaultdict(lambda: -1, {0: -1})
-        ans = 0
-        pref = 0
-        # nums += [1]
+        curr, count = 0, 0
+        pref = defaultdict(int, {0: 1})
 
-        for i, num in enumerate(nums):
-            if num % 2 == 1:
-                pref += 1
-                freq[pref] = i
+        for num in nums:
+            curr += num % 2
+            count += pref[curr - k]
+            pref[curr] += 1
+
+        return count
+
+
+
             
-            ans += freq[pref-k+1] - freq[pref-k]
-            # print(pref, dict(freq), ans)
-
-
-
-        return ans
