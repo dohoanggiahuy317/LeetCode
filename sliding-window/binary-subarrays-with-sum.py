5dee4b9fdd26fe9e491c0dea38d08bb2ec913c10
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-22 01:11:30 -0400

    LC-binary-subarrays-with-sum [Runtime: 35 ms; Memory: 23.4 MB]

--- sliding-window/binary-subarrays-with-sum.py
+++ sliding-window/binary-subarrays-with-sum.py
@@ -1,9 +1,11 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        dp = defaultdict(int, {0: 1})
-        cur, ans = 0, 0
-        for i, num in enumerate(nums):
-            cur += num
-            ans += dp[cur-goal]
-            dp[cur] += 1
-        return ans
+        curr, count = 0, 0
+        pref = defaultdict(int, {0: 1})
+
+        for num in nums:
+            curr += num
+            count += pref[curr - goal]
+            pref[curr] += 1
+
+        return count
