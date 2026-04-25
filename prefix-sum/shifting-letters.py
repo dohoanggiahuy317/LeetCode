bb42803caca38c312ae1b61dcc8575cf86451b50
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-25 10:14:20 -0400

    LC-shifting-letters [Runtime: 99 ms; Memory: 31.4 MB]

--- prefix-sum/shifting-letters.py
+++ prefix-sum/shifting-letters.py
@@ -1,20 +1,13 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
-        # compute sum array
-        curSum = 0
-        for i in range(len(shifts)-1, -1, -1):
-            curSum += shifts[i]
-            shifts[i] = curSum
+        n = len(s)
+        total = sum(shifts)
+        prefix = list(accumulate(shifts))
 
-        ans = ""
-
-        # character replacement
-        for c, shift_amt in zip(list(s), shifts):
-            new_ord = ord(c) + shift_amt
-            new_ord = (new_ord - ord("a")) % 26 + ord("a")
-        
-            ans += chr(new_ord)
-
-        return ans
-
-                
+        result = []
+        for i in range(n):
+            shift_amount = (total - (prefix[i - 1] if i > 0 else 0)) % 26
+            new_char = chr((ord(s[i]) - ord("a") + shift_amount) % 26 + ord("a"))
+            result.append(new_char)
+            
+        return "".join(result)
