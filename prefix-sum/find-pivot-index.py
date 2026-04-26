3b4adce29d2169db3a72a2058efd7f83b3fea5ff
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-26 13:56:11 -0400

    LC-find-pivot-index [Runtime: 23 ms; Memory: 21.6 MB]

--- prefix-sum/find-pivot-index.py
+++ prefix-sum/find-pivot-index.py
@@ -11,7 +11,7 @@ class Solution:
             right_sum[i] = right_sum[i + 1] + nums[i]
         
         for i in range(n):
-            if left_sum[i - 1] == right_sum[i + 1]:
+            if left_sum[i] == right_sum[i]:
                 return i 
 
         return -1
