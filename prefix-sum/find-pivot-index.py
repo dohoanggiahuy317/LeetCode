099573a48d1f27d79fb71f9e0ccf3589f6b8e692
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-26 13:55:17 -0400

    LC-find-pivot-index [Runtime: 19 ms; Memory: 21.9 MB]

--- prefix-sum/find-pivot-index.py
+++ prefix-sum/find-pivot-index.py
@@ -5,13 +5,13 @@ class Solution:
         right_sum = defaultdict(int)
 
         for i in range(n):
-            left_sum[i+1] = (left_sum[i]) + nums[i]
+            left_sum[i] = left_sum[i - 1] + nums[i]
         
         for i in range(n-1, -1, -1):
-            right_sum[i] = (right_sum[i+1]) + nums[i + 1]
+            right_sum[i] = right_sum[i + 1] + nums[i]
         
         for i in range(n):
-            if left_sum[i-1] == right_sum[i+1]:
+            if left_sum[i - 1] == right_sum[i + 1]:
                 return i 
 
         return -1
