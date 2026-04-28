0077c0830c183bce0fe27960527336b16e9751fa
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-28 00:01:48 -0400

    LC-minimum-average-difference [Runtime: N/A; Memory: N/A]

---
+++ array/minimum-average-difference.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumAverageDifference(self, nums: List[int]) -> int:
+        n = len(nums)
+        best_diff = inf
+        best_idx = -1
+
+        pref_sum = list(accumulate(nums))
+        total = pref_sum [-1]
+
+        for i, (pref, num) in enumerate(zip(pref_sum, nums)):
+            suff = total - pref
+            diff = abs(int(
+                            pref/(i + 1) - 
+                            (suff/(n - i - 1) if i < n - 1 else 0)
+                        ))
+
+            if diff < best_diff:
+                best_diff = diff
+                best_idx = i
+            
+        return best_idx
