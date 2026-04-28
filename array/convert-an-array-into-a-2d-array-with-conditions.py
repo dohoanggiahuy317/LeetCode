c071a18dbbbcbe36ec43bdc51691407400d19624
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-28 15:31:32 -0400

    LC-convert-an-array-into-a-2d-array-with-conditions [Runtime: 0 ms; Memory: 19.4 MB]

--- array/convert-an-array-into-a-2d-array-with-conditions.py
+++ array/convert-an-array-into-a-2d-array-with-conditions.py
@@ -1,7 +1,5 @@
 class Solution:
-    def findMatrix(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()    
-        
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:        
         freq = Counter(nums)
         max_freq = max(freq.values())
         ans = [[] for _ in range(max_freq)]
