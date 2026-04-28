783489801b208bbdb871864f7dd7b2258c7d82a4
Jeremy <giahuy.jeremy.do@gmail.com>
2026-04-27 23:29:50 -0400

    LC-single-number [Runtime: 2 ms; Memory: 21 MB]

--- bit-manipulation/single-number.py
+++ bit-manipulation/single-number.py
@@ -1,9 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        print(nums)
-        for i in range(len(nums) - 1):
-            if nums[i] != nums[i+1] and nums[i] != nums[i-1]:
-                return nums[i]
-        else:
-            return nums[len(nums) - 1]
+        ans = 0
+        for num in nums:
+            ans ^= num
+
+        return ans
