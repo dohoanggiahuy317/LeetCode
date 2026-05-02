50726f6a26f1ea11f4e93d080b4304a6207a761d
Jeremy <giahuy.jeremy.do@gmail.com>
2026-05-02 10:17:29 -0400

    LC-corporate-flight-bookings [Runtime: N/A; Memory: N/A]

--- array/corporate-flight-bookings.py
+++ array/corporate-flight-bookings.py
@@ -1,9 +1,9 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        flights = [0] * (n + 2)
+        line = [0] * (n + 2)
 
         for f, l, s in bookings:
-            flights[f] += s
-            flights[l + 1] -= s
+            line[f] += s
+            line[l + 1] -= s
 
         return list(accumulate(flights))[1:-1]
