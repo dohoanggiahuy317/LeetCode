b6c74b48d9eda69dff337ad19bfa43314e7dd711
Jeremy <giahuy.jeremy.do@gmail.com>
2026-06-13 18:42:54 -0700

    LC-minimum-operations-to-collect-elements [Runtime: N/A; Memory: N/A]

---
+++ hash-table/minimum-operations-to-collect-elements.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        target = set([i for i in range(1, k)])
+        ans = 0
+
+        while len(target) > 0:
+            last_elem = nums.pop()
+            if last_elem in target:
+                target.remove(last_elem)
+
+            ans += 1
+        
+        return ans
