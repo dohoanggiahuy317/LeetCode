31842934ad14003c97c827a969cfd857dbc40db2
Jeremy <giahuy.jeremy.do@gmail.com>
2026-07-11 19:10:09 -0700

    LC-maximum-k-to-sort-a-permutation [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-k-to-sort-a-permutation.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortPermutation(self, nums: List[int]) -> int:
+        ans = 2 ** (len(nums) + 1) - 1
+
+        for i, num in enumerate(nums):
+            if num != i:
+                ans = ans & i
+
+        return ans
