08eca356a85a370cd6fd833340d0f5a4978e5b46
Jeremy <giahuy.jeremy.do@gmail.com>
2026-08-18 23:38:09 -0700

    LC-01-matrix [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/01-matrix.py
@@ -0,0 +1,37 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        ans = [[inf] * m for _ in range(n)]
+
+        sx, sy = 0, 0
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    sx, sy = i, j
+                    ans[i][j] = 0
+                    break
+        
+        DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
+        queue = deque([(sx, sy)])
+        visited = set(queue)
+
+        while queue:
+            x, y = queue.popleft()
+            for dx, dy in DIRS:
+                nx, ny = x + dx, y + dy
+
+                if (nx, ny) in visited:
+                    continue
+                if not (0 <= nx < n and 0 <= ny < m):
+                    continue
+                
+                if mat[nx][ny] == 0:
+                    ans[nx][ny] = 0
+                else:
+                    ans[nx][ny] = ans[x][y] + 1
+
+                visited.add((nx, ny))
+                queue.append((nx, ny))
+        
+        return ans
