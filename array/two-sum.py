c17476df35f9e659c1a92d8837374d88c82b919b
Jeremy <giahuy.jeremy.do@gmail.com>
2026-08-26 23:08:55 -0700

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- array/two-sum.py
+++ array/two-sum.py
@@ -1,9 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums2idx = {num: idx for idx, num in enumerate(nums)}
+        appear = defaultdict(int)
 
         for i, num in enumerate(nums):
-            if target - num in nums2idx and nums2idx[target - num] != i:
-                return [i, nums2idx[target - num] ]
+            if target - num in appear:
+                return [i, appear[target - num]]
+            appear[num] = i
 
-        return []
+        return [-1, -1]
