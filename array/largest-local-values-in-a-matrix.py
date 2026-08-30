89bb9d5efeb7023f954df756934c16436be68d44
Jeremy <giahuy.jeremy.do@gmail.com>
2026-08-29 19:16:05 -0700

    LC-largest-local-values-in-a-matrix [Runtime: N/A; Memory: N/A]

--- array/largest-local-values-in-a-matrix.py
+++ array/largest-local-values-in-a-matrix.py
@@ -1,22 +1,16 @@
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        DIRS = [(0, -1), (0, 0), (0, 1), (-1, -1), (-1, 0), (-1, 1), (1, -1), (1, 0), (1, 1)]
         n = len(grid)
-        ans = [[0] * (n-2) for _ in range(n - 2)]
 
-        for i in range(n-2):
-            for j in range(n-2):
-                ans[i][j] = max(
-                                grid[i][j],
-                                grid[i][j+1],
-                                grid[i][j+2],
-                                grid[i+1][j],
-                                grid[i+1][j+1],
-                                grid[i+1][j+2],
-                                grid[i+2][j],
-                                grid[i+2][j+1],
-                                grid[i+2][j+2],
-                                )
+        ans = [[0] * (n - 2) for _ in range(n - 2)]
+
+        for i in range(1, n - 1):
+            for j in range(1, n - 1):
+                ans_i, ans_j = i - 1, j - 1
+
+                val = max(grid[i + di][j + dj] for di, dj in DIRS)
+                ans[ans_i][ans_j] = val
+
 
         return ans
-
-
