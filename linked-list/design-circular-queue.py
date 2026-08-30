57023d5f5331a3b5c822573568099f68df3d3e24
Jeremy <giahuy.jeremy.do@gmail.com>
2026-08-29 18:55:18 -0700

    LC-design-circular-queue [Runtime: N/A; Memory: N/A]

--- linked-list/design-circular-queue.py
+++ linked-list/design-circular-queue.py
@@ -1,43 +1,71 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.queue = [0] * k
-        self.head = 0
-        self.count = 0
+        self.cap = k
+        self.size = 0
+        self.head = None
+        self.rear = None
+        
 
     def enQueue(self, value: int) -> bool:
         if self.isFull():
             return False
 
-        self.queue[ (self.head + self.count) % len(self.queue) ] = value
-        self.count += 1
+        new_node = Node(value)
+
+        if self.isEmpty():
+            self.head = new_node
+            self.rear = new_node
+        else:
+            new_node.next = self.rear
+            self.rear.prev = new_node
+            self.rear = new_node
         
+        self.size += 1
         return True
 
     def deQueue(self) -> bool:
         if self.isEmpty():
             return False
 
-        self.count -= 1
-        self.head = (self.head + 1) % len(self.queue)
-
+        prev_node = self.head.prev
+        if not prev_node:
+            self.head = None
+            self.rear = None
+        else:
+            self.head.prev = None
+            prev_node.next = None
+            self.head = prev_node
+        
+        self.size -= 1
         return True
+        
 
     def Front(self) -> int:
         if self.isEmpty():
             return -1
-        return self.queue[self.head]
+
+        return self.head.val
+        
 
     def Rear(self) -> int:
         if self.isEmpty():
             return -1
-        return self.queue[ (self.head + self.count - 1) % len(self.queue) ]
+
+        return self.rear.val
 
     def isEmpty(self) -> bool:
-        return self.count == 0
+        return self.size == 0
 
     def isFull(self) -> bool:
-        return self.count == len(self.queue)
+        return self.cap == self.size
+        
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
