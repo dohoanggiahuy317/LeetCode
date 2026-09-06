098a8fe34b856879316d62319d6b6cec020a0e2e
Jeremy <giahuy.jeremy.do@gmail.com>
2026-09-05 19:08:56 -0700

    LC-final-prices-with-a-special-discount-in-a-shop [Runtime: N/A; Memory: N/A]

--- array/final-prices-with-a-special-discount-in-a-shop.py
+++ array/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,16 +1,13 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        # Create a copy of prices array to store discounted prices
-        result = prices.copy()
+        stack = []
+        ans = prices[:]
 
-        stack = deque()
+        for i, price in enumerate(prices):
+            while stack and stack[-1][1] >= price:
+                ci, cprice = stack.pop()
+                ans[ci] -= price
 
-        for i in range(len(prices)):
-            # Process items that can be discounted by current price
-            while stack and prices[stack[-1]] >= prices[i]:
-                # Apply discount to previous item using current price
-                result[stack.pop()] -= prices[i]
-            # Add current index to stack
-            stack.append(i)
+            stack.append((i, price))
 
-        return result
+        return ans
