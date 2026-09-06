38f8b3923f64f1ece6b52be06e00793898a74e0b
Jeremy <giahuy.jeremy.do@gmail.com>
2026-09-05 19:25:16 -0700

    LC-range-frequency-queries [Runtime: N/A; Memory: N/A]

--- hash-table/range-frequency-queries.py
+++ hash-table/range-frequency-queries.py
@@ -1,24 +1,14 @@
 class RangeFreqQuery:
 
     def __init__(self, arr: List[int]):
-        self.N = len(arr)
-        self.num2idx = defaultdict(list)
-        for i, num in enumerate(arr):
-            self.num2idx[num].append(i)
+        self.freq = defaultdict(list)
 
-        # print(self.num2idx)
+        for i, num in enumerate(arr):
+            self.freq[num].append(i)
         
 
     def query(self, left: int, right: int, value: int) -> int:
-        if value not in self.num2idx:
-            return 0
-        
-        first_appear = bisect.bisect_left(self.num2idx[value], left)
-        last_appear = bisect.bisect_right(self.num2idx[value], right) - 1
-
-        # print(first_appear, last_appear)
-
-        return last_appear - first_appear + 1
+        return bisect.bisect_right(self.freq[value], right) - bisect.bisect_left(self.freq[value], left) 
 
 
 # Your RangeFreqQuery object will be instantiated and called as such:
