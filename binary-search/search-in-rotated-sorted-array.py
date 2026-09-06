984065cb1455ddbf6c96508f8413fff25c29c48e
Jeremy <giahuy.jeremy.do@gmail.com>
2026-09-05 19:19:39 -0700

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- binary-search/search-in-rotated-sorted-array.py
+++ binary-search/search-in-rotated-sorted-array.py
@@ -9,13 +9,13 @@ class Solution:
             if target == nums[m]:
                 return m
 
-            if nums[l] <= nums[m]:
-                if target < nums[l]:
-                    l = m + 1
-                else:
+            if nums[l] < nums[m]:
+                if nums[m] > target >= nums[l]:
                     r = m - 1
+                else:
+                    l= m + 1
             else:
-                if target > nums[m]:
+                if nums[m] < target <= nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
