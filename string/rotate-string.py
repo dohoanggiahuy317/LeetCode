0be576b9d540ebb4193d632235df76fa75d00ebc
Jeremy <giahuy.jeremy.do@gmail.com>
2026-09-05 19:03:21 -0700

    LC-rotate-string [Runtime: N/A; Memory: N/A]

--- string/rotate-string.py
+++ string/rotate-string.py
@@ -1,7 +1,7 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        for i in range(len(s)):
-            if goal == s[i:] + s[i:]:
+        for i in range(len(s) + 1):
+            if goal == s[i:] + s[:i]:
                 return True
 
         return False
